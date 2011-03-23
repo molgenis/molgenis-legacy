4f3e46c1eaeaa308d4ecd3b3a9c6c6a22a8c7934
Jacques Poelstra <j.h.poelstra@st.hanze.nl>
2011-03-23 10:00:59 +0000

    added "String name" and getters and setters in order to give a name to a module
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@7082 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/model/jaxb/Module.java
+++ src/org/molgenis/model/jaxb/Module.java
@@ -5,11 +5,25 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Module
 {
+	@XmlAttribute
+	private String name;
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
 	@XmlElement(name="entity")
 	private List<Entity> entities = new ArrayList<Entity>();
 
