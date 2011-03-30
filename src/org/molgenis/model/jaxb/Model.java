243830fc609dd701db125a93ab75a87ea705e07e
Joeri van der Velde <joeriv@gmail.com>
2011-03-30 14:25:04 +0000

    Added some helper functions for JAXB molgenis modelling
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@7294 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/model/jaxb/Model.java
+++ src/org/molgenis/model/jaxb/Model.java
@@ -9,30 +9,30 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
-@XmlRootElement(name="molgenis")
-@XmlAccessorType(XmlAccessType.FIELD) //so use fields bypassing get/set
+@XmlRootElement(name = "molgenis")
+@XmlAccessorType(XmlAccessType.FIELD)
+// so use fields bypassing get/set
 public class Model
 {
 	@XmlAttribute
 	private String name;
-	
+
 	@XmlAttribute
 	private String label;
-	
+
 	@XmlAttribute
 	private String version;
-	
-	@XmlElement(name="entity")
+
+	@XmlElement(name = "entity")
 	private List<Entity> entities = new ArrayList<Entity>();
-	
-	@XmlElement(name="module")
-	private List<Module> modules = new ArrayList<Module>();	
-	
+
+	@XmlElement(name = "module")
+	private List<Module> modules = new ArrayList<Module>();
+
 	@XmlElement
 	private List<Screen> screens = new ArrayList<Screen>();
-	
-//GETTERS AND SETTERS
+
+	// GETTERS AND SETTERS
 	public String getName()
 	{
 		return name;
@@ -42,7 +42,7 @@ public class Model
 	{
 		this.name = name;
 	}
-	
+
 	public String getLabel()
 	{
 		return label;
@@ -67,78 +67,74 @@ public class Model
 	{
 		return entities;
 	}
-	
-	//added function addEntity to add entity to model
-	public void addEntity(Entity e){
+
+	// added function addEntity to add entity to model
+	public void addEntity(Entity e)
+	{
 		this.entities.add(e);
 	}
-	
-	//added function addModule to add module to model
-	public void addModule(Module e){
+
+	// added function addModule to add module to model
+	public void addModule(Module e)
+	{
 		modules.add(e);
 	}
-	
-	
-	
+
 	public void setEntities(List<Entity> entities)
 	{
 		this.entities = entities;
 	}
-	
+
 	public Entity getEntity(String name)
 	{
 		for (Entity entity : entities)
 		{
-			if (entity.getName().toLowerCase().equals(name.toLowerCase()))
-				return entity;
+			if (entity.getName().toLowerCase().equals(name.toLowerCase())) return entity;
 		}
 		return null;
 	}
-	
-	/** find entity across local entities, and the ones contained in modules
+
+	/**
+	 * find entity across local entities, and the ones contained in modules
 	 * 
 	 * @param name
 	 * @return
 	 */
-	public Entity findEntity(String name){
+	public Entity findEntity(String name)
+	{
 		for (Entity entity : entities)
 		{
-			if (entity.getName().toLowerCase().equals(name.toLowerCase()))
-				return entity;
+			if (entity.getName().toLowerCase().equals(name.toLowerCase())) return entity;
 		}
-		
-		for(Module module : modules){
+
+		for (Module module : modules)
+		{
 			for (Entity entity : module.getEntities())
 			{
-				if (entity.getName().toLowerCase().equals(name.toLowerCase()))
-					return entity;
+				if (entity.getName().toLowerCase().equals(name.toLowerCase())) return entity;
 			}
 		}
 		return null;
 	}
-	
-	
+
 	public Module getModule(String name)
 	{
 		for (Module module : modules)
 		{
-			if (module.getName().toLowerCase().equals(name.toLowerCase()))
-				return module;
+			if (module.getName().toLowerCase().equals(name.toLowerCase())) return module;
 		}
-		
+
 		return null;
 	}
-	
+
 	public void removeModule(String name)
 	{
-		for(int i = 0; i < modules.size(); i++){
-			if (modules
-					.get(i)
-					.getName().toLowerCase()
-					.equals(name
-							.toLowerCase())){
-				modules
-				.remove(i);
+		for (int i = 0; i < modules.size(); i++)
+		{
+			if (modules.get(i).getName().toLowerCase().equals(
+					name.toLowerCase()))
+			{
+				modules.remove(i);
 			}
 		}
 	}
@@ -162,5 +158,39 @@ public class Model
 	{
 		this.modules = modules;
 	}
-	
+
+	/**
+	 * Find and remove an entity from either root or a module. If remove from
+	 * module, report back its name.
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public String findRemoveEntity(String name)
+	{
+		for (int i = 0; i < entities.size(); i++)
+		{
+			if (entities.get(i).getName().toLowerCase().equals(
+					name.toLowerCase()))
+			{
+				entities.remove(i);
+				return null;
+			}
+		}
+		for (Module module : modules)
+		{
+			for (int i = 0; i < module.getEntities().size(); i++)
+			{
+				if (module.getEntities().get(i).getName().toLowerCase().equals(
+						name.toLowerCase()))
+				{
+					module.getEntities().remove(i);
+					return module.getName();
+				}
+			}
+		}
+		return null;
+
+	}
+
 }
