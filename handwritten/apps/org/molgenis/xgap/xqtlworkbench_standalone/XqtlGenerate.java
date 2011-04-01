be4d7274c13be3c8c0e61cc7679d55da72983a59
Danny Arends <danny.arends@gmail.com>
2011-04-01 08:05:44 +0000

    Semi stand alone xqtl ;)
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7335 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/apps/org/molgenis/xgap/xqtlworkbench_standalone/XqtlGenerate.java
@@ -0,0 +1,38 @@
+package org.molgenis.xgap.xqtlworkbench_standalone;
+
+
+import java.io.File;
+
+import org.molgenis.Molgenis;
+
+public class XqtlGenerate
+{
+	
+  static public boolean deleteDirectory(File path) {
+	    if( path.exists() ) {
+	      File[] files = path.listFiles();
+	      for(int i=0; i<files.length; i++) {
+	         if(files[i].isDirectory()) {
+	           deleteDirectory(files[i]);
+	         }
+	         else {
+	           files[i].delete();
+	         }
+	      }
+	    }
+	    return( path.delete() );
+	  }
+	
+	public static void main(String[] args) throws Exception
+	{
+		try
+		{
+			deleteDirectory(new File("hsqldb"));
+			new Molgenis("handwritten/apps/org/molgenis/xgap/xqtlworkbench_standalone/xqtl.properties").generate();
+			
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
