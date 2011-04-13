2aa53334781bd581b9b0c3faedd97239b375df4e
Danny Arends <danny.arends@gmail.com>
2011-04-13 16:55:05 +0000

    Updates to runStandalone, we now have a GUI telling users what to do, clicking the cross will stop the web server when started from a .jar file
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7507 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/webserver/RunStandalone.java
+++ handwritten/webserver/RunStandalone.java
@@ -1,18 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
-public class RunStandalone
-{
+import org.molgenis.MolgenisOptions;
 
+import app.servlet.MolgenisServlet;
+
+public class RunStandalone extends JFrame{
+	private static final long serialVersionUID = -4617569886547354084L;
+	JTextArea output;
+
+	RunStandalone() throws IOException{
+		setSize(250,100); 
+		setTitle("Molgenis Webserver");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    setVisible(true);
+		WWWServer web = new WWWServer();
+		Thread t = new Thread(web);
+		t.start();
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(),getHeight());
+		g.setColor(Color.white);
+		g.drawString("Please open a browser and go to:",25,50);
+		g.drawString("http://localhost:" + WWWServer.DEF_PORT + "/" + MolgenisServlet.getMolgenisVariantID(),25,75);
+	}
+	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		WWWServer web = new WWWServer();
-		Thread t = new Thread(web);
-		t.start();
+		new RunStandalone();
 	}
 
 }
