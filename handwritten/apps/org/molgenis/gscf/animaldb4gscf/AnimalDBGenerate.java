4d44cd8163e8ac448a1e7086c3e4013eb3634e23
Joeri van der Velde <joeriv@gmail.com>
2011-04-14 11:21:23 +0000

    Added small app used in the demo of the NBIC hackathon product of molgenis (animaldb) + GSCF. See: http://hackathon.nmcdsp.org/
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7523 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/apps/org/molgenis/gscf/animaldb4gscf/AnimalDBGenerate.java
@@ -0,0 +1,14 @@
+package org.molgenis.gscf.animaldb4gscf;
+
+import org.apache.log4j.Logger;
+import org.molgenis.Molgenis;
+
+public class AnimalDBGenerate
+{
+	public static void main(String[] args) throws Exception
+	{
+		Logger logger = Logger.getLogger("AnimalDBGenerate");
+		new Molgenis("handwritten/apps/org/molgenis/animaldb/animaldb.properties").generate();
+		logger.info("AnimalDB generated successfully!");
+	}
+}
