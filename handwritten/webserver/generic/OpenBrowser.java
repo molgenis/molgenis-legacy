e40bf7cebc95973d9cfb721086818543257bc137
Danny Arends <danny.arends@gmail.com>
2011-04-15 14:08:21 +0000

    Added  "safari", "camino" as possible  browsers
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7565 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/webserver/generic/OpenBrowser.java
+++ handwritten/webserver/generic/OpenBrowser.java
@@ -9,7 +9,7 @@ public class OpenBrowser {
 			if (osName.startsWith("Windows")){
 				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
 			}else {
-                String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+                String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape", "safari", "camino" };
                 String browser = null;
                 for (int count = 0; count < browsers.length && browser == null; count++)
                 	if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0){
