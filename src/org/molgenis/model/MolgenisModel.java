b07cda830e6bfad2deb21114768dc3d5672c1997
Danny Arends <danny.arends@gmail.com>
2011-04-18 07:32:32 +0000

    Warning free java
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@7583 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/model/MolgenisModel.java
+++ src/org/molgenis/model/MolgenisModel.java
@@ -3,7 +3,6 @@ package org.molgenis.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Vector;
 
 import org.apache.log4j.Logger;
 import org.molgenis.MolgenisOptions;
