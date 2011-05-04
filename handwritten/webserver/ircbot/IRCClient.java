476b064bc8376d873a20e4d1fafd1a5f5f50619e
Danny Arends <danny.arends@gmail.com>
2011-05-04 05:56:32 +0000

    Doxygen comments on new files
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7683 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/webserver/ircbot/IRCClient.java
+++ handwritten/webserver/ircbot/IRCClient.java
@@ -1,3 +1,25 @@
+/**
+*
+* \file IRCClient.java
+*
+* copyright (c) 2009-2010, Danny Arends
+* last modified May, 2011
+* first written May, 2011
+*
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License,
+*     version 3, as published by the Free Software Foundation.
+* 
+*     This program is distributed in the hope that it will be useful,
+*     but without any warranty; without even the implied warranty of
+*     merchantability or fitness for a particular purpose.  See the GNU
+*     General Public License, version 3, for more details.
+* 
+*     A copy of the GNU General Public License, version 3, is available
+*     at http://www.r-project.org/Licenses/GPL-3
+*
+*/
+
 package ircbot;
 
 import java.io.IOException;
@@ -7,6 +29,12 @@ import org.jibble.pircbot.DccChat;
 import org.jibble.pircbot.PircBot;
 import org.jibble.pircbot.User;
 
+/**
+ * \brief Basic implementation of a IRC bot<br>
+ *
+ * Basic implementation of a IRC bot
+ * bugs: none found<br>
+ */
 public class IRCClient extends PircBot {
 	IRCClientData mydata;
 	ArrayList<IRCClientData> other_clients = new ArrayList<IRCClientData>();
--- handwritten/webserver/ircbot/IRCHandler.java
+++ handwritten/webserver/ircbot/IRCHandler.java
@@ -1,9 +1,36 @@
+/**
+*
+* \file IRCHandler.java
+*
+* copyright (c) 2009-2010, Danny Arends
+* last modified May, 2011
+* first written May, 2011
+*
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License,
+*     version 3, as published by the Free Software Foundation.
+* 
+*     This program is distributed in the hope that it will be useful,
+*     but without any warranty; without even the implied warranty of
+*     merchantability or fitness for a particular purpose.  See the GNU
+*     General Public License, version 3, for more details.
+* 
+*     A copy of the GNU General Public License, version 3, is available
+*     at http://www.r-project.org/Licenses/GPL-3
+*
+*/
 package ircbot;
 
 import java.util.ArrayList;
 
 import org.jibble.pircbot.NickAlreadyInUseException;
 
+/**
+ * \brief Threadable object that contains the IRC bot<br>
+ *
+ * Threadable object that contains the IRC bot
+ * bugs: none found<br>
+ */
 public class IRCHandler implements Runnable{
 	private IRCClient ircclient;
 	private static String default_name = "MWorkBOT";
