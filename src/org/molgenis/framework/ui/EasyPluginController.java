b8efc9c1d2fb683820d96ebfe769baec08e99ac0
Morris Swertz <m.a.swertz@rug.nl>
2011-05-06 20:19:58 +0000

    added automatic transactions (beginTx, commitTx, rollbackTx) to the super.handleRequest of EasyPlugin
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@7735 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/EasyPluginController.java
+++ src/org/molgenis/framework/ui/EasyPluginController.java
@@ -10,12 +10,12 @@ import org.molgenis.util.Tuple;
 /**
  * Simplified controller that handles a lot of the hard stuff in handleRequest.
  */
-public abstract class EasyPluginController<M extends ScreenModel> extends SimpleScreenController<M>
+public abstract class EasyPluginController<M extends ScreenModel> extends
+		SimpleScreenController<M>
 {
 	private static final long serialVersionUID = 1L;
 
-	public EasyPluginController(String name, M model,
-			ScreenController<?> parent)
+	public EasyPluginController(String name, M model, ScreenController<?> parent)
 	{
 		super(name, model, parent);
 	}
@@ -24,47 +24,62 @@ public abstract class EasyPluginController<M extends ScreenModel> extends Simple
 	public void handleRequest(Database db, Tuple request, PrintWriter out)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	/**
-	 * If a user sends a request it can be handled here.
-	 * Default, it will be automatically mapped to methods based request.getAction();
+	 * If a user sends a request it can be handled here. Default, it will be
+	 * automatically mapped to methods based request.getAction();
 	 */
 	@Override
 	public void handleRequest(Database db, Tuple request)
 	{
-		//automatically calls functions with same name as action
-		delegate(request.getAction(), db, request);		
+		// automatically calls functions with same name as action
+		delegate(request.getAction(), db, request);
 	}
-	
+
 	public void delegate(String action, Database db, Tuple request)
 	{
+		//try/catch for db.rollbackTx
 		try
 		{
-			logger.debug("trying to use reflection to call "
-					+ this.getClass().getName() + "." + action);
-			Method m = this.getClass().getMethod(action, Database.class,
-					Tuple.class);
-			m.invoke(this, db, request);
-			logger.debug("call of " + this.getClass().getName() + "(name="
-					+ this.getName() + ")." + action + " completed");
-		}
-		catch (NoSuchMethodException e1)
-		{
-			this.getModel().setMessages(new ScreenMessage("Unknown action: "+action, false));
-			logger.error("call of " + this.getClass().getName() + "(name="
-					+ this.getName() + ")." + action + "(db,tuple) failed: "
-					+ e1.getMessage());
+			//try/catch for method calling
+			try
+			{
+				db.beginTx();
+				logger.debug("trying to use reflection to call "
+						+ this.getClass().getName() + "." + action);
+				Method m = this.getClass().getMethod(action, Database.class,
+						Tuple.class);
+				m.invoke(this, db, request);
+				logger.debug("call of " + this.getClass().getName() + "(name="
+						+ this.getName() + ")." + action + " completed");
+				db.commitTx();
+			}
+			catch (NoSuchMethodException e1)
+			{
+				this.getModel().setMessages(
+						new ScreenMessage("Unknown action: " + action, false));
+				logger.error("call of " + this.getClass().getName() + "(name="
+						+ this.getName() + ")." + action
+						+ "(db,tuple) failed: " + e1.getMessage());
+				db.rollbackTx();
+			}
+			catch (Exception e)
+			{
+				logger.error("call of " + this.getClass().getName() + "(name="
+						+ this.getName() + ")." + action + " failed: "
+						+ e.getMessage());
+				e.printStackTrace();
+				this.getModel().setMessages(
+						new ScreenMessage(e.getCause().getMessage(), false));
+				db.rollbackTx();
+			}
 		}
 		catch (Exception e)
 		{
-			logger.error("call of " + this.getClass().getName() + "(name="
-					+ this.getName() + ")." + action + " failed: "
-					+ e.getMessage());
 			e.printStackTrace();
-			this.getModel().setMessages(new ScreenMessage(e.getCause().getMessage(), false));
-		}			
+		}
 	}
 
 }
