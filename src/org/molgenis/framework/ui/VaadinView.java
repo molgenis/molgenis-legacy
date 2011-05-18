c946ad25a36c495348d2c051c7e7f674e57d3c42
Joeri van der Velde <joeriv@gmail.com>
2011-05-18 08:41:10 +0000

    Commented out non-compiling code for now
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@7902 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/VaadinView.java
+++ src/org/molgenis/framework/ui/VaadinView.java
@@ -1,54 +1,54 @@
-package org.molgenis.framework.ui;
-
-import org.apache.log4j.Logger;
-
-import com.vaadin.Application;
-
-/** Under development. Idea is this you only need to implement this class and that MOLGENIS than automatically solves all vaadin things for you */
-public abstract class VaadinView extends Application implements ScreenView
-{
-	// wrapper of this VaadinApplication
-	private ScreenModel model;
-	private transient Logger logger = Logger.getLogger(this.getClass());
-	
-	public VaadinView(ScreenModel model)
-	{
-		this.model = model;
-	}
-	
-	@Override
-	public abstract void init();
-	
-	@Override
-	public String getCustomHtmlHeaders()
-	{
-		String divId = this.model.getController().getName();
-		//path of this app; should also work when null (but doesn't)
-		String appPath = "";//"/molgenis_apps/"; 
-		//path with the vaadin app serlvet (including trailing slash)
-		String servletPath = appPath + "vaadin/"+this.model.getController().getName()+"/";
-
-		
-		return "<link rel=\"stylesheet\" style=\"text/css\" type=\"text/css\" href=\""+appPath+"VAADIN/themes/molgenis/styles.css\">" +
-		"<script type=\"text/javascript\">"+
-		"var vaadin = {"+
-		//optionally repeat for each div
-		"	vaadinConfigurations: {"+
-		"		'"+divId+"' :{"+
-		"			appUri:'"+servletPath+"',"+ 
-		"			themeUri: '"+appPath+"VAADIN/themes/molgenis', "+
-		"			versionInfo : {vaadinVersion:\"6.5.2\",applicationVersion:\"NONVERSIONED\"}"+
-		"		}"+
-		"	}};"+
-		"</script>"+
-		"<script language='javascript' src='"+appPath+"VAADIN/widgetsets/com.vaadin.terminal.gwt.DefaultWidgetSet/com.vaadin.terminal.gwt.DefaultWidgetSet.nocache.js'></script>";
-	}
-
-
-
-	@Override
-	public String render()
-	{
-		return new FreemarkerView("VaadinView.ftl",model).render();
-	}
-}
+//package org.molgenis.framework.ui;
+//
+//import org.apache.log4j.Logger;
+//
+//import com.vaadin.Application;
+//
+///** Under development. Idea is this you only need to implement this class and that MOLGENIS than automatically solves all vaadin things for you */
+//public abstract class VaadinView extends Application implements ScreenView
+//{
+//	// wrapper of this VaadinApplication
+//	private ScreenModel model;
+//	private transient Logger logger = Logger.getLogger(this.getClass());
+//	
+//	public VaadinView(ScreenModel model)
+//	{
+//		this.model = model;
+//	}
+//	
+//	@Override
+//	public abstract void init();
+//	
+//	@Override
+//	public String getCustomHtmlHeaders()
+//	{
+//		String divId = this.model.getController().getName();
+//		//path of this app; should also work when null (but doesn't)
+//		String appPath = "";//"/molgenis_apps/"; 
+//		//path with the vaadin app serlvet (including trailing slash)
+//		String servletPath = appPath + "vaadin/"+this.model.getController().getName()+"/";
+//
+//		
+//		return "<link rel=\"stylesheet\" style=\"text/css\" type=\"text/css\" href=\""+appPath+"VAADIN/themes/molgenis/styles.css\">" +
+//		"<script type=\"text/javascript\">"+
+//		"var vaadin = {"+
+//		//optionally repeat for each div
+//		"	vaadinConfigurations: {"+
+//		"		'"+divId+"' :{"+
+//		"			appUri:'"+servletPath+"',"+ 
+//		"			themeUri: '"+appPath+"VAADIN/themes/molgenis', "+
+//		"			versionInfo : {vaadinVersion:\"6.5.2\",applicationVersion:\"NONVERSIONED\"}"+
+//		"		}"+
+//		"	}};"+
+//		"</script>"+
+//		"<script language='javascript' src='"+appPath+"VAADIN/widgetsets/com.vaadin.terminal.gwt.DefaultWidgetSet/com.vaadin.terminal.gwt.DefaultWidgetSet.nocache.js'></script>";
+//	}
+//
+//
+//
+//	@Override
+//	public String render()
+//	{
+//		return new FreemarkerView("VaadinView.ftl",model).render();
+//	}
+//}
