61654e087882712d91e93863830fa3fc402a7d71
Joris Lops <jorislops@gmail.com>
2011-05-20 08:03:42 +0000

    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7911 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/apps/org/molgenis/lifelines/lifelinesUpdateDatabase.java
+++ handwritten/apps/org/molgenis/lifelines/lifelinesUpdateDatabase.java
@@ -3,29 +3,10 @@ package org.molgenis.lifelines;
 
 import org.molgenis.Molgenis;
 
-import app.JDBCDatabase;
-
-
 public class lifelinesUpdateDatabase
 {
 	public static void main(String[] args) throws Exception
 	{
 		new Molgenis("handwritten/apps/org/molgenis/lifelines/lifelines.molgenis.properties").updateDb(true);
-		
-		//loader
-		//TODO: Danny old code ? please remove then
-		/*Database db =*/new JDBCDatabase("handwritten/apps/org/molgenis/lifelines/lifelines.molgenis.properties");
-		
-		//MolgenisUser u = new MolgenisUser();
-		//u.setName("admin");
-		//u.setPassword("admin");
-		//u.setSuperuser(true);
-		//u.setFirstname("Despoina");
-		//u.setLastname("Antonakaki");
-		//u.setEmailaddress("antonakd@gmail.com");
-		
-		//db.add(u);
-		
-		//do batch import
 	}
 }
