6368f3d5b77a4bebcf57f5a0ff5888eb39547d9e
GCC Groningen <gcc-groningen@googlegroups.com>
2011-05-27 07:52:43 +0000

    Added entity ChangeLog . Currently is filled in Biobankdecorator with current date and a test message.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@7963 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/java/decorators/BiobankDecorator.java
+++ handwritten/java/decorators/BiobankDecorator.java
@@ -7,13 +7,19 @@
 
 package decorators;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import org.molgenis.auth.DatabaseLogin;
+import org.molgenis.bbmri.ChangeLog;
+import org.molgenis.framework.db.Database;
 import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.db.Mapper;
 import org.molgenis.framework.db.jdbc.MappingDecorator;
 import org.molgenis.framework.security.Login;
+import org.molgenis.organization.Investigation;
 
 public class BiobankDecorator<E extends org.molgenis.bbmri.BiobankPanel> extends MappingDecorator<E>
 {
@@ -32,6 +38,9 @@ public class BiobankDecorator<E extends org.molgenis.bbmri.BiobankPanel> extends
 	@Override
 	public int add(List<E> entities) throws DatabaseException
 	{
+		org.molgenis.bbmri.ChangeLog chl = null;
+		Date date = new Date(); 
+		
 		//retrieve the user who uploaded/added the record
 		Login login = this.getDatabase().getSecurity();
 		if (login != null) {
@@ -46,6 +55,9 @@ public class BiobankDecorator<E extends org.molgenis.bbmri.BiobankPanel> extends
 			}
 		}
 		
+		
+
+		
 		// here we call the standard 'add'
 		int count = super.add(entities);
 
@@ -58,13 +70,21 @@ public class BiobankDecorator<E extends org.molgenis.bbmri.BiobankPanel> extends
 	@Override
 	public int update(List<E> entities) throws DatabaseException
 	{
-
-		// add your pre-processing here, e.g.
-		// for (org.molgenis.bbmri.BiobankPanel e : entities)
-		// {
-		// 		e.setTriggeredField("Before update called!!!");
-		// }
-
+		Date date = new Date(); 
+		Database db = this.getDatabase();
+		
+		try {
+			
+			ChangeLog changeLog = new ChangeLog();
+			changeLog.setDate(date.toString());
+			changeLog.setEntity("Entity TEST");
+		
+			db.add(changeLog);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		// here we call the standard 'update'
 		int count = super.update(entities);
 
