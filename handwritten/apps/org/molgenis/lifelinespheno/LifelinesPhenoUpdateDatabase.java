08d720449da7675d17c1c140fb42c810f8fc4364
Chao Pang <chaopang229@gmail.com>
2011-06-01 13:34:25 +0000

    I`ve created a life line pheno application.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8010 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/apps/org/molgenis/lifelinespheno/LifelinesPhenoUpdateDatabase.java
@@ -0,0 +1,10 @@
+package org.molgenis.lifelinespheno;
+
+import org.molgenis.Molgenis;
+
+public class LifelinesPhenoUpdateDatabase {
+	public static void main(String[] args) throws Exception
+	{
+		new Molgenis("handwritten/apps/org/molgenis/lifelinespheno/LifelinesPheno.properties").updateDb(true);
+	}
+}
