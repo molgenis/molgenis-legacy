284ead82f3488af0c81be0beaca997c700c47ac9
Robert Wagner <robert.wagner42@googlemail.com>
2011-06-08 08:23:03 +0000

    Added naive ui for export.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8026 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/java/org/molgenis/mutation/ui/export/Export.java
@@ -0,0 +1,35 @@
+
+package org.molgenis.mutation.ui.export;
+
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.ui.FreemarkerView;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.framework.ui.EasyPluginController;
+import org.molgenis.mutation.service.PatientService;
+
+/**
+ * ExportController takes care of all user requests and application logic.
+ *
+ * <li>Each user request is handled by its own method based action=methodName. 
+ * <li> MOLGENIS takes care of db.commits and catches exceptions to show to the user
+ * <li>ExportModel holds application state and business logic on top of domain model. Get it via this.getModel()/setModel(..)
+ * <li>ExportView holds the template to show the layout. Get/set it via this.getView()/setView(..).
+ */
+public class Export extends EasyPluginController<ExportModel>
+{
+	private static final long serialVersionUID = 1L;
+
+	public Export(String name, ScreenController<?> parent)
+	{
+		super(name, null, parent);
+		this.setModel(new ExportModel(this));
+		this.setView(new FreemarkerView("ExportView.ftl", getModel()));
+	}
+
+	@Override
+	public void reload(Database db) throws Exception
+	{	
+		PatientService patientService = PatientService.getInstance(db);
+		this.getModel().setCsv(patientService.exportCsv());
+	}
+}
