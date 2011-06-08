d80e9e7fbf7accfb0da8246d5cf591cca3dd8af8
Robert Wagner <robert.wagner42@googlemail.com>
2011-06-08 08:24:07 +0000

    Added radio buttons for switching between mutation and patient view.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8027 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/java/org/molgenis/mutation/ui/search/form/SimpleSearchForm.java
+++ handwritten/java/org/molgenis/mutation/ui/search/form/SimpleSearchForm.java
@@ -1,9 +1,13 @@
 package org.molgenis.mutation.ui.search.form;
 
+import java.util.Vector;
+
 import org.molgenis.framework.ui.html.ActionInput;
 import org.molgenis.framework.ui.html.Container;
 import org.molgenis.framework.ui.html.HiddenInput;
+import org.molgenis.framework.ui.html.RadioInput;
 import org.molgenis.framework.ui.html.TextLineInput;
+import org.molgenis.util.ValueLabel;
 
 public class SimpleSearchForm extends Container
 {
@@ -13,10 +17,15 @@ public class SimpleSearchForm extends Container
 	public SimpleSearchForm()
 	{
 		this.add(new HiddenInput("__target", ""));
+		this.add(new HiddenInput("select", ""));
 		this.add(new HiddenInput("__action", "findMutationsByTerm"));
 		this.add(new TextLineInput("term"));
 		((TextLineInput) this.get("term")).setClazz("simple_search");
 		((TextLineInput) this.get("term")).setSize(50);
+		Vector<ValueLabel> options = new Vector<ValueLabel>();
+		options.add(new ValueLabel("mutations", "Show mutations"));
+		options.add(new ValueLabel("patients", "Show patients"));
+		this.add(new RadioInput("result", "", "", options, "mutations"));
 		this.add(new ActionInput("findMutationsByTerm"));
 		((ActionInput) this.get("findMutationsByTerm")).setLabel("Search");
 		((ActionInput) this.get("findMutationsByTerm")).setTooltip("Search");
