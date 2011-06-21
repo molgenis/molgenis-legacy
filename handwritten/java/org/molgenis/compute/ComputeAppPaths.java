b715ecbfa16a66805e738d104f3ce137b6f5f853
George Byelas <h.v.byelas@gmail.com>
2011-06-21 13:27:30 +0000

    DatabaseUpdater - to update database during workflow execution, log/output/error files are stored in DB
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8089 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/java/org/molgenis/compute/ComputeAppPaths.java
@@ -0,0 +1,58 @@
+package org.molgenis.compute;
+
+import org.molgenis.protocol.ComputeApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: georgebyelas
+ * Date: 21/06/2011
+ * Time: 10:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComputeAppPaths
+{
+    ComputeApplication application = null;
+    String logpath;
+    String outpath;
+    String errpath;
+
+    public ComputeApplication getApplication()
+    {
+        return application;
+    }
+
+    public void setApplication(ComputeApplication application)
+    {
+        this.application = application;
+    }
+
+    public String getLogpath()
+    {
+        return logpath;
+    }
+
+    public void setLogpath(String logpath)
+    {
+        this.logpath = logpath;
+    }
+
+    public String getOutpath()
+    {
+        return outpath;
+    }
+
+    public void setOutpath(String outpath)
+    {
+        this.outpath = outpath;
+    }
+
+    public String getErrpath()
+    {
+        return errpath;
+    }
+
+    public void setErrpath(String errpath)
+    {
+        this.errpath = errpath;
+    }
+}
