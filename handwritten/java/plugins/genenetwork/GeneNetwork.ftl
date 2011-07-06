5e72c0e63f10680ba2a9c57dee9f571b00cb024b
Joeri van der Velde <joeriv@gmail.com>
2011-07-06 14:16:53 +0000

    Initial bridge to GeneNetwork. Downloading and uploading of data.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8153 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/java/plugins/genenetwork/GeneNetwork.ftl
@@ -0,0 +1,79 @@
+<#macro GeneNetwork screen>
+<!-- normally you make one big form for the whole plugin-->
+<form method="post" enctype="multipart/form-data" name="${screen.name}" action="">
+	<!--needed in every form: to redirect the request to the right screen-->
+	<input type="hidden" name="__target" value="${screen.name}">
+	<!--needed in every form: to define the action. This can be set by the submit button-->
+	<input type="hidden" name="__action">
+	<!--need to be set to "true" in order to force a download-->
+	<input type="hidden" name="__show">
+	
+<!-- this shows a title and border -->
+	<div class="formscreen">
+		<div class="form_header" id="${screen.getName()}">
+		${screen.label}
+		</div>
+		
+		<#--optional: mechanism to show messages-->
+		<#list screen.getMessages() as message>
+			<#if message.success>
+		<p class="successmessage">${message.text}</p>
+			<#else>
+		<p class="errormessage">${message.text}</p>
+			</#if>
+		</#list>
+		
+		<#-- Hack to immediatly clear the message so it doesn't "stick". -->
+		${screen.clearMessage()}
+
+
+<#if screen.myModel?exists>
+	<#assign modelExists = true>
+	<#assign model = screen.myModel>
+<#else>
+	No model. An error has occurred.
+	<#assign modelExists = false>
+</#if>
+
+<table><tr><td>
+
+<label>Request from GeneNetwork:</label><br>
+<br>
+probeset: <input name="probeset" value="${model.probeset}"><br>
+db: <input name="db" value="${model.db}"><br>
+probe: <input name="probe" value="${model.probe}"><br>
+format: <input name="format" value="${model.format}"><br>
+<br><br>
+<input type="submit" value="Download" onclick="__action.value='postInput';return true;"/><br>
+<br>
+
+Result:
+
+<#list model.result as res>
+${res}<br>
+</#list>
+
+</td>
+<td>
+
+<label>Push to GeneNetwork:</label><br>
+<br>
+<input type="file" name="theFile" /><input type="submit" value="Submit" onclick="__action.value='submitFile';return true;"><br>
+
+<br>
+Response:<br>
+
+${model.uploadResponse}
+
+
+</td>
+</tr>
+</table>
+
+
+<#--end of your plugin-->	
+			</div>
+		</div>
+	</div>
+</form>
+</#macro>
---
+++ handwritten/java/plugins/genenetwork/GeneNetwork.java
@@ -0,0 +1,209 @@
+/* Date:        February 2, 2010
+ * Template:	PluginScreenJavaTemplateGen.java.ftl
+ * generator:   org.molgenis.generators.ui.PluginScreenJavaTemplateGen 3.3.2-testing
+ * 
+ * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
+ */
+
+package plugins.genenetwork;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.ui.PluginModel;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.framework.ui.ScreenMessage;
+import org.molgenis.util.Tuple;
+
+public class GeneNetwork extends PluginModel
+{
+	private GeneNetworkModel model = new GeneNetworkModel();
+	String baseurl = "http://www.genenetwork.org/webqtl/main.py";
+
+	public GeneNetworkModel getMyModel()
+	{
+		return model;
+	}
+
+	public GeneNetwork(String name, ScreenController<?> parent)
+	{
+		super(name, parent);
+	}
+
+	@Override
+	public String getViewName()
+	{
+		return "GeneNetwork";
+	}
+
+	@Override
+	public String getViewTemplate()
+	{
+		return "plugins/genenetwork/GeneNetwork.ftl";
+	}
+
+	@Override
+	public void handleRequest(Database db, Tuple request)
+	{
+		System.out.println("*** handleRequest WRAPPER __action: " + request.getString("__action"));
+		this.handleRequest(db, request, null);
+	}
+
+	@Override
+	public boolean isVisible()
+	{
+		return true;
+	}
+
+	public void handleRequest(Database db, Tuple request, PrintWriter out)
+	{
+		if (request.getString("__action") != null)
+		{
+
+			System.out.println("*** handleRequest __action: " + request.getString("__action"));
+			String action = request.getString("__action");
+
+			try
+			{
+				if (action.equals("postInput"))
+				{
+
+					model.setDb(request.getString("db"));
+					model.setFormat(request.getString("format"));
+					model.setProbe(request.getString("probe"));
+					model.setProbeset(request.getString("probeset"));
+
+					ArrayList res = wrapGet(model.getProbeset(), model.getDb(), model.getProbe(), model.getFormat());
+
+					model.setResult(res);
+
+				}
+				if (action.equals("submitFile"))
+				{
+
+					String resp = postData(request.getFile("theFile"));
+					
+					model.setUploadResponse(resp);
+					
+				}
+
+				this.setMessages();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				this.setMessages(new ScreenMessage(e.getMessage() != null ? e.getMessage() : "null", false));
+			}
+		}
+	}
+
+	public void clearMessage()
+	{
+		this.setMessages();
+	}
+
+	@Override
+	public void reload(Database db)
+	{
+
+	}
+
+	ArrayList<String> wrapGet(String probeset, String db, String probe, String format)
+	{
+		ArrayList<String> ret = new ArrayList<String>();
+		String cmd = "?cmd=";
+		cmd += "get&probeset=" + probeset;
+		cmd += "&db=" + db;
+		cmd += "&probe=" + probe;
+		cmd += "&format=" + format;
+		String urlstring = baseurl + cmd;
+		// http://www.genenetwork.org/webqtl/main.py?cmd=get&probeset=98332_at&db=bra08-03MAS5&probe=119637&format=col
+		try
+		{
+			URL url = new URL(urlstring);
+			URLConnection conn = url.openConnection();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null)
+			{
+				ret.add(line);
+				System.out.println("LINE: " + line);
+			}
+			rd.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	private String readFromFile(File theFile) throws IOException
+	{
+		String content = "";
+
+		// Open the file that is the first
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(theFile);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		// Read File Line By Line
+		while ((strLine = br.readLine()) != null)
+		{
+			// Print the content on the console
+			// System.out.println (strLine);
+			content += strLine + "\n";
+		}
+		// Close the input stream
+		in.close();
+
+		return content;
+
+	}
+
+	private String postData(File theFile) throws IOException
+	{
+		String response = "";
+		String data = URLEncoder.encode("RISet", "UTF-8") + "=" + URLEncoder.encode("BXD", "UTF-8");
+		
+		data += "&" + URLEncoder.encode("batchdatafile", "UTF-8") + "=" + URLEncoder.encode(readFromFile(theFile), "UTF-8");
+		data += "&" + URLEncoder.encode("FormID", "UTF-8") + "=" + URLEncoder.encode("batSubmitResult", "UTF-8");
+	//	data += "&" + URLEncoder.encode("Default_Name", "UTF-8") );
+		
+		
+		URL url = new URL(baseurl);
+		URLConnection conn = url.openConnection();
+		conn.setDoOutput(true);
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		wr.write(data);
+		wr.flush();
+
+		// Get the response
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		while ((line = rd.readLine()) != null)
+		{
+			// Process line...
+			System.out.println("RESPONSE: " + line);
+			response += line;
+		}
+		wr.close();
+		rd.close();
+		return response;
+	}
+
+}
