65cbdd255eebb245f579ec1521569d537a601115
Joeri van der Velde <joeriv@gmail.com>
2011-07-12 08:54:35 +0000

    Reordering XGAP package, some renaming, phase 1: adding
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8186 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/apps/org/molgenis/xgap/xqtlworkbench/XqtlGenerate.java
@@ -0,0 +1,40 @@
+package org.molgenis.xgap.xqtlworkbench;
+
+
+import java.io.File;
+
+import org.molgenis.Molgenis;
+import org.molgenis.generators.Generator;
+import org.molgenis.generators.cpp.CPPCassette;
+
+public class XqtlGenerate
+{
+	
+	  static public boolean deleteDirectory(File path) {
+		  if( path.exists() && !path.toString().contains(".svn") ) {
+		      File[] files = path.listFiles();
+		      for(int i=0; i<files.length; i++) {
+		         if(files[i].isDirectory()) {
+		           deleteDirectory(files[i]);
+		         }
+		         else {
+		           files[i].delete();
+		         }
+		      }
+		    }
+		    return( path.delete() );
+		  }
+	  
+	public static void main(String[] args) throws Exception
+	{
+		try
+		{
+			deleteDirectory(new File("hsqldb"));
+			new Molgenis("handwritten/apps/org/molgenis/xgap/xqtlworkbench_standalone/xqtl.properties").generate();
+			
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ handwritten/apps/org/molgenis/xgap/xqtlworkbench/XqtlUpdateDatabase.java
@@ -0,0 +1,13 @@
+package org.molgenis.xgap.xqtlworkbench;
+import java.io.File;
+
+import org.molgenis.Molgenis;
+
+public class XqtlUpdateDatabase
+{
+	public static void main(String[] args) throws Exception
+	{
+		XqtlGenerate.deleteDirectory(new File("hsqldb"));
+		new Molgenis("handwritten/apps/org/molgenis/xgap/xqtlworkbench_standalone/xqtl.properties").updateDb(true);
+	}
+}
