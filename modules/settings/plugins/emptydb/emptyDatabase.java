1c8d0e9373135c07d0ef840c8eaafc1947802546
Joeri van der Velde <joeriv@gmail.com>
2011-07-20 12:15:32 +0000

    Enhancement for emptyDb: now tried to get SQL from JAR if not found on build location
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8316 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/settings/plugins/emptydb/emptyDatabase.java
+++ modules/settings/plugins/emptydb/emptyDatabase.java
@@ -1,6 +1,8 @@
 package plugins.emptydb;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,63 +14,98 @@ import java.sql.Statement;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.db.jdbc.JDBCDatabase;
+import org.molgenis.util.JarClass;
 import org.molgenis.util.cmdline.CmdLineException;
 
-public class emptyDatabase {
-	
+public class emptyDatabase
+{
+
 	/**
-	 * Empties the supplied database and then runs the generated create_tables.sql
-	 * and, if desired, insert_metadata.sql.
+	 * Empties the supplied database and then runs the generated
+	 * create_tables.sql and, if desired, insert_metadata.sql.
 	 * 
 	 * @param db
-	 * @param insertMetadata Run generated insert_metadata.sql or not
+	 * @param insertMetadata
+	 *            Run generated insert_metadata.sql or not
 	 * @throws Exception
 	 */
-	public emptyDatabase(Database db, boolean insertMetadata) throws Exception {
+	public emptyDatabase(Database db, boolean insertMetadata) throws Exception
+	{
+
 		System.out.println("start running create_tables.sql");
-		empty(db, "../../create_tables.sql");
+		try
+		{
+			InputStream fis = this.getClass().getResourceAsStream("../../create_tables.sql");
+			empty(db, fis);
+		}
+		catch (NullPointerException e)
+		{
+			System.out.println("NullPointerException caught - trying to obtain SQL from JAR instead");
+			// maybe we're running in a JAR?
+			// TODO: name of the app is now hardcoded, booo
+			InputStream f = JarClass.getFileFromJARFile("Application.jar", "create_tables.sql");
+			empty(db, f);
+		}
 		System.out.println("done running create_tables.sql");
-		if (insertMetadata == true) {
+
+		if (insertMetadata == true)
+		{
+
 			System.out.println("start running insert_metadata.sql");
-			empty(db, "../../insert_metadata.sql");
+			try
+			{
+				InputStream fis = this.getClass().getResourceAsStream("../../insert_metadata.sql");
+				empty(db, fis);
+			}
+			catch (NullPointerException e)
+			{
+				System.out.println("NullPointerException caught - trying to obtain SQL from JAR instead");
+				// maybe we're running in a JAR?
+				// TODO: name of the app is now hardcoded, booo
+				InputStream f = JarClass.getFileFromJARFile("Application.jar", "insert_metadata.sql");
+				empty(db, f);
+			}
 			System.out.println("done running insert_metadata.sql");
 		}
 	}
 
-
-	private void empty(Database db, String sqlScriptName) throws SQLException, DatabaseException, FileNotFoundException, IOException, CmdLineException {
+	private void empty(Database db, InputStream sqlFile) throws Exception
+	{
 		Connection conn = null;
-		try {
-			InputStream fis = this.getClass().getResourceAsStream(sqlScriptName);
+		try
+		{
 			conn = ((JDBCDatabase) db).getConnection();
 			String create_tables_sql = "";
-			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-				String line;
-				while ((line = in.readLine()) != null) {
-					create_tables_sql += line + "\n";
-				}
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(sqlFile));
+			String line;
+			while ((line = in.readLine()) != null)
+			{
+				create_tables_sql += line + "\n";
 			}
+			in.close();
+
 			Statement stmt = conn.createStatement();
 			int i = 0;
-			for (String command : create_tables_sql.split(";")) {
-				if (command.trim().length() > 0) {
-					try {
-						stmt.executeUpdate(command + ";");
-						if (i++ % 10 == 0){
-							//System.out.print(".");
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
+			for (String command : create_tables_sql.split(";"))
+			{
+				if (command.trim().length() > 0)
+				{
+					stmt.executeUpdate(command + ";");
+					if (i++ % 10 == 0)
+					{
+						// System.out.print(".");
 					}
+
 				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+		}
+		catch (Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
 			conn.close();
 		}
 	}
