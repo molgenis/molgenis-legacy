0f25a8b91d8775a532ba576a0c90142d0020441f
Joeri van der Velde <joeriv@gmail.com>
2011-07-22 10:14:00 +0000

    Decorator cleanup
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8361 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/org/molgenis/xgap/decoratoroverriders/DataDecorator.java
+++ apps/xgap/org/molgenis/xgap/decoratoroverriders/DataDecorator.java
@@ -1,17 +1,10 @@
-/* Date:        February 15, 2011
- * Template:	MapperDecoratorGen.java.ftl
- * generator:   org.molgenis.generators.db.MapperDecoratorGen 3.3.3
- *
- * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
- */
-
 package org.molgenis.xgap.decoratoroverriders;
 
-import org.molgenis.framework.db.jdbc.JDBCMapper;
+import org.molgenis.framework.db.Mapper;
 
 public class DataDecorator<E extends org.molgenis.data.Data> extends decorators.DataDecorator<E>
 {
-	public DataDecorator(JDBCMapper<E> generatedMapper)
+	public DataDecorator(Mapper<E> generatedMapper)
 	{
 		super(generatedMapper);
 		this.strict = true;
--- apps/xgap/org/molgenis/xgap/decoratoroverriders/MolgenisFileDecorator.java
+++ apps/xgap/org/molgenis/xgap/decoratoroverriders/MolgenisFileDecorator.java
@@ -1,18 +1,11 @@
-/* Date:        November 1, 2010
- * Template:	MapperDecoratorGen.java.ftl
- * generator:   org.molgenis.generators.db.MapperDecoratorGen 3.3.3
- *
- * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
- */
-
 package org.molgenis.xgap.decoratoroverriders;
 
 import org.molgenis.core.MolgenisFile;
-import org.molgenis.framework.db.jdbc.JDBCMapper;
+import org.molgenis.framework.db.Mapper;
 
 public class MolgenisFileDecorator<E extends MolgenisFile> extends decorators.MolgenisFileDecorator<E>
 {
-	public MolgenisFileDecorator(JDBCMapper<E> generatedMapper)
+	public MolgenisFileDecorator(Mapper<E> generatedMapper)
 	{
 		super(generatedMapper);
 		this.strict = true;
