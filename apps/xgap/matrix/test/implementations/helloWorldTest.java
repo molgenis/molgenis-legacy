529586ae1fbf3cce4cb27e9164135a26d615834e
Joeri van der Velde <joeriv@gmail.com>
2011-07-26 11:55:15 +0000

    Restored matrix test and made helloworld test
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8376 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ apps/xgap/matrix/test/implementations/helloWorldTest.java
@@ -0,0 +1,15 @@
+package matrix.test.implementations;
+
+import org.testng.annotations.Test;
+
+public class helloWorldTest
+{
+
+	@Test
+	public void testHell()
+	{
+		System.out.println("hello");
+		
+		org.testng.Assert.assertEquals(false, true);
+	}
+}
