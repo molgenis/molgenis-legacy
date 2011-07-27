7cf8234252f16aeaad8a78787c71fda6ff4b562c
Danny Arends <danny.arends@gmail.com>
2011-07-27 13:40:55 +0000

    Added tests for the Web server, Generics and BotNetwork
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8405 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ modules/webserver/tests/TestBot.java
@@ -0,0 +1,23 @@
+package tests;
+
+import ircbot.IRCHandler;
+
+import java.io.File;
+
+import generic.CommandExecutor;
+import generic.JavaCompiler;
+import generic.JavaCompiler.CompileUnit;
+import generic.Utils;
+import generic.FileUtils;
+
+import org.testng.annotations.Test;
+
+public class TestBot {
+	@Test
+	public void testBot(){
+		System.out.println("Molgenis bot Tests");
+		//IRCHandler h = new IRCHandler();
+		//org.testng.Assert.assertEquals(h.connect("testBot",1,IRCHandler.default_channel,true), true);
+	}
+	
+}
---
+++ modules/webserver/tests/TestWebserver.java
@@ -0,0 +1,20 @@
+package tests;
+
+import java.io.File;
+
+import generic.CommandExecutor;
+import generic.JavaCompiler;
+import generic.JavaCompiler.CompileUnit;
+import generic.Utils;
+import generic.FileUtils;
+
+import org.testng.annotations.Test;
+
+public class TestWebserver {
+	@Test
+	public void testWebserver(){
+		System.out.println("Webserver Tests");
+		org.testng.Assert.assertEquals(true, true);
+	}
+	
+}
