1c68e393fdf67da199f582cc97809aef16b6bcbe
Tomasz Adamusiak <tomasz@ebi.ac.uk>
2011-08-09 15:49:00 +0000

    ongoing
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8593 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/phenoflow/loaders/LoadInvestigationFromDirectory.java
+++ apps/phenoflow/loaders/LoadInvestigationFromDirectory.java
@@ -2,19 +2,24 @@ package loaders;
 
 import java.io.File;
 
+import org.molgenis.Molgenis;
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.Database.DatabaseAction;
 import org.molgenis.util.SimpleTuple;
 
 import app.CsvImport;
 import app.JDBCDatabase;
 
-public class LoadInvestigationFromDirectory
-{
-	public static void main(String[] args) throws Exception
-	{
+public class LoadInvestigationFromDirectory {
+	public static void main(String[] args) throws Exception {
 		String directory = "../phenoflow_data/Europhenome2";
 
+		new Molgenis("apps/phenoflow/phenoflow.properties").updateDb(true);
+
 		Database db = new JDBCDatabase("apps/phenoflow/phenoflow.properties");
-		CsvImport.importAll(new File(directory), db, new SimpleTuple());
+
+		CsvImport.importAll(new File(directory), db, new SimpleTuple(), null,
+				DatabaseAction.ADD, "NA");
+		System.out.println("Done");
 	}
 }
