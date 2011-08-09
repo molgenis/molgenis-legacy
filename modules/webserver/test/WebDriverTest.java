062930f4a6b2fa5b5ba08e26953c5487c20f5186
Joeri van der Velde <joeriv@gmail.com>
2011-08-09 17:02:54 +0000

    Proper structure and shutdown for WebDriver test
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8595 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/webserver/test/WebDriverTest.java
+++ modules/webserver/test/WebDriverTest.java
@@ -4,17 +4,32 @@ import junit.framework.Assert;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class WebDriverTest
 {
 
+	WebDriver driver;
+	
+	@BeforeClass
+	public void start() throws InterruptedException
+	{
+		driver = new FirefoxDriver();
+	}
+	
 	@Test
 	public void frontPage() throws InterruptedException
 	{
-		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
+	}
+	
+	@AfterClass
+	public void stop() throws InterruptedException
+	{
+		driver.close();
 	}
 	
 }
