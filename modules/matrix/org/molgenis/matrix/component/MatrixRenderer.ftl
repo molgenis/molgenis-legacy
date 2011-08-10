5fa88923ef9022a771f2643ab1b552cf628327a8
Joeri van der Velde <joeriv@gmail.com>
2011-08-10 15:14:34 +0000

    Made XGAP version of renderable matrix: works (plugin not yet committed)
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8603 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/matrix/org/molgenis/matrix/component/MatrixRenderer.ftl
+++ modules/matrix/org/molgenis/matrix/component/MatrixRenderer.ftl
@@ -1,3 +1,21 @@
 name: ${name}<br>
-value: ${value}<br>
-matrix: ${matrix.toHtml()}
+label: <#if value??>${value}<#else>null</#if><br>
+rows: ${matrix.totalNumberOfRows}
+cols: ${matrix.totalNumberOfCols}
+
+<table>
+	<tr>
+		<td></td>
+		<#list matrix.visibleCols as col>
+			<td>${matrix.renderCol(col)}</td>
+		</#list>
+	</tr>
+	<#list matrix.visibleRows as row>
+		<tr>
+		<td>${matrix.renderRow(row)}</td>
+			<#list matrix.visibleCols as col>
+				<td>${matrix.renderValue(matrix.visibleValues[row_index][col_index])}
+			</#list>
+		</tr>
+	</#list>
+</table>
---
+++ modules/matrix/org/molgenis/matrix/component/XgapRenderableMatrix.java
@@ -0,0 +1,145 @@
+package org.molgenis.matrix.component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import matrix.AbstractDataMatrixInstance;
+import matrix.general.DataMatrixHandler;
+
+import org.molgenis.data.Data;
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.QueryRule;
+import org.molgenis.framework.db.QueryRule.Operator;
+import org.molgenis.pheno.ObservationElement;
+import org.molgenis.pheno.ObservationTarget;
+
+public class XgapRenderableMatrix implements RenderableMatrix<ObservationElement, ObservationElement, Object>{
+	
+	private List<ObservationElement> visibleRows;
+	private List<ObservationElement> visibleCols;
+	private Object[][] visibleValues;
+	private int totalNumberOfRows;
+	private int totalNumberOfCols;
+	private int filteredNumberOfRows;
+	private int filteredNumberOfCols;
+	private int rowIndex;
+	private int colIndex;
+	private List<Filter> filters;
+	private String constraintLogic;
+	
+	public XgapRenderableMatrix(Database db, Data data, DataMatrixHandler dmh) throws Exception {
+		
+		boolean verifiedBackend = false;
+		verifiedBackend = dmh.isDataStoredIn(data, data.getStorage());
+		AbstractDataMatrixInstance<Object> dmi = null;
+		
+		if (verifiedBackend)
+		{
+			dmi = dmh.createInstance(data);
+		}
+		else
+		{
+			throw new Exception("Could not verify existence of data source");
+		}
+		
+		rowIndex = 0;
+		visibleRows = db.find(ObservationElement.class, new QueryRule("name", Operator.IN, dmi.getRowNames()));
+		System.out.println("dmi.getRowNames() = " + dmi.getRowNames());
+		System.out.println("visibleRows size = " + visibleRows.size());
+		totalNumberOfRows = visibleRows.size();
+		
+		colIndex = 0;
+		visibleCols = db.find(ObservationElement.class, new QueryRule("name", Operator.IN, dmi.getColNames()));
+		System.out.println("dmi.getColNames() = " + dmi.getColNames());
+		System.out.println("visibleCols size = " + visibleCols.size());
+		totalNumberOfCols = visibleCols.size();
+		
+		visibleValues = dmi.getElements();
+		
+		// Filter shizzle: TODO
+		filters = new ArrayList<Filter>();
+		filteredNumberOfRows = totalNumberOfRows;
+		filteredNumberOfRows = totalNumberOfCols;
+		constraintLogic = "";
+		
+	}
+
+	
+	@Override
+	public String renderValue(Object value) {
+		if(value == null){
+			return "";
+		}else{
+			return value.toString();
+		}
+	}
+
+	@Override
+	public String renderRow(ObservationElement row) {
+		return row.getName();
+	}
+
+	@Override
+	public String renderCol(ObservationElement col) {
+		return col.getName();
+	}
+
+	@Override
+	public List<ObservationElement> getVisibleRows() {
+		return visibleRows;
+	}
+
+	@Override
+	public List<ObservationElement> getVisibleCols() {
+		return visibleCols;
+	}
+
+	@Override
+	public Object[][] getVisibleValues() {
+		return visibleValues;
+	}
+
+	@Override
+	public int getRowIndex() {
+		return rowIndex;
+	}
+
+	@Override
+	public int getColIndex() {
+		return colIndex;
+	}
+
+	@Override
+	public List<Filter> getFilters() {
+		return filters;
+	}
+
+	@Override
+	public String getConstraintLogic() {
+		return constraintLogic;
+	}
+
+	@Override
+	public int getTotalNumberOfRows() {
+		return totalNumberOfRows;
+	}
+
+	@Override
+	public int getTotalNumberOfCols() {
+		return totalNumberOfCols;
+	}
+
+	@Override
+	public int getFilteredNumberOfRows() {
+		return filteredNumberOfRows;
+	}
+
+	@Override
+	public int getFilteredNumberOfCols() {
+		return filteredNumberOfCols;
+	}
+
+	
+	
+
+}
