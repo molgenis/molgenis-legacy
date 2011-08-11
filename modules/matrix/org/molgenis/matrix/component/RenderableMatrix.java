341000bd0276883703ee81cb0732c871efd65746
Joeri van der Velde <joeriv@gmail.com>
2011-08-11 08:10:05 +0000

    Documented renderable matrix.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8609 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/matrix/org/molgenis/matrix/component/RenderableMatrix.java
+++ modules/matrix/org/molgenis/matrix/component/RenderableMatrix.java
@@ -14,34 +14,81 @@ import java.util.List;
  */
 public interface RenderableMatrix<R, C, V> {
 
+	/**
+	 * Explains the renderer how to visualize your values from the generic type.
+	 */
 	public String renderValue(V value);
 	
+	/**
+	 * Explains the renderer how to visualize your row headers from the generic type.
+	 */
 	public String renderRow(R row);
 	
+	/**
+	 * Explains the renderer how to visualize your column headers from the generic type.
+	 */
 	public String renderCol(C col);
 	
+	/**
+	 * The currently selected rows that are going to be rendered in the view.
+	 */
 	public List<R> getVisibleRows();
 	
+	/**
+	 * The currently selected columns that are going to be rendered in the view.
+	 */
 	public List<C> getVisibleCols();
 	
+	/**
+	 * The currently selected matrix values that are going to be rendered in the view.
+	 */
 	public V[][] getVisibleValues();
 	
+	/**
+	 * The maximum number of rows in this matrix you could possible browse or filter.
+	 */
 	public int getTotalNumberOfRows();
 	
+	/**
+	 * The maximum number of columns in this matrix you could possible browse or filter.
+	 */
 	public int getTotalNumberOfCols();
 
+	/**
+	 * The number of rows in a matrix after it has been potentially filtered.
+	 * Can be less than or equal to getTotalNumberOfRows().
+	 */
 	public int getFilteredNumberOfRows();
 	
+	/**
+	 * The number of columns in a matrix after it has been potentially filtered.
+	 * Can be less than or equal to getTotalNumberOfCols().
+	 */
 	public int getFilteredNumberOfCols();
 	
+	/**
+	 * Tells the renderer the current height offset. For example, if your matrix
+	 * has 10 rows, getVisibleRows has 3 entries, and the current offset is 4,
+	 * the 'human readable' displayed row indices are 5-8. (offset 0 means 1-4 etc)
+	 */
 	public int getRowIndex();
 	
+	/**
+	 * Tells the renderer the current height offset.
+	 */
 	public int getColIndex();
 	
+	/**
+	 * The list of filters that was applied to reach the currently rendered matrix.
+	 * @return
+	 */
 	public List<Filter> getFilters();
 	
+	/**
+	 * The constraint logic that was used to parse the filters in a special way.
+	 * @return
+	 */
 	public String getConstraintLogic();
 	
-	// TODO: add StepSize?
 	
 }
