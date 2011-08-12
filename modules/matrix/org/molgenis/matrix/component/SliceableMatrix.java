8d426bc76caf7e979b53190756f7bfd939e88e90
Joeri van der Velde <joeriv@gmail.com>
2011-08-12 08:20:51 +0000

    Documented sliceable matrix
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@8616 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/matrix/org/molgenis/matrix/component/SliceableMatrix.java
+++ modules/matrix/org/molgenis/matrix/component/SliceableMatrix.java
@@ -4,13 +4,56 @@ import org.molgenis.framework.db.QueryRule;
 
 public interface SliceableMatrix {
 	
-	public RenderableMatrix getSubMatrixByOffset(RenderableMatrix matrix, int rowStart, int rowLength, int colStart, int colLength) throws Exception;
+	/**
+	 * Grab a matrix slice from the data backend by specifying coordinates. Row- and column indices start at 0 for the first element.
+	 * Retrieving nRows 1, nCols 1 from index 0,0 returns the first (upper left most) element of the matrix.
+	 * @param matrix
+	 * @param rowIndex
+	 * @param nRows
+	 * @param colIndex
+	 * @param nCols
+	 * @return
+	 * @throws Exception
+	 */
+	public RenderableMatrix getSubMatrixByOffset(RenderableMatrix matrix, int rowIndex, int nRows, int colIndex, int nCols) throws Exception;
 	
+	/**
+	 * Grab a matrix slice from the data backend by specifying a filter applied to the matrix values in a certain row.
+	 * TODO: example queryrule - steal this from XGAP :)
+	 * @param matrix
+	 * @param q
+	 * @return
+	 * @throws Exception
+	 */
 	public RenderableMatrix getSubMatrixByRowValueFilter(RenderableMatrix matrix, QueryRule q) throws Exception;
 	
+	/**
+	 * Grab a matrix slice from the data backend by specifying a filter applied to one of the row headers.
+	 * TODO: example queryrule - steal this from XGAP :)
+	 * @param matrix
+	 * @param q
+	 * @return
+	 * @throws Exception
+	 */
 	public RenderableMatrix getSubMatrixByRowHeaderFilter(RenderableMatrix matrix, QueryRule q) throws Exception;
 	
+	/**
+	 * Grab a matrix slice from the data backend by specifying a filter applied to the matrix values in a certain column.
+	 * TODO: example queryrule - steal this from XGAP :)
+	 * @param matrix
+	 * @param q
+	 * @return
+	 * @throws Exception
+	 */
 	public RenderableMatrix getSubMatrixByColValueFilter(RenderableMatrix matrix, QueryRule q) throws Exception;
 	
+	/**
+	 * Grab a matrix slice from the data backend by specifying a filter applied to one of the column headers.
+	 *  TODO: example queryrule - steal this from XGAP :)
+	 * @param matrix
+	 * @param q
+	 * @return
+	 * @throws Exception
+	 */
 	public RenderableMatrix getSubMatrixByColHeaderFilter(RenderableMatrix matrix, QueryRule q) throws Exception;
 }
