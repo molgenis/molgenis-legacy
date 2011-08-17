9c929078f8835bc504e284b4226013ff75305906
Robert Wagner <robert.wagner42@googlemail.com>
2011-08-17 14:07:00 +0000

    Changes for AddXrefCommand: post xref entity via rest api, set xref field to point to new entity.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@8662 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/commands/AddXrefCommand.java
+++ src/org/molgenis/framework/ui/commands/AddXrefCommand.java
@@ -3,6 +3,7 @@ package org.molgenis.framework.ui.commands;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.ui.FormController;
@@ -10,8 +11,11 @@ import org.molgenis.framework.ui.FormModel;
 import org.molgenis.framework.ui.ScreenController;
 import org.molgenis.framework.ui.ScreenMessage;
 import org.molgenis.framework.ui.ScreenModel;
+import org.molgenis.framework.ui.html.ActionInput;
 import org.molgenis.framework.ui.html.EntityForm;
+import org.molgenis.framework.ui.html.HiddenInput;
 import org.molgenis.framework.ui.html.HtmlInput;
+import org.molgenis.framework.ui.html.ActionInput.Type;
 import org.molgenis.util.Entity;
 import org.molgenis.util.Tuple;
 
@@ -21,7 +25,7 @@ import org.molgenis.util.Tuple;
 public class AddXrefCommand<E extends Entity> extends AddCommand<E>
 {
 	private static final long serialVersionUID = 1512493344265778285L;
-	private Entity xrefEntity;
+	private E xrefEntity;
 	private EntityForm<?> xrefForm;
 
 	public AddXrefCommand(ScreenController<?>  parent)
@@ -29,7 +33,7 @@ public class AddXrefCommand<E extends Entity> extends AddCommand<E>
 		this("", parent, null, null);
 	}
 
-	public AddXrefCommand(String name, ScreenController<?> parent, Entity xrefEntity, EntityForm<?> xrefForm)
+	public AddXrefCommand(String name, ScreenController<?> parent, E xrefEntity, EntityForm<?> xrefForm)
 	{
 		super(name, parent);
 		this.xrefEntity = xrefEntity;
@@ -42,9 +46,29 @@ public class AddXrefCommand<E extends Entity> extends AddCommand<E>
 	}
 
 	@Override
+	public List<ActionInput> getActions()
+	{
+		List<ActionInput> actions = super.getActions();
+		
+		// Override functionality of standard save button:
+		// postData(): post data via rest api, retrieve object including id back
+		// setXrefOption(): set newly inserted object as current option
+		for (int i = 0; i < actions.size(); i++)
+			if (actions.get(i).getType() == Type.SAVE)
+				actions.get(i).setJavaScriptAction("if( validateForm(molgenis_popup,molgenis_required) ) { if( window.opener.name == '' ){ window.opener.name = 'molgenis'+Math.random();} var entity = postData(document.forms[0].entity_name.value); window.opener.setXrefOption(document.forms[0].__action.value, document.forms[0].id_field.value, document.forms[0].label_field.value, entity); window.close();} else return false;");
+		
+		return actions;
+	}
+
+	@Override
 	public List<HtmlInput<?>> getInputs() throws DatabaseException
 	{
-		return this.xrefForm.getInputs();
+		List<HtmlInput<?>> inputs = this.xrefForm.getInputs();
+		// add two hidden fields for javascript to know id field and label field
+		inputs.add(new HiddenInput("entity_name", StringUtils.uncapitalise(xrefEntity.getClass().getSimpleName())));
+		inputs.add(new HiddenInput("id_field", this.xrefEntity.getIdField()));
+		inputs.add(new HiddenInput("label_field", this.xrefEntity.getLabelFields().get(0)));
+		return inputs;
 	}
 
 	@Override
@@ -65,13 +89,14 @@ public class AddXrefCommand<E extends Entity> extends AddCommand<E>
 				xrefEntity.set(request);
 				int updatedRows = db.add(xrefEntity);
 				db.commitTx();
-				((FormController<?>) this.getController()).getModel().getCurrent().set(this.getName(), this.xrefEntity.getIdValue());
 				msg = new ScreenMessage("ADD SUCCESS: affected " + updatedRows,	null, true);
 			}
 			catch (Exception e)
 			{
-				db.rollbackTx();
 				msg = new ScreenMessage("ADD FAILED: " + e.getMessage(), null, false);
+				e.printStackTrace();
+				if (db.inTx())
+					db.rollbackTx();
 			}
 			((FormController<?>) this.getController()).getModel().getMessages().add(msg);
 		}
