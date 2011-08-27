5f7ab4fb3d628b331b645ac9053a1a02fc3a4089
Morris Swertz <m.a.swertz@rug.nl>
2011-08-27 10:31:45 +0000

    bugfix enuminput/selectinput: constructor SelectInput(name,label) and SelectInput(name,value) where conflicting
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@8784 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/html/SelectInput.java
+++ src/org/molgenis/framework/ui/html/SelectInput.java
@@ -40,11 +40,12 @@ public class SelectInput extends OptionInput<Object>
 		super(name, null);
 	}
 	
-	public SelectInput(String name, String label)
-	{
-		this(name);
-		this.setLabel(label);
-	}
+//  removed! want ambiguous with SelectInput(String name, Object value)	
+//	public SelectInput(String name, String label)
+//	{
+//		this(name);
+//		this.setLabel(label);
+//	}
 
 	public SelectInput(String name, Object value)
 	{
