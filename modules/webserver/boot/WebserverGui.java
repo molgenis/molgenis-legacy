4423fa3a3e414754e9f5457cd27484946326cce8
Joeri van der Velde <joeriv@gmail.com>
2011-09-12 20:36:59 +0000

    Webserver GUI improvements.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9012 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/webserver/boot/WebserverGui.java
+++ modules/webserver/boot/WebserverGui.java
@@ -7,12 +7,14 @@ import ircbot.IRCHandler;
 
 import java.awt.Color;
 import java.awt.Event;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
@@ -23,7 +25,8 @@ import app.servlet.MolgenisServlet;
 import app.servlet.UsedMolgenisOptions;
 import core.WWWServer;
 
-public class WebserverGui extends JFrame implements MouseListener{
+public class WebserverGui extends JFrame implements MouseListener
+{
 	private static final long serialVersionUID = -4617569886547354084L;
 	JTextArea output;
 	WWWServer web;
@@ -32,152 +35,203 @@ public class WebserverGui extends JFrame implements MouseListener{
 	MolgenisOptions usedOptions = new UsedMolgenisOptions();
 	OpenBrowser browser = new OpenBrowser();
 	String variant = MolgenisServlet.getMolgenisVariantID();
-	String title =  variant + " powered by Molgenis Webserver";
+	String title = variant + " powered by Molgenis Webserver";
 	final String url;
-	boolean init=false;
+	boolean init = false;
 
-	WebserverGui(Integer port) throws IOException{
+	public Map<String, Button> buttons = new HashMap<String, Button>();
+
+	WebserverGui(Integer port) throws IOException
+	{
 		WWWServer.DEF_PORT = port;
-		setSize(300,150); 
+		setSize(300, 150);
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    setVisible(true);
-	    addMouseListener(this);
-	    (webserverthread = new Thread(web = new WWWServer(variant))).start();
-	    if(usedOptions.generate_BOT){
-	    	web.setAttribute("bot", botentry);
+		setVisible(true);
+		addMouseListener(this);
+		(webserverthread = new Thread(web = new WWWServer(variant))).start();
+		if (usedOptions.generate_BOT)
+		{
+			web.setAttribute("bot", botentry);
 			new Thread(botentry).start();
-	    }
+		}
 		url = "http://localhost:" + WWWServer.DEF_PORT + "/" + variant;
 		init = true;
 		this.repaint();
 	}
-	
-	public void paint(Graphics g) {
+
+	public void paint(Graphics g)
+	{
 		paintComponent(g);
 	}
-	
-	protected void clear(Graphics g) {
+
+	protected void clear(Graphics g)
+	{
 		super.paintComponents(g);
 	}
-	
-	public void paintComponent(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		
-		g2d.setColor(Color.WHITE);
-		g2d.fillRect(0, 0, getWidth(),getHeight());
-		
-		//CLICK to Molgenis
-		if(init){
-			
-			g2d.setColor(Color.BLACK);
-			g2d.drawString("Please open a browser and go to:",25,50);
-			
-			Button.render(url, 25, 60, g2d);
 
-			//STOP
-			if(webserverthread != null && webserverthread.isAlive()){
-			Button.render("Stop", 90, 85, g2d);	
-			
-			}else{
-			//START
-			Button.render("Start", 130, 85, g2d);
-			}
-			Button.render("Restart", 170, 85, g2d);
-			
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2d = (Graphics2D) g;
+		
+		Font f = new Font(Font.MONOSPACED,Font.BOLD, 12);
+		g2d.setFont(f);
+
+		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+
+		if (init)
+		{
+			buttons.put("url", new Button(url, 25, 60, url.length()*8, g2d));
+			buttons.put("stop", new Button("Stop", 25, 85, 60, g2d));
+			buttons.put("start", new Button("Start", 25, 85, 60, g2d));
+			buttons.put("restart", new Button("Restart", 90, 85, 60, g2d));
+
 			g2d.setColor(Color.BLACK);
-			g2d.drawString("Webserver status:",35,120);
-			
-			if(webserverthread != null && webserverthread.isAlive()){
+			g2d.drawString("Please open a browser and go to:", 25, 48);
+
+			buttons.get("url").render();
+			buttons.get("restart").render();
+
+			if (webserverthread != null && webserverthread.isAlive())
+			{
+				buttons.get("stop").render();
+			}
+			else
+			{
+				buttons.get("start").render();
+			}
+
+			g2d.setColor(Color.BLACK);
+			g2d.drawString("Webserver status:", 25, 130);
+
+			if (webserverthread != null && webserverthread.isAlive())
+			{
 				g2d.setColor(Color.GREEN);
-				g2d.drawString("Online",25,140);
-				
-			}else{
-				g2d.setColor(Color.RED);
-				g2d.drawString("Down",25,140);
-				
+				g2d.fillRect(150, 119, 44, 12);
+				g2d.setColor(Color.BLACK);
+				g2d.drawString("Online", 150, 130);
+
 			}
-		}else{
+			else
+			{
+				g2d.setColor(Color.RED);
+				g2d.fillRect(150, 119, 50, 12);
+				g2d.setColor(Color.BLACK);
+				g2d.drawString("Offline", 150, 130);
+			}
+		}
+		else
+		{
 			g2d.setColor(Color.BLACK);
-			g2d.drawString("Webserver starting, please wait...",25,50);
+			g2d.drawString("Webserver starting, please wait...", 25, 48);
 		}
 	}
-	
-	void startWebServer(){
-	    try {
+
+	void startWebServer()
+	{
+		try
+		{
 			web = new WWWServer(variant);
-		} catch (IOException e1) {
+		}
+		catch (IOException e1)
+		{
 			e1.printStackTrace();
 		}
-	    webserverthread = new Thread(web);
-	    webserverthread.start();
+		webserverthread = new Thread(web);
+		webserverthread.start();
 	}
 
-	void stopWebServer(){
+	void stopWebServer()
+	{
 		web.shutdown();
-		try {
+		try
+		{
 			web.getAcceptor().destroy();
-		} catch (IOException e1) {
+		}
+		catch (IOException e1)
+		{
 			e1.printStackTrace();
 		}
 		web.getThreadPool().setMaxThreads(-1);
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
-		Utils.console(""+e.getX()+" "+e.getY());
-		if(e.getX() > 25 && e.getX() < 225){
-			if(e.getY() > 60 && e.getY() < 80){
-				Utils.console("GOTO Molgenis clicked");
-				browser.openURL(url);
-			}
-		}
-		if(e.getY() > 85 && e.getY() < 105){
-			if(e.getX() > 90 && e.getX() < 125){
-				Utils.console("STOP webserver clicked");
-				if(webserverthread.isAlive()){
-					stopWebServer();
+	public void mouseClicked(MouseEvent e)
+	{
+		Utils.console("" + e.getX() + " " + e.getY());
+
+		for (String key : buttons.keySet())
+		{
+			Button b = buttons.get(key);
+			
+			if (e.getX() > b.getX() && e.getX() < b.getXstop() && e.getY() > b.getY()
+					&& e.getY() < b.getYstop())
+			{
+				if (key.equals("url"))
+				{
+					Utils.console("GOTO Molgenis clicked");
+					browser.openURL(url);
 				}
-				
-			}else if(e.getX() > 130 && e.getX() < 170){
-				Utils.console("START webserver clicked");
-				if(!webserverthread.isAlive()){
+				if (key.equals("start"))
+				{
+					if (!webserverthread.isAlive())
+					{
+						Utils.console("START webserver clicked");
+						startWebServer();
+					}
+				}
+				if (key.equals("stop"))
+				{
+					if (webserverthread.isAlive())
+					{
+						Utils.console("STOP webserver clicked");
+						stopWebServer();
+					}
+				}
+				if (key.equals("restart"))
+				{
+					Utils.console("RESTART webserver clicked");
+					if (webserverthread.isAlive())
+					{
+						stopWebServer();
+					}
 					startWebServer();
 				}
-			}else if(e.getX() > 170 && e.getX() < 220){
-				Utils.console("RESTART webserver clicked");
-				if(webserverthread.isAlive()){
-				    stopWebServer();
-				}
-				startWebServer();
+
 			}
 		}
 		repaint();
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseEntered(MouseEvent e)
+	{
 		repaint();
 	}
-	
+
 	@Override
-	public boolean mouseMove(Event e, int x,int y) {
+	public boolean mouseMove(Event e, int x, int y)
+	{
 		repaint();
 		return true;
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	public void mouseExited(MouseEvent e)
+	{
 		repaint();
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
+	public void mousePressed(MouseEvent e)
+	{
 		repaint();
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void mouseReleased(MouseEvent e)
+	{
 		repaint();
 	}
 
--- modules/webserver/gui/Button.java
+++ modules/webserver/gui/Button.java
@@ -4,12 +4,58 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Button {
-	public static void render(String text, int x, int y,Graphics2D g2d){
+	
+	private String text;
+	private int x;
+	private int y;
+	private Graphics2D g2d;
+	
+	private int xStop;
+	private int yStop;
+	
+	public Button(String text, int x, int y, int length, Graphics2D g2d)
+	{
+		super();
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.g2d = g2d;
+		this.xStop = x + length;
+		this.yStop = y + 22;
+	}
+
+
+
+	public void render(){
 		g2d.setColor(Color.BLACK);
-		g2d.draw3DRect(x, y, (7*text.length())+2, 22, true);
+		g2d.draw3DRect(x, y, xStop-x, yStop-y, true);
 		g2d.setColor(Color.LIGHT_GRAY);
-		g2d.fillRect(x+1, y+1, 7*text.length(), 20);
+		g2d.fillRect(x+1, y+1, xStop-x-2, yStop-y-2);
 		g2d.setColor(Color.BLACK);
 		g2d.drawString(text,x+5,y+15);
 	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public int getXstop()
+	{
+		return xStop;
+	}
+
+	public int getYstop()
+	{
+		return yStop;
+	}
+
+
+
+	
 }
