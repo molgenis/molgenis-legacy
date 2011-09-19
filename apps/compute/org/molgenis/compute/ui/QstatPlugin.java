176a5b77c17d405c96e3b664d2dbe37a3300fb22
Morris Swertz <m.a.swertz@rug.nl>
2011-09-19 12:25:04 +0000

    removed matrix from compute until matrix is stable.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9098 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/compute/org/molgenis/compute/ui/QstatPlugin.java
+++ apps/compute/org/molgenis/compute/ui/QstatPlugin.java
@@ -13,9 +13,6 @@ import org.molgenis.framework.ui.html.ActionInput;
 import org.molgenis.framework.ui.html.DivPanel;
 import org.molgenis.framework.ui.html.MolgenisForm;
 import org.molgenis.framework.ui.html.StringInput;
-import org.molgenis.matrix.MatrixException;
-import org.molgenis.matrix.StringMemoryMatrix;
-import org.molgenis.matrix.ui.StringMatrixView;
 import org.molgenis.util.Pbs;
 import org.molgenis.util.PbsJob;
 import org.molgenis.util.Tuple;
@@ -73,72 +70,73 @@ public class QstatPlugin extends EasyPluginController<QstatPluginModel>
 	{
 		// show inputs for host, username, password
 
-		MolgenisForm f = new MolgenisForm(this.getModel());
+//		MolgenisForm f = new MolgenisForm(this.getModel());
+//
+//		DivPanel panel = new DivPanel("pbs_panel", null);
+//
+//		// create inputs
+//		panel.add(new StringInput("host", this.host));
+//		panel.add(new StringInput("username", this.username));
+//		panel.add(new StringInput("password", this.password));
+//		//this should of course be delegated to the matrix viewer...
+//		panel.add(new StringInput("ownerfilter", this.ownerfilter));
+//
+//		f.add(panel);
+//
+//		// create refresh button
+//		f.add(new ActionInput("refresh"));
+//
+//		// show log, now using primitive matrix
+//		try
+//		{
+//			List<String> rowIds = new ArrayList<String>();
+//			for (PbsJob job : jobs)
+//			{
+//				if (this.ownerfilter != null
+//						&& !job.getOwner().contains(this.ownerfilter))
+//				{
+//					continue;
+//				}
+//				else
+//				{
+//					rowIds.add(job.getId());
+//				}
+//			}
+//			List<String> colIds = Arrays.asList(new String[]
+//			{ "owner", "status", "walltime","queue","mem" });
+			//StringMemoryMatrix matrix = new StringMemoryMatrix(rowIds, colIds);
+//
+//			for (PbsJob job : jobs)
+//			{
+//				if (this.ownerfilter != null
+//						&& !job.getOwner().contains(this.ownerfilter))
+//				{
+//					continue;
+//				}
+//				else
+//				{
+//					matrix.setValue(job.getId(), "owner", job.getOwner());
+//					matrix.setValue(job.getId(), "status", job.getState()
+//							.toString());
+//					matrix.setValue(job.getId(), "walltime", job.getWalltime());
+//					matrix.setValue(job.getId(), "queue", job.getQueue());
+//					matrix.setValue(job.getId(), "mem", job.getMem());
+//				}
+//			}
+//
+//			StringMatrixView v = new StringMatrixView("pbs_matrix", matrix);
+//
+//			f.add(v);
+//
+//		}
+//		catch (MatrixException e)
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
-		DivPanel panel = new DivPanel("pbs_panel", null);
-
-		// create inputs
-		panel.add(new StringInput("host", this.host));
-		panel.add(new StringInput("username", this.username));
-		panel.add(new StringInput("password", this.password));
-		//this should of course be delegated to the matrix viewer...
-		panel.add(new StringInput("ownerfilter", this.ownerfilter));
-
-		f.add(panel);
-
-		// create refresh button
-		f.add(new ActionInput("refresh"));
-
-		// show log, now using primitive matrix
-		try
-		{
-			List<String> rowIds = new ArrayList<String>();
-			for (PbsJob job : jobs)
-			{
-				if (this.ownerfilter != null
-						&& !job.getOwner().contains(this.ownerfilter))
-				{
-					continue;
-				}
-				else
-				{
-					rowIds.add(job.getId());
-				}
-			}
-			List<String> colIds = Arrays.asList(new String[]
-			{ "owner", "status", "walltime","queue","mem" });
-			StringMemoryMatrix matrix = new StringMemoryMatrix(rowIds, colIds);
-
-			for (PbsJob job : jobs)
-			{
-				if (this.ownerfilter != null
-						&& !job.getOwner().contains(this.ownerfilter))
-				{
-					continue;
-				}
-				else
-				{
-					matrix.setValue(job.getId(), "owner", job.getOwner());
-					matrix.setValue(job.getId(), "status", job.getState()
-							.toString());
-					matrix.setValue(job.getId(), "walltime", job.getWalltime());
-					matrix.setValue(job.getId(), "queue", job.getQueue());
-					matrix.setValue(job.getId(), "mem", job.getMem());
-				}
-			}
-
-			StringMatrixView v = new StringMatrixView("pbs_matrix", matrix);
-
-			f.add(v);
-
-		}
-		catch (MatrixException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return f.render();
+//		return f.render();
+			return "TODO";
 	}
 
 	@Override
