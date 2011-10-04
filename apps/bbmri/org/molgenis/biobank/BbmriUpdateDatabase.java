760007521f1db0bc393774040aeeb64153a6662f
Erik Roos <tom.erik.roos@gmail.com>
2011-10-04 10:35:18 +0000

    Another bugfix for BBMRI
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9270 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/bbmri/org/molgenis/biobank/BbmriUpdateDatabase.java
+++ apps/bbmri/org/molgenis/biobank/BbmriUpdateDatabase.java
@@ -4,6 +4,7 @@ package org.molgenis.biobank;
 import org.molgenis.Molgenis;
 import org.molgenis.auth.MolgenisUser;
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.security.SimpleLogin;
 
 import app.DatabaseFactory;
 
@@ -16,15 +17,17 @@ public class BbmriUpdateDatabase
 		
 		Database db = DatabaseFactory.create("apps/bbmri/org/molgenis/biobank/bbmri.molgenis.properties");
 		
-		MolgenisUser u = new MolgenisUser();
-		u.setName("bbmri");
-		u.setPassword("bbmri");
-		u.setSuperuser(true);
-		u.setFirstname("Margreet");
-		u.setLastname(" Brandsma");
-		u.setEmailaddress("m.brandsma@bbmri.nl");
-		
-		db.add(u);
+		// Only add "Margreet Brandsma" user if type of Login allows for this
+		if (!(db.getSecurity() instanceof SimpleLogin)) {
+			MolgenisUser u = new MolgenisUser();
+			u.setName("bbmri");
+			u.setPassword("bbmri");
+			u.setSuperuser(true);
+			u.setFirstname("Margreet");
+			u.setLastname("Brandsma");
+			u.setEmailaddress("m.brandsma@bbmri.nl");
+			db.add(u);
+		}
 		
 		//TODO : do batch import
 	}
