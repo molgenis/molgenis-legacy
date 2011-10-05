662c4b347f9af4bd39c2e70c94c5d818e5c4ebfc
Pieter Neerincx <pieter.neerincx@gmail.com>
2011-10-05 15:33:45 +0000

    Convert VCF to Pheno update (work in progress) for GoNL Variant DB (Commit from Morris & Pi)
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9291 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/patho/org/molgenis/patho/ConvertVcfToPheno.java
+++ apps/patho/org/molgenis/patho/ConvertVcfToPheno.java
@@ -8,6 +8,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
+import org.molgenis.core.OntologyTerm;
+import org.molgenis.pheno.ObservedValue;
 import org.molgenis.util.CsvFileWriter;
 import org.molgenis.util.CsvWriter;
 import org.molgenis.util.Entity;
@@ -23,24 +25,19 @@ import org.molgenis.variant.SequenceVariant;
  * 
  * Currently, only aggregate data is exported as observedvalue.
  */
-public class ConvertVcfToPheno
-{
+public class ConvertVcfToPheno {
 	public static final int BATCH_SIZE = 10000;
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		BasicConfigurator.configure();
 
 		File vcfFile = null, outputDir = null;
 
-		if (args.length == 2)
-		{
+		if (args.length == 2) {
 
-		}
-		else
-		{
-			vcfFile = new File("/Users/mswertz/merged.vcf");
-			outputDir = new File("/Users/mswertz/test");
+		} else {
+			vcfFile = new File("/Volumes/Scratch/gonl_small_debug_variants.vcf");
+			outputDir = new File("/Volumes/Scratch/");
 		}
 
 		ConvertVcfToPheno convert = new ConvertVcfToPheno();
@@ -48,53 +45,59 @@ public class ConvertVcfToPheno
 	}
 
 	public void convertVariants(final File vcfFile, final File outputDir)
-			throws Exception
-	{
+			throws Exception {
 		System.out.println("converting aggregate data from vcf=" + vcfFile
 				+ " to directory " + outputDir);
 		VcfReader vcf = new VcfReader(vcfFile);
 
 		final List<SequenceVariant> variants = new ArrayList<SequenceVariant>();
+		final List<ObservedValue> values = new ArrayList<ObservedValue>();
 		final List<String> chromosomes = new ArrayList<String>();
+		final List<String> dbXrefs = new ArrayList<String>();
 
-		// create filename
-		final File file = new File(outputDir.getAbsolutePath()
+		// create file names
+		final File sequenceVariants = new File(outputDir.getAbsolutePath()
 				+ File.separatorChar + "SequenceVariant.txt");
+		final File observedValues = new File(outputDir.getAbsolutePath()
+				+ File.separatorChar + "ObservedValues.txt");
 
-		final String[] headers = new String[]
-		{ SequenceVariant.NAME, SequenceVariant.CHR_NAME,
-				SequenceVariant.STARTBP, SequenceVariant.REF,
+		// create file headers
+		final String[] svHeaders = new String[] { SequenceVariant.NAME,
+				SequenceVariant.CHR_NAME, SequenceVariant.STARTBP,
+				SequenceVariant.ENDBP, SequenceVariant.REF,
 				SequenceVariant.ALT, SequenceVariant.DESCRIPTION,
 				SequenceVariant.DBREFS_NAME };
+		final String[] ovHeaders = new String[] { ObservedValue.TARGET_NAME,
+				ObservedValue.RELATION_NAME, ObservedValue.FEATURE_NAME, ObservedValue.VALUE };
+
+		// create files
+		createFileAndHeader(sequenceVariants, svHeaders);
+		createFileAndHeader(observedValues, ovHeaders);
 
 		final List<Integer> count = new ArrayList<Integer>();
 		count.add(0);
 
-		create(file, headers);
-		vcf.parse(new VcfReaderListener()
-		{
+		vcf.parse(new VcfReaderListener() {
 
 			@Override
 			public void handleLine(int lineNumber, VcfRecord record)
-					throws Exception
-			{
+					throws Exception {
 				SequenceVariant v = new SequenceVariant();
+				ObservedValue o = new ObservedValue();
+
 				// create hgvs notation
 				// chr1:g.[12345A>T];[12345A>G]
-				if (record.getAlt().size() > 1)
-				{
+				if (record.getAlt().size() > 1) {
 					String result = "chr" + record.getChrom() + ":g.";
-					int  i = 0;
-					for (String alt : record.getAlt())
-					{
-						if(i++ > 0) result += ";";
+					int i = 0;
+					for (String alt : record.getAlt()) {
+						if (i++ > 0)
+							result += ";";
 						result += "[" + record.getPos() + record.getRef() + ">"
 								+ alt + "]";
 					}
 					v.setName(result);
-				}
-				else
-				{
+				} else {
 					v.setName("chr" + record.getChrom() + ":g."
 							+ record.getPos() + record.getRef() + ">"
 							+ record.getAlt().get(0));
@@ -110,28 +113,41 @@ public class ConvertVcfToPheno
 				// chr
 				v.setChr_Name(record.getChrom());
 
-				if (!chromosomes.contains(record.getChrom())) chromosomes
-						.add(record.getChrom());
+				if (!chromosomes.contains(record.getChrom()))
+					chromosomes.add(record.getChrom());
 
 				// pos
 				v.setStartBP(record.getPos());
+				v.setEndBP(record.getPos());
 
 				// dbrefs name
 				if (record.getId().size() > 0
-						&& !".".equals(record.getId().get(0)))
-				{
+						&& !".".equals(record.getId().get(0))) {
 					v.setDbRefs_Name(record.getId());
+					for (String ref : record.getId()) {
+						if (!dbXrefs.contains(ref))
+							dbXrefs.add(ref);
+					}
+
 				}
 
 				// put alt allele counts in description
-				v.setDescription(record.getInfo("AC").toString());
+				o.setValue(record.getInfo("AC").toString());
+				// TODO: fetch panel and relation from VCF if possible...
+				// and create it first, so we can use it here.
+				o.setTarget_Name("goNL");
+				o.setRelation_Name("Allele count");
+				o.setFeature_Name(v.getName());
 
 				variants.add(v);
+				values.add(o);
 
-				if (variants.size() == BATCH_SIZE)
-				{
-					writeBatch(variants, file, headers);
+				if (variants.size() == BATCH_SIZE) {
+					writeBatch(variants, sequenceVariants, svHeaders);
 					variants.clear();
+					writeBatch(values, observedValues, ovHeaders);
+					values.clear();
+
 					count.set(0, count.get(0) + BATCH_SIZE);
 
 					System.out.println(new Date() + ":" + count.get(0));
@@ -139,57 +155,64 @@ public class ConvertVcfToPheno
 			}
 		});
 
-		// write remaining
-		writeBatch(variants, file, headers);
+		// write remaining data for last batch.
+		writeBatch(variants, sequenceVariants, svHeaders);
+		writeBatch(values, observedValues, ovHeaders);
 
 		// write chromsomes
 		List<Chromosome> chrList = new ArrayList<Chromosome>();
-		for (String chr : chromosomes)
-		{
+		for (String chr : chromosomes) {
 			Chromosome c = new Chromosome();
 			c.setName(chr);
 			chrList.add(c);
 		}
 
+		// write dbXrefs
+		List<OntologyTerm> ontoList = new ArrayList<OntologyTerm>();
+		for (String dbXref : dbXrefs) {
+			OntologyTerm t = new OntologyTerm();
+			t.setName(dbXref);
+			ontoList.add(t);
+		}
+
 		File chrFile = new File(outputDir.getAbsolutePath()
-				+ File.separatorChar + "chromosome.txt");
-		String[] chrHeader = new String[]
-		{ "name" };
-		create(chrFile, chrHeader);
+				+ File.separatorChar + "Chromosome.txt");
+		String[] chrHeader = new String[] { "name" };
+		createFileAndHeader(chrFile, chrHeader);
 		writeBatch(chrList, chrFile, chrHeader);
+
+		File ontoFile = new File(outputDir.getAbsolutePath()
+				+ File.separatorChar + "OntologyTerm.txt");
+		String[] ontoHeader = new String[] { "name" };
+		createFileAndHeader(ontoFile, ontoHeader);
+		writeBatch(ontoList, ontoFile, ontoHeader);
+
 	}
 
-	private String toCsv(List<String> values)
-	{
+	private String toCsv(List<String> values) {
 		String result = "";
 		boolean first = true;
-		for (String val : values)
-		{
-			if (first)
-			{
+		for (String val : values) {
+			if (first) {
 				result += val;
 				first = false;
-			}
-			else
-			{
+			} else {
 				result += "," + val;
 			}
 		}
 		return result;
 	}
 
-	private void create(File file, String[] fields) throws IOException
-	{
+	private void createFileAndHeader(File file, String[] fields)
+			throws IOException {
 		CsvWriter writer = new CsvFileWriter(file, Arrays.asList(fields));
 		writer.writeHeader();
 		writer.close();
 	}
 
 	private void writeBatch(List<? extends Entity> entities, File file,
-			String[] fields) throws IOException
-	{
-		if (entities.size() > 0)
-		{
+			String[] fields) throws IOException {
+		if (entities.size() > 0) {
 			System.out.println("Writing to " + file);
 
 			// create appending csvWriter using the selected headers
@@ -197,8 +220,7 @@ public class ConvertVcfToPheno
 					true);
 
 			// write batch to csv
-			for (Entity e : entities)
-			{
+			for (Entity e : entities) {
 				writer.writeRow(e);
 			}
 
