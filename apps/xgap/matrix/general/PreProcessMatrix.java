aebef79c6c1306227aa9a8a24d2b6f3456ce04a4
Joeri van der Velde <joeriv@gmail.com>
2011-10-10 20:33:34 +0000

    Implemented a number of csv-matrix-data preprocessing tools that should help avoid painstaking reformatting work.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9337 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ apps/xgap/matrix/general/PreProcessMatrix.java
@@ -0,0 +1,108 @@
+package matrix.general;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.molgenis.util.CsvFileReader;
+import org.molgenis.util.CsvFileWriter;
+import org.molgenis.util.CsvReaderListener;
+import org.molgenis.util.Tuple;
+
+import decorators.NameConvention;
+
+public class PreProcessMatrix
+{
+
+	private List<String> colNames;
+	private List<String> rowNames;
+	private Object[][] elements;
+
+	public PreProcessMatrix(File in) throws Exception
+	{
+		CsvFileReader reader = new CsvFileReader(in);
+		this.colNames = reader.colnames();
+		if(this.colNames.get(0).isEmpty()) this.colNames.remove(0);
+		this.rowNames = reader.rownames();
+		this.elements = getElementsFromCsv(in, rowNames.size(), colNames.size());
+		reader.close();
+	}
+
+	public void prependUnderscoreToRowNames() throws Exception
+	{
+		ArrayList<String> newRowNames = new ArrayList<String>();
+		for (String s : this.rowNames)
+		{
+			newRowNames.add("_"+s);
+		}
+		this.rowNames = newRowNames;
+	}
+
+	public void prependUnderscoreToColNames() throws Exception
+	{
+		ArrayList<String> newColNames = new ArrayList<String>();
+		for (String s : this.colNames)
+		{
+			newColNames.add("_"+s);
+		}
+		this.colNames = newColNames;
+	}
+
+	public void escapeRowNames() throws Exception
+	{
+		ArrayList<String> newRowNames = new ArrayList<String>();
+		for (String s : this.rowNames)
+		{
+			newRowNames.add(NameConvention.escapeEntityNameStrict(s));
+		}
+		this.rowNames = newRowNames;
+	}
+
+	public void escapeColNames() throws Exception
+	{
+		ArrayList<String> newColNames = new ArrayList<String>();
+		for (String s : this.colNames)
+		{
+			newColNames.add(NameConvention.escapeEntityNameStrict(s));
+			this.colNames = newColNames;
+		}
+	}
+
+	public File getResult() throws IOException
+	{
+		return writeOutMatrix(this.rowNames, this.colNames, this.elements);
+	}
+
+	private File writeOutMatrix(List<String> newRowNames, List<String> newColNames, Object[][] newElements)
+			throws IOException
+	{
+		File out = new File(System.getProperty("java.io.tmpdir") + File.separator + "tmpMatrix" + System.nanoTime()
+				+ ".txt");
+		CsvFileWriter writer = new CsvFileWriter(out);
+		writer.writeMatrix(newRowNames, newColNames, newElements);
+		writer.close();
+		return out;
+	}
+
+	private Object[][] getElementsFromCsv(File in, int nRow, int nCol) throws FileNotFoundException, Exception
+	{
+		final Object[][] elements = new Object[nRow][nCol];
+		new CsvFileReader(in).parse(new CsvReaderListener()
+		{
+			public void handleLine(int line_number, Tuple line) throws Exception
+			{
+				if (line_number != 0)
+				{
+					for (int columnIndex = 1; columnIndex < line.size(); columnIndex++)
+					{
+						elements[line_number-1][columnIndex-1] = line.getObject(columnIndex);
+					}
+				}
+			}
+		});
+		return elements;
+	}
+
+}
---
+++ apps/xgap/matrix/general/TestPreProcessMatrix.java
@@ -0,0 +1,128 @@
+package matrix.general;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.molgenis.util.CsvFileReader;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestPreProcessMatrix
+{
+
+	File testMatrix;
+
+	@BeforeClass
+	public void setup() throws Exception
+	{
+		String testContent = "\tcol1$\t456col2\tcol3\n";
+		testContent += "234row1\tval1\tval2\tval3\n";
+		testContent += "&row2\tval4\tval5\tval6\n";
+		testContent += "678row3#\tval7\tval8\tval9\n";
+		File testMatrix = new File(System.getProperty("java.io.tmpdir") + File.separator + "testMatrix"
+				+ System.nanoTime() + ".txt");
+		System.out.println("testMatrix @ " + testMatrix.getAbsolutePath());
+		BufferedWriter out = new BufferedWriter(new FileWriter(testMatrix));
+		out.write(testContent);
+		out.close();
+		this.testMatrix = testMatrix;
+	}
+
+	@Test
+	public void testPrependUnderscoreToRowNames() throws Exception
+	{
+		PreProcessMatrix pm = new PreProcessMatrix(this.testMatrix);
+		pm.prependUnderscoreToRowNames();
+		File result = pm.getResult();
+		System.out.println("ACTUAL @ " + result.getAbsolutePath());
+		String actual = readFile(result);
+		System.out.println("ACTUAL = " + actual);
+		String expected = "\tcol1$\t456col2\tcol3\n";
+		expected += "_234row1\tval1\tval2\tval3\n";
+		expected += "_&row2\tval4\tval5\tval6\n";
+		expected += "_678row3#\tval7\tval8\tval9\n";
+		Assert.assertEquals(actual, expected);
+	}
+
+	@Test
+	public void testPrependUnderscoreToColNames() throws Exception
+	{
+		PreProcessMatrix pm = new PreProcessMatrix(this.testMatrix);
+		pm.prependUnderscoreToColNames();
+		File result = pm.getResult();
+		String actual = readFile(result);
+		String expected = "\t_col1$\t_456col2\t_col3\n";
+		expected += "234row1\tval1\tval2\tval3\n";
+		expected += "&row2\tval4\tval5\tval6\n";
+		expected += "678row3#\tval7\tval8\tval9\n";
+		Assert.assertEquals(actual, expected);
+	}
+
+	@Test
+	public void testEscapeRowNames() throws Exception
+	{
+		PreProcessMatrix pm = new PreProcessMatrix(this.testMatrix);
+		pm.escapeRowNames();
+		File result = pm.getResult();
+		String actual = readFile(result);
+		String expected = "\tcol1$\t456col2\tcol3\n";
+		expected += "row1\tval1\tval2\tval3\n";
+		expected += "row2\tval4\tval5\tval6\n";
+		expected += "row3\tval7\tval8\tval9\n";
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testEscapeColNames() throws Exception
+	{
+		PreProcessMatrix pm = new PreProcessMatrix(this.testMatrix);
+		pm.escapeColNames();
+		File result = pm.getResult();
+		String actual = readFile(result);
+		String expected = "\tcol1\tcol2\tcol3\n";
+		expected += "234row1\tval1\tval2\tval3\n";
+		expected += "&row2\tval4\tval5\tval6\n";
+		expected += "678row3#\tval7\tval8\tval9\n";
+		Assert.assertEquals(actual, expected);
+	}
+
+	@Test
+	public void testPrependAndEscapeAll() throws Exception
+	{
+		PreProcessMatrix pm = new PreProcessMatrix(this.testMatrix);
+		pm.prependUnderscoreToRowNames();
+		pm.prependUnderscoreToColNames();
+		pm.escapeRowNames();
+		pm.escapeColNames();
+		File result = pm.getResult();
+		String actual = readFile(result);
+		String expected = "\t_col1\t_456col2\t_col3\n";
+		expected += "_234row1\tval1\tval2\tval3\n";
+		expected += "_row2\tval4\tval5\tval6\n";
+		expected += "_678row3\tval7\tval8\tval9\n";
+		Assert.assertEquals(actual, expected);
+	}
+
+	private String readFile(File theFile) throws java.io.IOException
+	{
+		StringBuffer fileData = new StringBuffer(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(theFile));
+		char[] buf = new char[1024];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1)
+		{
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		reader.close();
+		return fileData.toString();
+	}
+
+}
