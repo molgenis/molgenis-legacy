15e98aaf95f6ace69fd47bcf45b8228e2cb62bc7
Morris Swertz <m.a.swertz@rug.nl>
2011-10-12 06:31:32 +0000

    added servlet.init to make sure log4j is properly loaded.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@9366 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/AbstractMolgenisServlet.java
+++ src/org/molgenis/framework/server/AbstractMolgenisServlet.java
@@ -21,6 +21,8 @@ import java.util.Locale;
 import java.util.Vector;
 
 import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,7 +33,9 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.molgenis.MolgenisOptions;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.db.DatabaseException;
@@ -133,6 +137,30 @@ public abstract class AbstractMolgenisServlet extends CXFNonSpringServlet
 	{
 		newWindowId++;
 		return newWindowId;
+	}
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Log4JInitServlet is initializing log4j");
+		String log4jLocation = config.getInitParameter("log4j-properties-location");
+
+		ServletContext sc = config.getServletContext();
+
+		if (log4jLocation == null) {
+			System.err.println("*** No log4j-properties-location init param, so initializing log4j with BasicConfigurator");
+			BasicConfigurator.configure();
+		} else {
+			String webAppPath = sc.getRealPath("/");
+			String log4jProp = webAppPath + log4jLocation;
+			File yoMamaYesThisSaysYoMama = new File(log4jProp);
+			if (yoMamaYesThisSaysYoMama.exists()) {
+				System.out.println("Initializing log4j with: " + log4jProp);
+				PropertyConfigurator.configure(log4jProp);
+			} else {
+				System.err.println("*** " + log4jProp + " file not found, so initializing log4j with BasicConfigurator");
+				BasicConfigurator.configure();
+			}
+		}
+		super.init(config);
 	}
 
 	@Override
