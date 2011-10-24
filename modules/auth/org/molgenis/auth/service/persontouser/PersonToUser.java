36603a9c3b6fdcd43ca781955126e033b3b32012
Joeri van der Velde <joeriv@gmail.com>
2011-10-24 07:15:55 +0000

    Added a plugin that allows you (probably admin) to transform a Person into a MolgenisUser. This means users can add their friends which can then be upgraded to full users by admin in one click.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9418 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ modules/auth/org/molgenis/auth/service/persontouser/PersonToUser.java
@@ -0,0 +1,147 @@
+/* Date:        February 2, 2010
+ * Template:	PluginScreenJavaTemplateGen.java.ftl
+ * generator:   org.molgenis.generators.ui.PluginScreenJavaTemplateGen 3.3.2-testing
+ * 
+ * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
+ */
+
+package org.molgenis.auth.service.persontouser;
+
+import java.util.List;
+
+import org.molgenis.auth.MolgenisGroup;
+import org.molgenis.auth.MolgenisRoleGroupLink;
+import org.molgenis.auth.MolgenisUser;
+import org.molgenis.auth.Person;
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.DatabaseException;
+import org.molgenis.framework.db.QueryRule;
+import org.molgenis.framework.db.QueryRule.Operator;
+import org.molgenis.framework.ui.PluginModel;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.framework.ui.ScreenMessage;
+import org.molgenis.util.Tuple;
+
+public class PersonToUser extends PluginModel
+{
+	private PersonToUserModel model = new PersonToUserModel();
+	public PersonToUserModel getMyModel()
+	{
+		return model;
+	}
+
+	public PersonToUser(String name, ScreenController<?> parent)
+	{
+		super(name, parent);
+	}
+
+	@Override
+	public String getViewName()
+	{
+		return "PersonToUser";
+	}
+
+	@Override
+	public String getViewTemplate()
+	{
+		return "org/molgenis/auth/service/persontouser/PersonToUser.ftl";
+	}
+
+	public void handleRequest(Database db, Tuple request)
+	{
+		if (request.getString("__action") != null)
+		{
+
+			System.out.println("*** handleRequest __action: " + request.getString("__action"));
+			String action = request.getString("__action");
+
+			try
+			{
+				if (action.equals("upgrade"))
+				{
+					//get person id
+					int pid = request.getInt("personId");
+					
+					//get group id if possible
+					int gid = -1;
+					if(request.getInt("groupId") != null)
+					{
+						gid = request.getInt("groupId");
+					}
+					
+					//get the person from db
+					Person p = db.find(Person.class, new QueryRule(Person.ID, Operator.EQUALS, pid)).get(0);
+					
+					//copy properties except ID and __Type, plus 'active' and 'password'
+					MolgenisUser mu = new MolgenisUser();
+					for(String f : p.getFields(true))
+					{
+						if(!f.equals(Person.__TYPE))
+						{
+							mu.set(f, p.get(f));
+						}
+						
+					}
+					mu.setActive(true);
+					mu.setPassword("changeme");
+					
+					//remove person
+					db.remove(p);
+					
+					//add as user
+					db.add(mu);
+					
+					//put user in the group if applicable
+					if(gid != -1)
+					{
+						MolgenisGroup mg = db.find(MolgenisGroup.class, new QueryRule(MolgenisGroup.ID, Operator.EQUALS, gid)).get(0);
+						System.out.println("** mg = " + mg.getName());
+						MolgenisRoleGroupLink mr = new MolgenisRoleGroupLink();
+						mr.setRole(mu);
+						mr.setGroup(mg);
+						db.add(mr);
+					}
+					
+					//great success
+					this.setMessages(new ScreenMessage("The selected person is now a user with password 'changeme'.", true));
+
+				}
+
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				this.setMessages(new ScreenMessage(e.getMessage() != null ? e.getMessage() : "null", false));
+			}
+		}
+	}
+
+
+
+	@Override
+	public void reload(Database db)
+	{
+
+		List<Person> personList;
+		List<MolgenisGroup> groupList;
+		
+		try
+		{
+			personList = db.find(Person.class, new QueryRule(Person.__TYPE, Operator.EQUALS, "Person"));
+			this.model.setPersonList(personList);
+			groupList = db.find(MolgenisGroup.class);
+			this.model.setGroupList(groupList);
+			
+		}
+		catch (DatabaseException e)
+		{
+			e.printStackTrace();
+			this.setMessages(new ScreenMessage(e.getMessage() != null ? e.getMessage() : "null", false));
+		}
+		
+		
+	}
+
+	
+
+}
