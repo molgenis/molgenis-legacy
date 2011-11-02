c3cf9580665b471eac64a54377ac7ccd6f18b7f8
Joeri van der Velde <joeriv@gmail.com>
2011-11-02 13:20:41 +0000

    Changes and additions to FrontController handlers
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@9515 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/MolgenisRequest.java
+++ src/org/molgenis/framework/server/MolgenisRequest.java
@@ -1,5 +1,6 @@
 package org.molgenis.framework.server;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.molgenis.framework.db.Database;
@@ -8,8 +9,9 @@ import org.molgenis.util.HttpServletRequestTuple;
 public class MolgenisRequest extends HttpServletRequestTuple
 {
 	Database database;
+	ServletContext sc;
 	
-	public MolgenisRequest(Database database, HttpServletRequest request) throws Exception
+	public MolgenisRequest(Database database, HttpServletRequest request, ServletContext sc) throws Exception
 	{
 		super(request);
 		this.database = database;
@@ -18,6 +20,16 @@ public class MolgenisRequest extends HttpServletRequestTuple
 	public Database getDatabase()
 	{
 		return database;
+	}
+
+	public String getRequestURI()
+	{
+		return getRequest().getRequestURI();
+	}
+
+	public ServletContext getServletContext()
+	{
+		return sc;
 	}
 
 }
--- src/org/molgenis/framework/server/MolgenisResponse.java
+++ src/org/molgenis/framework/server/MolgenisResponse.java
@@ -1,6 +1,7 @@
 package org.molgenis.framework.server;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
@@ -23,7 +24,37 @@ public class MolgenisResponse
 	{
 		return response;
 	}
-	
+
+	public OutputStream getOutputStream() throws IOException
+	{
+		return response.getOutputStream();
+	}
+
+	public void setStatus(int status)
+	{
+		response.setStatus(status);
+		
+	}
+
+	public void setContentLength(int length)
+	{
+		response.setContentLength(length);
+		
+	}
+
+	public void setContentType(String string)
+	{
+		response.setContentType(string);
+		
+	}
+
+	public void setHeader(String string, String string2)
+	{
+		response.setHeader(string, string2);
+		
+	}
+
+
 	
 
 }
