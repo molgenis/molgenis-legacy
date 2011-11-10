0b1e6baad16615977ba3a3fb468f6d3e96883a58
Erik Roos <tom.erik.roos@gmail.com>
2011-11-10 15:47:57 +0000

    Target selection in ApplyProtocol now with Matrix! Simplified Matrix paging.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@9639 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ src/org/molgenis/framework/ui/html/HorizontalRuler.java
@@ -0,0 +1,16 @@
+package org.molgenis.framework.ui.html;
+
+public class HorizontalRuler extends HtmlInput<String>
+{
+	
+	public HorizontalRuler() {
+		this.setLabel("");
+	}
+
+	@Override
+	public String toHtml()
+	{
+		return "<hr/>";
+	}
+
+}
