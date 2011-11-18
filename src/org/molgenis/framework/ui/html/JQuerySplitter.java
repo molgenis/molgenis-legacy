ad957caa9c99ccf1611157cfb8379bf0f435d238
GCC Groningen <gcc-groningen@googlegroups.com>
2011-11-18 13:16:02 +0000

    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@9730 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/html/JQuerySplitter.java
+++ src/org/molgenis/framework/ui/html/JQuerySplitter.java
@@ -1,33 +1,52 @@
 package org.molgenis.framework.ui.html;
 
+import java.util.Vector;
+
+import org.molgenis.util.Tree;
+
 public class JQuerySplitter<E> extends HtmlWidget
 {
-	private String contents;
+	private String splitterName;
+	private JQuerySplitterContents splitterContents;
 
-	public JQuerySplitter(String name)
+	public JQuerySplitter(String name, JQuerySplitterContents contents)
 	{
 		super(name);
+		this.setSplitterContents(new JQuerySplitterContents());
+		this.setSplitterContents(contents);
+		this.setSplitterName(name);
 	}
 	
-	private String renderSplitter(String contents) {
+
+	private String renderSplitter(JQuerySplitterContents contents) {
 		String returnString = "";
-		
+
+			returnString = "<p>" + this.getSplitterName() + "</p>";
+			returnString += "test>>>>>>>>>>" ; 
+			returnString += contents; 
+
 		return returnString;
 	}
+	
+	
 	
 	@Override
 	public String toHtml(){
 		
 		String html = "<ul id=\"browser\" class=\"pointtree\">";
-		html += renderSplitter(contents);
 		html += "</ul>";
 	    html += "<script src=\"res/jquery-plugins/datatables/js/jquery.js\"></script>\n"
 		+ "<link rel=\"stylesheet\" href=\"res/jquery-plugins/splitter/jquery.spliter.css\" type=\"text/css\" media=\"screen\" />\n" 
 		+ "<script src=\"res/jquery-plugins/splitter/splitter.js\" language=\"javascript\"></script>"
 		+ " <style type=\"text/css\">\n"
 		+ "$(document).ready(function() {"
-		+ "$(\"#splitterContainer\").splitter({minAsize:100,maxAsize:300,splitVertical:true,A:$('#leftPane'),B:$('#rightPane'),slave:$(\"#rightSplitterContainer\"),closeableto:0});"
-		+ "$(\"#rightSplitterContainer\").splitter({splitHorizontal:true,A:$('#rightTopPane'),B:$('#rightBottomPane'),closeableto:100});"
+		+ "$(\"#splitterContainer\").splitter({minAsize:100,maxAsize:300,splitVertical:true,A:" 
+		+ "$('#leftPane'),B:" 
+		+ "$('#rightPane'),slave:" 
+		+ "$(\"#rightSplitterContainer\"),closeableto:0});"
+		+ "$(\"#rightSplitterContainer\").splitter({splitHorizontal:true,A:" 
+		+ "$('#rightTopPane'),B:" 
+		+ "$('#rightBottomPane'),closeableto:100});"
 		+ "});"
 		+ "<div id=\"splitterContainer\">"
 		+ "	<div id=\"leftPane\">"
@@ -51,6 +70,30 @@ public class JQuerySplitter<E> extends HtmlWidget
 
 		
 	    return html;
+	}
+
+
+	public void setSplitterContents(JQuerySplitterContents splitterContents)
+	{
+		this.splitterContents = splitterContents;
+	}
+
+
+	public JQuerySplitterContents getSplitterContents()
+	{
+		return splitterContents;
+	}
+
+
+	public void setSplitterName(String splitterName)
+	{
+		this.splitterName = splitterName;
+	}
+
+
+	public String getSplitterName()
+	{
+		return splitterName;
 	}
 }
 
