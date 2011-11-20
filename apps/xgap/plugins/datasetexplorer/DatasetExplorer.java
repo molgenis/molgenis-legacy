5c9c027699c155181c5a3e0de0f9da7d47b48505
Joeri van der Velde <joeriv@gmail.com>
2011-11-20 00:10:13 +0000

    Placeholder for the 'Dataset Explorer' tool.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9740 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ apps/xgap/plugins/datasetexplorer/DatasetExplorer.java
@@ -0,0 +1,119 @@
+/* Date:        February 2, 2010
+ * Template:	PluginScreenJavaTemplateGen.java.ftl
+ * generator:   org.molgenis.generators.ui.PluginScreenJavaTemplateGen 3.3.2-testing
+ * 
+ * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
+ */
+
+package plugins.datasetexplorer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Vector;
+
+import matrix.DataMatrixInstance;
+import matrix.general.DataMatrixHandler;
+
+import org.molgenis.data.Data;
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.QueryRule;
+import org.molgenis.framework.db.QueryRule.Operator;
+import org.molgenis.framework.ui.PluginModel;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.framework.ui.ScreenMessage;
+import org.molgenis.pheno.ObservableFeature;
+import org.molgenis.pheno.ObservationElement;
+import org.molgenis.pheno.ObservationTarget;
+import org.molgenis.util.Entity;
+import org.molgenis.util.Tuple;
+
+import plugins.rplot.MakeRPlot;
+import app.JDBCMetaDatabase;
+
+public class DatasetExplorer extends PluginModel
+{
+
+	private DatasetExplorerModel model = new DatasetExplorerModel();
+	private DataMatrixHandler dmh = null;
+
+	public DatasetExplorerModel getMyModel()
+	{
+		return model;
+	}
+
+	public DatasetExplorer(String name, ScreenController<?> parent)
+	{
+		super(name, parent);
+	}
+
+	@Override
+	public String getViewName()
+	{
+		return "DatasetExplorer";
+	}
+
+	@Override
+	public String getViewTemplate()
+	{
+		return "plugins/datasetexplorer/DatasetExplorer.ftl";
+	}
+
+	public void handleRequest(Database db, Tuple request)
+	{
+		if (request.getString("__action") != null)
+		{
+
+			System.out.println("*** handleRequest __action: " + request.getString("__action"));
+			String action = request.getString("__action");
+
+			try
+			{
+
+				if (action.equals("buildReport"))
+				{
+				
+					
+				}
+				else if(action.startsWith("disambig_"))
+				{
+				
+				}
+
+				this.setMessages();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				this.setMessages(new ScreenMessage(e.getMessage() != null ? e.getMessage() : "null", false));
+			}
+		}
+	}
+
+	
+
+
+
+	@Override
+	public void reload(Database db)
+	{
+
+		try
+		{
+			
+			
+			
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			this.setMessages(new ScreenMessage(e.getMessage() != null ? e.getMessage() : "null", false));
+		}
+
+	}
+
+}
