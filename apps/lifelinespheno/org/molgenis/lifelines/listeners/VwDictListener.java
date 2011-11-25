bc0e55b8fb9a74965809dc8a5f0147d80ab79cb8
Joris Lops <jorislops@gmail.com>
2011-11-25 14:47:25 +0000

    Importer for lifelines changed,
    should fix hudson,
    Jpa support reverse relationships, these are not generated for molgenis. So I comment all the code. 
    
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9859 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/lifelinespheno/org/molgenis/lifelines/listeners/VwDictListener.java
+++ apps/lifelinespheno/org/molgenis/lifelines/listeners/VwDictListener.java
@@ -1,79 +1,79 @@
-package org.molgenis.lifelines.listeners;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.persistence.EntityManager;
-
-import org.molgenis.framework.db.Database;
-import org.molgenis.framework.db.DatabaseException;
-import org.molgenis.organization.Investigation;
-import org.molgenis.pheno.Measurement;
-import org.molgenis.protocol.Protocol;
-import org.molgenis.util.Tuple;
-
-
-
-/**
- * This listener creates Measurement for each field and Protocol for each table in VW_DICT
- * 
- * TABNAAM
-GROEP
-VELDNR
-VELD -> Measurement.name
-VLDTYPE -> Measurement.dataType
-OMSCHR -> Measurement.description
-
- * @author jorislops
- *
- */
-public class VwDictListener extends ImportTupleListener {
-
-	private Map<String, Protocol> protocols = new LinkedHashMap<String, Protocol>();
-	
-	private final Investigation investigation;
-	
-	public VwDictListener(Investigation investigation, String name, Database db) {
-		super(name, db);
-		this.investigation = investigation;
-	}
-
-	@Override
-	public void handleLine(int line_number, Tuple tuple) throws Exception {
-		
-		String protocolName = tuple.getString("TABNAAM");
-		
-		//create new protocol if not yet known
-		Protocol p = protocols.get(protocolName);
-		if(p == null)
-		{
-			p = new Protocol();
-			p.setName(protocolName);
-			//p.setInvestigation_Id(investigationId);
-			p.setInvestigation(investigation);
-			protocols.put(protocolName, p);
-		}
-		
-		Measurement m = new Measurement();
-		m.setName(tuple.getString("TABNAAM")+"_"+tuple.getString("VELD"));
-		m.setDataType( tuple.getString("VLDTYPE")  );
-		m.setDescription(tuple.getString("OMSCHR"));
-		
-		p.getFeatures().add(m);
-	}
-	
-	public void commit() throws DatabaseException
-	{
-		EntityManager em = db.getEntityManager();
-		em.getTransaction().begin();
-		for(Protocol p : protocols.values()) {
-			em.persist(p);
-		}		
-		em.flush();
-		em.getTransaction().commit();
-	}
-
-	public Map<String, Protocol> getProtocols() {
-		return protocols;
-	}
-}
+//package org.molgenis.lifelines.listeners;
+//
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//import javax.persistence.EntityManager;
+//
+//import org.molgenis.framework.db.Database;
+//import org.molgenis.framework.db.DatabaseException;
+//import org.molgenis.organization.Investigation;
+//import org.molgenis.pheno.Measurement;
+//import org.molgenis.protocol.Protocol;
+//import org.molgenis.util.Tuple;
+//
+//
+//
+///**
+// * This listener creates Measurement for each field and Protocol for each table in VW_DICT
+// * 
+// * TABNAAM
+//GROEP
+//VELDNR
+//VELD -> Measurement.name
+//VLDTYPE -> Measurement.dataType
+//OMSCHR -> Measurement.description
+//
+// * @author jorislops
+// *
+// */
+//public class VwDictListener extends ImportTupleListener {
+//
+//	private Map<String, Protocol> protocols = new LinkedHashMap<String, Protocol>();
+//	
+//	private final Investigation investigation;
+//	
+//	public VwDictListener(Investigation investigation, String name, Database db) {
+//		super(name, db);
+//		this.investigation = investigation;
+//	}
+//
+//	@Override
+//	public void handleLine(int line_number, Tuple tuple) throws Exception {
+//		
+//		String protocolName = tuple.getString("TABNAAM");
+//		
+//		//create new protocol if not yet known
+//		Protocol p = protocols.get(protocolName);
+//		if(p == null)
+//		{
+//			p = new Protocol();
+//			p.setName(protocolName);
+//			//p.setInvestigation_Id(investigationId);
+//			p.setInvestigation(investigation);
+//			protocols.put(protocolName, p);
+//		}
+//		
+//		Measurement m = new Measurement();
+//		m.setName(tuple.getString("TABNAAM")+"_"+tuple.getString("VELD"));
+//		m.setDataType( tuple.getString("VLDTYPE")  );
+//		m.setDescription(tuple.getString("OMSCHR"));
+//		
+//		p.getFeatures().add(m);
+//	}
+//	
+//	public void commit() throws DatabaseException
+//	{
+//		EntityManager em = db.getEntityManager();
+//		em.getTransaction().begin();
+//		for(Protocol p : protocols.values()) {
+//			em.persist(p);
+//		}		
+//		em.flush();
+//		em.getTransaction().commit();
+//	}
+//
+//	public Map<String, Protocol> getProtocols() {
+//		return protocols;
+//	}
+//}
