85ceb0c66f463fba73d12335cce4598471f955b5
GCC Groningen <gcc-groningen@googlegroups.com>
2011-11-28 12:50:25 +0000

    renamed plugin
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9878 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ apps/lifelines/plugins/JavascriptTests/JavascriptTestsPlugin.java
@@ -0,0 +1,65 @@
+package plugins.JavascriptTests;
+
+
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.ui.PluginModel;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.framework.ui.html.JavascriptTests;
+import org.molgenis.util.Entity;
+import org.molgenis.util.Tuple;
+
+public class JavascriptTestsPlugin extends PluginModel<Entity>{
+	
+	//A Test class for  javascript. 
+	JavascriptTests JsTest = new JavascriptTests("test");
+
+	
+	public JavascriptTestsPlugin(String name, ScreenController<?> parent) {
+		super(name, parent);
+
+
+		
+		
+	}
+	
+	@Override 
+	public String getViewName() {
+		return "plugins_JavascriptTests_JavascriptTestsPlugin";
+	
+	}
+	
+	@Override
+	public String getViewTemplate()
+	{
+		return "plugins/JavascriptTests/JavascriptTestsPlugin.ftl";
+		
+	}
+	
+	@Override
+	public void handleRequest(Database db, Tuple request){
+		
+	
+	}
+	
+	@Override
+	public void reload(Database db) {
+	
+	}
+
+
+	@Override
+	public boolean isVisible()
+	{
+		if (!this.getLogin().isAuthenticated()) {
+			return false;
+		}
+		return true;
+	}
+
+	public String getJavascriptTest() {
+		System.out.println("test from JavascripttestsPlugin");
+		return JsTest.toHtml();
+	}
+	
+
+}
