4d5e3f58b6c586f1adc57d1ee5c1c097a250e856
Joeri van der Velde <joeriv@gmail.com>
2011-12-02 15:48:01 +0000

    Added placeholder services for SOAP and REST.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@9961 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/services/MolgenisSoapService.java
+++ src/org/molgenis/framework/server/services/MolgenisSoapService.java
@@ -6,7 +6,10 @@ import java.util.Hashtable;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.xml.ws.Endpoint;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
 import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
 import org.apache.log4j.Logger;
 import org.molgenis.framework.server.MolgenisContext;
@@ -17,43 +20,44 @@ import org.molgenis.framework.server.ServeConfig;
 
 /**
  * 
+ * NOT IMPLEMENTED
  * NOT TESTED
  * 
- * See http://cxf.apache.org/docs/jax-rs.html
- * http://cxf.apache.org/docs/jaxrs-services-configuration.html
- *
  */
-public class MolgenisRestService extends CXFNonSpringJaxrsServlet implements MolgenisService 
+public class MolgenisSoapService extends CXFNonSpringJaxrsServlet implements MolgenisService 
 {
 	private static final long serialVersionUID = -6699220792069809444L;
 	Logger logger = Logger.getLogger(MolgenisRapiService.class);
 	Hashtable<String,Object> restParams;
 	
 	private MolgenisContext mc;
+	private boolean cxfLoaded = false;
 	
-	public MolgenisRestService(MolgenisContext mc)
+	public MolgenisSoapService(MolgenisContext mc)
 	{
 		this.mc = mc;
 	}
 	
-	public MolgenisRestService() throws ServletException
+	public MolgenisSoapService() throws ServletException
 	{
-		restParams = new Hashtable<String,Object>();
-		restParams.put("jaxrs.serviceClasses", "app.servlet.RestApi");
+//		restParams = new Hashtable<String,Object>();
+//		restParams.put("jaxrs.serviceClasses", "app.servlet.RestApi");
 	}
 	
 	public void handleRequest(MolgenisRequest r,
 			MolgenisResponse response) throws IOException
 	{
-		try
-		{
-			super.init(new ServeConfig( mc.getServletContext(), restParams, "/"));
-			super.service(r.getRequest(), response.getResponse());
-			throw new IOException();
-		}
-		catch (ServletException e)
-		{
-			throw new IOException(e);
-		}
+//		if (this.cxfLoaded == false && (this.getSoapImpl() != null))// ||
+//			// this.getRestImpl()
+//			// != null))
+//			{
+//				super.loadBus(this.getServletConfig());
+//				Bus bus = this.getBus();
+//				BusFactory.setDefaultBus(bus);
+//				Endpoint.publish("/soap/", this.getSoapImpl());
+//				this.cxfLoaded = true;
+//			}
+//
+//			super.doPost(request, response);
 	}
 }
