92e42033d2cbacfbf35023347c7b1344e68ff45d
Joeri van der Velde <joeriv@gmail.com>
2011-12-02 08:52:46 +0000

    Added Csv to XGAP binary conversion to xQTL R tools.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9955 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/xgap/R/DataMatrix.R
+++ apps/xgap/xgap/R/DataMatrix.R
@@ -167,3 +167,36 @@ remove.datamatrix <- function(id=NULL, name=NULL, investigation_id=NULL)
     eval(call2)
     remove.data(container)    
 }
+
+#
+# CsvToXgapBin.R
+#
+# From: https://github.com/DannyArends/iqtl/blob/master/R/CsvToXgapBin.R
+#
+# copyright (c) 2011, Danny Arends
+# last modified dec, 2011
+# first written dec, 2011
+#
+# R functions: CsvToXgapBin
+#
+
+CsvToXgapBin <- function(name = "ExampleData", investigation = "ExampleInv", rowtype= "Metabolite", coltype= "Individual", valuetype="Decimal", file="metab.txt", verbose = TRUE){
+  require(RCurl)
+  if(file.exists("CsvToBin.jar")){
+    if(verbose) cat("WARNING: 'CsvToBin.jar' exists and will be re-downloaded\n")
+    file.remove("CsvToBin.jar")
+  }
+  csvtobin <- getBinaryURL("http://www.molgenis.org/svn/standalone_tools/jars/CsvToBin.jar")
+  writeBin(csvtobin,"CsvToBin.jar")
+  outname <- gsub(".txt",".bin",file)
+  if(file.exists(outname)){
+    if(verbose) cat("WARNING: Output file '",outname,"'exists and will be overwitten\n")
+    file.remove(outname)
+  }
+  command <- paste("java -jar CsvToBin.jar",name,investigation,rowtype,coltype,valuetype,file)
+  if(verbose) cat("CMD:", command,"\n")
+  res <- system(command,show.output.on.console=verbose)
+  if(res != 0){
+    stop("Error executing CsvToBin")
+  }
+}
