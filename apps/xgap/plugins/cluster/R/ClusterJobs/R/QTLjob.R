7f026e7cae202c2014b7981d205bf48b81bc3c35
Joeri van der Velde <joeriv@gmail.com>
2011-12-05 09:21:27 +0000

    Fixed uploading of QTL plots by using the MOLGENIS.upload() function which wraps the RCurl session so the authentication is used from the proper session.
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@9979 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/plugins/cluster/R/ClusterJobs/R/QTLjob.R
+++ apps/xgap/plugins/cluster/R/ClusterJobs/R/QTLjob.R
@@ -42,7 +42,7 @@ run_QTL <- function(dbpath = "", subjob, item, jobid, outname, myanalysisfile, j
   cat(Generate_Statement(paste("plot(results)","\n",sep="")),file=myanalysisfile,append=T)
   cat(paste("dev.off()","\n",sep=""),file=myanalysisfile,append=T)
   cat("report(2,\"PlotInTemp.fig\")\n",file=myanalysisfile,append=T)
-  cat(Generate_Statement(paste("postForm('",paste(dbpath,"/uploadfile",sep=""),"',Investigation_name='",investigationname,"', name='",imagefilename,"', type = 'InvestigationFile', file = fileUpload(filename='",imagefilename,"'), style='HTTPPOST')","\n",sep="")),file=myanalysisfile,append=T)
+  cat(Generate_Statement(paste("MOLGENIS.upload('",paste(dbpath,"/uploadfile",sep=""),"',Investigation_name='",investigationname,"', name='",imagefilename,"', type = 'InvestigationFile', filename='",imagefilename,"')","\n",sep="")),file=myanalysisfile,append=T)
   cat("report(2,\"UploadedFIGtoDatabase\")\n",file=myanalysisfile,append=T)
   cat("report(2,\"StoringQTLResults\")\n",file=myanalysisfile,append=T)
   cat("info: Store QTL results by using ResultsToMolgenis function or any other function provided in the molgenis R API\n")
