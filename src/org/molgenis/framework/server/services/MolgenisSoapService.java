75e7150205b07cc2ddb8b449f02294be78fbd0e5
Joeri van der Velde <joeriv@gmail.com>
2011-12-06 22:01:23 +0000

    Working on MolgenisService SOAP API. Still not working, it seems to do more than before.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@10021 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/services/MolgenisSoapService.java
+++ src/org/molgenis/framework/server/services/MolgenisSoapService.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.xml.ws.Endpoint;
 
@@ -21,28 +20,31 @@ import org.molgenis.framework.server.ServeConfig;
 
 /**
  * 
- * NOT WORKING
- * NOT TESTED
+ * NOT WORKING, seems to get stuck on:
+ * 
+ * Dec 6, 2011 10:59:16 PM org.apache.cxf.jaxrs.impl.WebApplicationExceptionMapper toResponse
+ * WARNING: WebApplicationException has been caught : no cause is available
  * 
  */
-public class MolgenisSoapService extends CXFNonSpringJaxrsServlet
-		implements MolgenisService
+public class MolgenisSoapService extends CXFNonSpringJaxrsServlet implements
+		MolgenisService
 {
-	private static final long serialVersionUID = -6699220792069809444L;
 	Logger logger = Logger.getLogger(MolgenisRapiService.class);
-
-	protected MolgenisContext mc;
-	private boolean cxfLoaded = false;
+	private static final long serialVersionUID = 1L;
+	//private MolgenisContext mc;
+	private boolean cxfLoaded;
+	
 	protected Database freshDatabase = null;
 
 	public MolgenisSoapService(MolgenisContext mc) throws ServletException
 	{
-//		Hashtable<String,Object> params = new Hashtable<String,Object>();
-//		params.put("javax.ws.rs.Application", "app.servlet.SoapService");
-//		ServletConfig sc = new ServeConfig( mc.getServletContext(), params, "/");
-//		
-//		super.init(sc);
-		this.mc = mc;
+		//this.mc = mc;
+
+		Hashtable<String, Object> params = new Hashtable<String, Object>();
+		params.put("jaxrs.serviceClasses", "app.servlet.RestApi");
+
+		ServletConfig sc = new ServeConfig(mc.getServletContext(), params, "/");
+		super.init(sc);
 	}
 
 	public void handleRequest(MolgenisRequest request, MolgenisResponse response)
@@ -52,25 +54,22 @@ public class MolgenisSoapService extends CXFNonSpringJaxrsServlet
 
 		try
 		{
-			if (this.cxfLoaded == false && (this.getSoapImpl(mc) != null))// ||
+			if (this.cxfLoaded == false && (this.getSoapImpl() != null))
 			{
-				Hashtable<String,Object> params = new Hashtable<String,Object>();
-				params.put("javax.ws.rs.Application", "app.servlet.SoapService");
-				ServletConfig sc = new ServeConfig( mc.getServletContext(), params, "/");
-//				
-//				super.init(sc);
-				super.loadBus(sc);
+				super.loadBus(this.getServletConfig());
 				Bus bus = this.getBus();
 				BusFactory.setDefaultBus(bus);
-				Endpoint.publish("/soap/", this.getSoapImpl(mc));
+				Endpoint.publish("/soap/", this.getSoapImpl());
 				this.cxfLoaded = true;
 			}
+
 			super.doPost(request.getRequest(), response.getResponse());
 		}
-		catch (ServletException e)
+		catch (Exception e)
 		{
 			throw new IOException(e);
 		}
+
 	}
 
 	/**
@@ -79,10 +78,11 @@ public class MolgenisSoapService extends CXFNonSpringJaxrsServlet
 	 * 
 	 * @param mc
 	 * @return
-	 * @throws ServletException 
+	 * @throws ServletException
 	 */
-	public Object getSoapImpl(MolgenisContext mc) throws ServletException
+	public Object getSoapImpl() throws ServletException
 	{
-		throw new UnsupportedOperationException("Don't use this! Try the generated subclass 'SoapService' instead.");
+		throw new UnsupportedOperationException(
+				"Don't use this! Try the generated subclass 'SoapService' instead.");
 	}
 }
