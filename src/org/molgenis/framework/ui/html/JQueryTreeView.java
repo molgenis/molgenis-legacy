b7a7541d20d938f7c004e11f649a58f6f0ed36ce
Erik Roos <tom.erik.roos@gmail.com>
2012-01-09 16:18:20 +0000

    Tree in LL Catalog now remembers state: protocols from which measurements have been chosen remain open, and measurements that have been selected remain checked.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@10355 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/html/JQueryTreeView.java
+++ src/org/molgenis/framework/ui/html/JQueryTreeView.java
@@ -1,10 +1,10 @@
 package org.molgenis.framework.ui.html;
 
+import java.util.List;
 import java.util.Vector;
 
 import org.molgenis.util.SimpleTree;
 import org.molgenis.util.Tree;
-
 
 public class JQueryTreeView<E> extends HtmlWidget
 {
@@ -16,6 +16,19 @@ public class JQueryTreeView<E> extends HtmlWidget
 		this.treeData = treeData;
 	}
 	
+	private boolean nodeOpen(JQueryTreeViewElement node, List<String> selected) {
+		Vector<JQueryTreeViewElement> children = node.getChildren();
+		for (JQueryTreeViewElement child : children) {
+			if (selected.contains(child.getLabel())) {
+				return true;
+			}
+			if (child.hasChildren()) {
+				return nodeOpen(child, selected);
+			}
+		}
+		return false;
+	}
+	
 	/**
 	 * 
 	 * No Nodes collapsed explicitly. Node is manually closed then by giving its LI element a "closed"(/opened) CSS class.
@@ -23,31 +36,33 @@ public class JQueryTreeView<E> extends HtmlWidget
 	 * "Cookie" persistence enabled, causing the current tree state to be persisted.
 	 *	Dynamically adding a sub tree to the existing tree demonstrated.
 	 */
-	private String renderTree(JQueryTreeViewElement node) {
+	private String renderTree(JQueryTreeViewElement node, List<String> selected) {
 		String returnString;
 		if (node.hasChildren()) {
-			returnString = "<li class=\"closed\"><span class=\"folder\">" + node.getLabel() + "</span>";
+			returnString = "<li class=\"" + (nodeOpen(node, selected) ? "opened" : "closed") + 
+					"\"><span class=\"folder\">" + node.getLabel() + "</span>";
 			returnString += "<ul>";
 			Vector<JQueryTreeViewElement> children = node.getChildren();
 			for (JQueryTreeViewElement child : children) {
-				returnString += renderTree(child);
+				returnString += renderTree(child, selected);
 			}
 			returnString += "</ul></li>";
 		} else {
 			returnString = "<li><span class=\"point\"><input type=\"checkbox\" id=\"" + 
-				node.getLabel() + "\" name=\"" + node.getLabel() + "\" />" + node.getLabel() + "</span></li>";
+				node.getLabel() + "\" name=\"" + node.getLabel() + "\"" + 
+				(selected.contains(node.getLabel()) ? " checked=\"yes\"" : "") + 
+				" />" + node.getLabel() + "</span></li>";
 		}
 		return returnString;
 	}
 	
-	@Override
-	public String toHtml(){
+	public String toHtml(List<String> selected){
 		
 		String html = "<div id=\"masstoggler\">	<a title=\"Collapse entire tree\" href=\"#\">Collapse All</a> | ";
 		html += "<a title=\"Expand entire tree\" href=\"#\">Expand All</a>" ;
 		//html += " | <a title=\"Toggle the tree below\" href=\"#\">Toggle All</a></div> ";
 		html += "<ul id=\"browser\" class=\"pointtree\">";
-		html += renderTree(treeData.getRoot());
+		html += renderTree(treeData.getRoot(), selected);
 		html += "</ul>";
 	    html += "<script src=\"res/jquery-plugins/datatables/js/jquery.js\"></script>\n"
 		+ "<link rel=\"stylesheet\" href=\"res/jquery-plugins/Treeview/jquery.treeview.css\" type=\"text/css\" media=\"screen\" />\n" 
@@ -68,6 +83,11 @@ public class JQueryTreeView<E> extends HtmlWidget
 		+"</script>\n";
 	    
 	    return html;
+	}
+	
+	@Override
+	public String toHtml(){
+		return "";
 	}
 }
 
