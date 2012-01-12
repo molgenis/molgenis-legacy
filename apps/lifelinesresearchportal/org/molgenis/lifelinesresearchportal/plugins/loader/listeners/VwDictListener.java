d06bddf1043abf9fdcbb9e7d95a9e9b49e05e5e4
Erik Roos <tom.erik.roos@gmail.com>
2012-01-12 11:26:18 +0000

    Use VW_BEZOEK_PIVOT instead of BEZOEK, but rename it to BEZOEK
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@10404 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/plugins/loader/listeners/VwDictListener.java
+++ apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/plugins/loader/listeners/VwDictListener.java
@@ -42,8 +42,15 @@ public class VwDictListener extends ImportTupleListener {
 	@Override
 	public void handleLine(int line_number, Tuple tuple) throws Exception {
 		
-		if (tuple.getString("TABNAAM") != null && !tuple.getString("TABNAAM").equals("")) {
+		// BEZOEK is skipped because we use BEZOEK_PIVOT, where each subvisit has become a row
+		if (tuple.getString("TABNAAM") != null && !tuple.getString("TABNAAM").equals("") && 
+			!tuple.getString("TABNAAM").equals("BEZOEK")) {
+			
 			String protocolName = tuple.getString("TABNAAM");
+			// rename BEZOEK_PIVOT to BEZOEK
+			if (protocolName.equals("VW_BEZOEK_PIVOT")) {
+				protocolName = "BEZOEK";
+			}
 			
 			//create new protocol if not yet known
 			Protocol p = protocols.get(protocolName);
