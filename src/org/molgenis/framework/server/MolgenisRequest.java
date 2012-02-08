25e4fbaffb3887436ad65d75af874a238a1f0115
Joeri van der Velde <joeriv@gmail.com>
2012-02-08 15:07:15 +0000

    Implemented special toString for MolgenisRequest, which breaks off large values, and hide passwords.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@10787 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/MolgenisRequest.java
+++ src/org/molgenis/framework/server/MolgenisRequest.java
@@ -46,4 +46,44 @@ public class MolgenisRequest extends HttpServletRequestTuple
 
 	
 	
+	/**
+	 * Special toString for MolgenisRequest
+	 * Will break off large values, and hide passwords
+	 */
+	public String toString()
+	{
+		if (this.getNrColumns() == 0) return "EMPTY REQUEST";
+		String result = "";
+		for (int columnIndex = 0; columnIndex < this.getNrColumns(); columnIndex++)
+		{
+			
+			//take care of the value
+			String value = "NULL";
+			if(getObject(columnIndex) != null)
+			{
+				value =  getObject(columnIndex).toString();
+				value = value.length() > 25 ? value.substring(0, 25) + ".." : value;
+			}
+		
+			//take care of the name
+			String name = columnIndex + "";
+			if (getColName(columnIndex) != null)
+			{
+				name = getColName(columnIndex);
+			}
+			
+			//print name + value
+			if (name.contains("password") || name.equals(MolgenisServiceAuthenticationHelper.LOGIN_PASSWORD))
+			{
+				result += name + "='*****' ";
+			}
+			else
+			{
+				result += name + "='" + value + "' ";
+			}
+			
+		}
+		return result;
+	}
+
 }
