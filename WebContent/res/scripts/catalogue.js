b6d775a929c82d888bee532a8e4bbeedde28f82a
GCC Groningen <gcc-groningen@googlegroups.com>
2012-02-17 13:03:17 +0000

    Added more javascript code into catalogue.js
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@10914 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- WebContent/res/scripts/catalogue.js
+++ WebContent/res/scripts/catalogue.js
@@ -1,4 +1,44 @@
 //debugger;"
+//This hashmap contains the details of the measurements that are shown when we click on a tree leaf. 
+HashMap = function(){
+			  this._dict = [];
+			}
+			HashMap.prototype._get = function(key){
+			  for(var i=0, couplet; couplet = this._dict[i]; i++){
+			    if(couplet[0] === key){
+			      return couplet;
+			    }
+			  }
+			}
+			HashMap.prototype.put = function(key, value){
+			  var couplet = this._get(key);
+			  if(couplet){
+			    couplet[1] = value;
+			  }else{
+			    this._dict.push([key, value]);
+			  }
+			  return this; // for chaining
+			}
+			HashMap.prototype.get = function(key){
+			  var couplet = this._get(key);
+			  if(couplet){
+			    return couplet[1];
+			  }
+			}
+			
+function createHashMap(key, content)	{
+				map.put(key, content);
+				
+			}
+			
+			function getHashMapContent(key){
+				
+				var value = map.get(key);
+				$('#details').empty();
+				$('#details').append(value);
+				
+			}
+			
 //adding css styling on click 
 	$("ul").delegate("li", "click", function() {
 		$(this).addClass("active");
@@ -20,3 +60,7 @@
 	$(document).unload(function() {
 //		alert('Handler for .unload() called.');"
 	});
+	
+	
+	
+	
--- apps/catalogue/plugins/catalogueTree/catalogueTreePlugin.ftl
+++ apps/catalogue/plugins/catalogueTree/catalogueTreePlugin.ftl
@@ -10,52 +10,7 @@
 	
 <!-- this shows a title and border -->
 	<div class="formscreen">
-		<script type="text/javascript">
-			HashMap = function(){
-			  this._dict = [];
-			}
-			HashMap.prototype._get = function(key){
-			  for(var i=0, couplet; couplet = this._dict[i]; i++){
-			    if(couplet[0] === key){
-			      return couplet;
-			    }
-			  }
-			}
-			HashMap.prototype.put = function(key, value){
-			  var couplet = this._get(key);
-			  if(couplet){
-			    couplet[1] = value;
-			  }else{
-			    this._dict.push([key, value]);
-			  }
-			  return this; // for chaining
-			}
-			HashMap.prototype.get = function(key){
-			  var couplet = this._get(key);
-			  if(couplet){
-			    return couplet[1];
-			  }
-			}
-		</script>
-		
-		<script>
-			
-			
-			function createHashMap(key, content)
-			{
-				map.put(key, content);
-				
-			}
-			
-			function getHashMapContent(key){
-				
-				var value = map.get(key);
-				$('#details').empty();
-				$('#details').append(value);
-				
-			}
-			
-		</script>
+	
 		
 		<div class="form_header" id="${screen.getName()}">
 			${screen.label}
