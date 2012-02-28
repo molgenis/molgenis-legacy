4ffd5f55b95162e859e533ab28b8f7423b7f74e3
Joeri van der Velde <joeriv@gmail.com>
2012-02-28 13:55:52 +0000

    Added toggle JS function
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@11077 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/res/scripts/menu.js
+++ src/org/molgenis/framework/ui/res/scripts/menu.js
@@ -52,3 +52,13 @@ function showhide(id){
 		}
 	}
 }
+
+function toggle(showHideDiv) {
+	var ele = document.getElementById(showHideDiv);
+	if(ele.style.display == "block") {
+    	ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+}
