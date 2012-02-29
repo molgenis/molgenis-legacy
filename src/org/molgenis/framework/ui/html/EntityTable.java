59b7ea2c4c338d845c0c8aa73a4f458d559323b2
Chao Pang <chaopang229@gmail.com>
2012-02-29 08:39:17 +0000

    Added a simple example for creating a Molgenis-style table in your plugin! The example is attached to the method toHtml() method in EntityTable.java
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@11091 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/ui/html/EntityTable.java
+++ src/org/molgenis/framework/ui/html/EntityTable.java
@@ -10,12 +10,14 @@ public class EntityTable extends HtmlWidget
 {
 	List<? extends Entity> entities = null;
 	List<String> columns = null;
+	private boolean editButton = false;
 
 	public EntityTable(List<? extends Entity> entities, boolean editButton, String... columns)
 	{
 		super(UUID.randomUUID().toString());
 
 		this.entities = entities;
+		this.editButton  = editButton;
 		
 		if(columns != null && columns.length > 0)
 		{
@@ -30,7 +32,24 @@ public class EntityTable extends HtmlWidget
 			}
 		}
 	}
-
+	
+	/**
+	 * A easy way to create a Molgenis-style table in your plugin. 
+	 * 
+	 * 1.Get a list of entities that you want to show in the table
+	 * List<Measurement> listOfMeasurements = db.find(Measurement.class, 
+	 *      new QueryRule(Measurement.INVESTIGATION_NAME, Operator.EQUAL, "inv"));
+	 * 2.Create a new instance of EntityTable and pass the list of measurements as first parameters. 
+	 * The second parameter indicates whether to show the editButton in the table.
+	 * The rest of parameters are the column that you want to show in the table.
+	 * String htmlTable = new EntityTable(listOfMeasurements, true, Measurement.NAME, 
+	 *      Measurement.DESCRIPTION, Measurement.DATATYPE, Measurement.TEMPORARY).toHtml();
+	 * 3.Optional, if u have selected the editButton to be true, you`ll need to get the entity 
+	 * in your plugin and follow the code below:
+	 * 
+	 * htmlTable = new MeasurementForm(measurement).getHtml();
+	 */
+	
 	@Override
 	public String toHtml()
 	{
@@ -48,13 +67,18 @@ public class EntityTable extends HtmlWidget
 		int color = 0;
 		for(Entity e: entities)
 		{
-			htmlTable +="<tr class=\"form_listrow"+color+"\">";
+			htmlTable +="<tr class=\"form_listrow" + color + "\" id=\"" + e.getIdValue() + "\">";
 			if(color == 0) color = 1; else color = 0;
 			for(String column: this.columns)
 			{
 				htmlTable +="<td>"+e.get(column)+"</td>";
 			}
-			htmlTable +="</tr>";
+			if(editButton == true){
+				htmlTable += "<td><img class=\"edit_button\" src=\"generated-res/img/editview.gif\" title=\"edit record\"" +
+					     "onclick=\"submitFormMethod('" + e.getIdValue() + "');\"" +
+					     "></td>";
+			}
+			htmlTable += "</tr>";
 		}
 		htmlTable += "</table>";
 		
