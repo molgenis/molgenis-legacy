5203e98dae6642d83740ecf2a1f9290b22417e58
Joris Lops <jorislops@gmail.com>
2012-03-16 10:12:15 +0000

    fixed import --> Removes dependency to a jar that is not used anywhere!
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11266 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/plugins/loader/ThreadReaders/CSVFileSpliter.java
+++ apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/plugins/loader/ThreadReaders/CSVFileSpliter.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.james.mime4j.field.FieldName;
 import org.molgenis.util.SimpleTuple;
 
 public class CSVFileSpliter {
