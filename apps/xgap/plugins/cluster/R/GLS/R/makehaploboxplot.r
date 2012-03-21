f90648de363627873ad683e257055d83d4ab7dcf
Danny Arends <danny.arends@gmail.com>
2012-03-21 09:17:06 +0000

    Cleaned the file makehaplotboxplot.R and added a header to show who made it, and what it contains
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11327 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/plugins/cluster/R/GLS/R/makehaploboxplot.r
+++ apps/xgap/plugins/cluster/R/GLS/R/makehaploboxplot.r
@@ -1,10 +1,12 @@
-MakeHaploBoxplot    <- function(data.vector,hap.vector,file.name="") {
-  plot.data   <- data.frame(trait.value=as.numeric(data.vector),hap.vector=as.integer(hap.vector)) 
-  if (file.name=="") {
-    boxplot(trait.value ~ hap.vector,data=plot.data,names=as.character(tabulate(hap.vector)))
-  } else {
-    jpeg(file.name,quality=100)
-    boxplot(trait.value ~ hap.vector,data=plot.data,names=as.character(tabulate(hap.vector)))
-    dev.off()
-  }
+#
+# MakeHaploBoxplot.R
+# - Description: Plot routine for haplotypes
+#
+# Copyright (c) 2012 Willem Kruijer and Danny Arends
+
+MakeHaploBoxplot <- function(data.vector, hap.vector, file.name) {
+  plot.data <- data.frame(trait.value = as.numeric(data.vector), hap.vector = as.integer(hap.vector))
+  if(!missing(file.name)) jpeg(file.name,quality=100)
+  boxplot(trait.value ~ hap.vector,data=plot.data,names=as.character(tabulate(hap.vector)))
+  if(!missing(file.name)) dev.off()
 }
