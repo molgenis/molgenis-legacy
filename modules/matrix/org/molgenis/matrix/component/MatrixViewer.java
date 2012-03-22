a52fbdb3d3f5ec59315f48d371fad515955daa32
Erik Roos <tom.erik.roos@gmail.com>
2012-03-22 16:29:05 +0000

    Added stuff to target info box (commented out)
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11354 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- modules/matrix/org/molgenis/matrix/component/MatrixViewer.java
+++ modules/matrix/org/molgenis/matrix/component/MatrixViewer.java
@@ -460,7 +460,9 @@ public class MatrixViewer extends HtmlWidget
 								{
 									String relName = ((ObservedValue) val).getRelation_Name();
 									// Make a nice info box about the target in the cell:
-									String infoBoxContents = "<strong>" + relName + "</strong><hr><ul>";
+									String infoBoxContents = "<strong>" + relName + "</strong><hr>";
+									infoBoxContents += "<p>Values set on this target:</p>";
+									infoBoxContents += "<ul>";
 									try
 									{
 										List<ObservedValue> valList = db.query(ObservedValue.class).
@@ -475,6 +477,17 @@ public class MatrixViewer extends HtmlWidget
 											infoBoxContents +="<li>" + (infoVal.getFeature_Name() + ": " + infoValue + "</li>");
 										}
 										infoBoxContents +="</ul>";
+										// If you want info about values in which the target is referred to:
+//										infoBoxContents += "<p>Values in which this target is referred to:</p>";
+//										infoBoxContents += "<ul>";
+//										valList = db.query(ObservedValue.class).
+//												eq(ObservedValue.RELATION_NAME, relName).
+//												sortASC(ObservedValue.FEATURE_NAME).
+//												find();
+//										for (ObservedValue infoVal : valList) {
+//											infoBoxContents +="<li>" + (infoVal.getFeature_Name() + ": " + infoVal.getTarget_Name() + "</li>");
+//										}
+//										infoBoxContents +="</ul>";
 									}
 									catch (DatabaseException e)
 									{
