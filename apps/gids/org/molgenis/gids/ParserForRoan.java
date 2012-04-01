566b1c9334955a082b9edb84d7a99f547f6d1ff2
Morris Swertz <m.a.swertz@rug.nl>
2012-04-01 08:25:28 +0000

    update gids: replaced CsvListener with Iterator, i.e. for(Tuple t: new CsvReader) {...}
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11463 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/gids/org/molgenis/gids/ParserForRoan.java
+++ apps/gids/org/molgenis/gids/ParserForRoan.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 import org.molgenis.util.CsvFileReader;
-import org.molgenis.util.CsvReaderListener;
 import org.molgenis.util.Tuple;
 
 public class ParserForRoan
@@ -15,46 +14,44 @@ public class ParserForRoan
 		new ParserForRoan("/Users/roankanninga/GIDS/Export/Export_CeliacSprue_17aug.csv");
 
 	}
-	
+
 	public ParserForRoan(String location) throws Exception
 	{
 		CsvFileReader reader = new CsvFileReader(new File(location));
-		
+
 		final ArrayList<String> allIndv = new ArrayList<String>();
 		final ArrayList<String> allParents = new ArrayList<String>();
 		final ArrayList<String> wrongdob = new ArrayList<String>();
-		
-		reader.parse(new CsvReaderListener()
+
+		for (Tuple tuple : reader)
 		{
-			public void handleLine(int line_number, Tuple tuple)
-					throws Exception
+			String indv = tuple.getString(0);
+			String dad = tuple.getString(3);
+			String mom = tuple.getString(4);
+			String dob = tuple.getString(8);
+
+			// date of birth column 8
+			allIndv.add(indv);
+			if (dad != null)
 			{
-				
-				String indv = tuple.getString(0);
-				String dad = tuple.getString(3);
-				String mom = tuple.getString(4);
-				String dob = tuple.getString(8);
-				
-				//date of birth column 8
-				allIndv.add(indv);
-				if(dad != null){ 
-					
-					allParents.add(dad);
-					
-				}
-				if(mom != null ){ 
-					allParents.add(mom);
-				}
+
+				allParents.add(dad);
+
 			}
-		});
-		
-		for(String parent : allParents)
+			if (mom != null)
+			{
+				allParents.add(mom);
+			}
+		}
+
+		for (String parent : allParents)
 		{
 			System.out.println(parent);
-			if(!allIndv.contains(parent)){
+			if (!allIndv.contains(parent))
+			{
 				System.out.println("Missing parent: " + parent);
 			}
 		}
-		
+
 	}
 }
