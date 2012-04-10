25e055cd3cbb500fbd8699069d2394ab32120fc8
Danny Arends <danny.arends@gmail.com>
2012-04-10 13:32:40 +0000

    More headers and checks
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11554 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/plugins/cluster/R/GLS/R/MakeVarcompFile.r
+++ apps/xgap/plugins/cluster/R/GLS/R/MakeVarcompFile.r
@@ -1,6 +1,14 @@
-MakeVarcompFile <- function(file.name,var.comp.values=c(1,0.1)) {
+#
+# MakeVarcompFile.R
+# - Description: Create a variance components file
 # write the values of the variance components contained in var.comp.values to the file file.name (for use in scan_GLS)
-    var.comp.values      <- data.frame(var.comp=as.numeric(unlist(var.comp.values)))
-    row.names(var.comp.values)<- c("sigma2_g","sigma2_e")
-    write.table(var.comp.values,file=file.name,quote=F,row.names = T,col.names=F,sep=",")
+#
+# Copyright (c) 2012 Willem Kruijer and Danny Arends
+
+MakeVarcompFile <- function(file.name, var.comp.values=c(1,0.1)) {
+  if(missing(file.name))       stop("argument 'file.name' is missing, with no default")
+  
+  var.comp.values      <- data.frame(var.comp=as.numeric(unlist(var.comp.values)))
+  row.names(var.comp.values)<- c("sigma2_g","sigma2_e")
+  write.table(var.comp.values, file=file.name, quote=F, row.names = T, col.names=F, sep=",")
 }
--- apps/xgap/plugins/cluster/R/GLS/R/NormalQuantileTransform.r
+++ apps/xgap/plugins/cluster/R/GLS/R/NormalQuantileTransform.r
@@ -1,4 +1,11 @@
-NormalQuantileTransform <- function(y) {
-# the normal quantile transform as in Guan and Stephens (2010)
-qqnorm(y,plot.it=F)$x
+#
+# NormalQuantileTransform.R
+# - Description: Normalize quantiles
+# The normal quantile transform as in Guan and Stephens (2010)
+#
+# Copyright (c) 2012 Willem Kruijer and Danny Arends
+
+NormalQuantileTransform <- function(x, plot.it=FALSE){
+  if(missing(x))  stop("argument 'x' is missing, with no default")
+  qqnorm(x, plot.it=plot.it)$x
 }
