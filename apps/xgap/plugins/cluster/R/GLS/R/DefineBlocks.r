786f0eb9cd0a18203126026bb3658c9f9f6612cc
Danny Arends <danny.arends@gmail.com>
2012-04-10 14:17:35 +0000

    Header and missing parameter check for DefineBlocks.R
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11559 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/xgap/plugins/cluster/R/GLS/R/DefineBlocks.r
+++ apps/xgap/plugins/cluster/R/GLS/R/DefineBlocks.r
@@ -1,16 +1,28 @@
-DefineBlocks  <- function(indices,block.size=1000) {
-    nsnp            <- length(indices)
-    if (nsnp<=block.size) {
-      blocks  <- list(indices)
-    } else {
-      blocks          <- NULL
-      nbl             <- ceiling(nsnp/block.size)
-      if (nbl== nsnp/block.size) {
-          for (i in 1:nbl) {blocks[[i]]   <- indices[(i-1)*block.size + 1:block.size]}
-      } else {
-          for (i in 1:(nbl-1)) {blocks[[i]]   <- indices[(i-1)*block.size + 1:block.size]}
-          blocks[[nbl]]   <- indices[-(1:((nbl-1)*block.size))]
-      }   
-    }
-blocks
+#
+# DefineBlocks.R
+# - Description: Compute False Discovery Proportion under the full (strong) null
+#
+# Copyright (c) 2012 Willem Kruijer and Danny Arends
+
+DefineBlocks  <- function(indices, block.size=1000) {
+  if(missing(indices)) stop("argument 'indices' is missing, with no default")
+ 
+  nsnp            <- length(indices)
+  if(nsnp<=block.size){
+    blocks <- list(indices)
+  }else{
+    blocks <- NULL
+    nbl <- ceiling(nsnp/block.size)
+    if(nbl== nsnp/block.size){
+      for(i in 1:nbl){
+        blocks[[i]]   <- indices[(i-1)*block.size + 1:block.size]
+      }
+    }else{
+      for(i in 1:(nbl-1)){
+        blocks[[i]]   <- indices[(i-1)*block.size + 1:block.size]
+      }
+      blocks[[nbl]]   <- indices[-(1:((nbl-1)*block.size))]
+    }   
+  }
+  blocks
 }
