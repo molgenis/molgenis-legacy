d926f5c1317539bb204a6cdf65a4171b298c9fc9
GCC Groningen <gcc-groningen@googlegroups.com>
2012-04-12 17:28:08 +0000

    Changed update database so that new demo account is inserted as well as molgenis Permission is filled so demo has appropriate privileges.Hope doesn't break hudson ...otherwise revert!!
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11608 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/catalogue/org/molgenis/catalogue/catalogueUpdateDatabase.java
+++ apps/catalogue/org/molgenis/catalogue/catalogueUpdateDatabase.java
@@ -1,21 +1,36 @@
 package org.molgenis.catalogue;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.PropertyConfigurator;
 import org.molgenis.Molgenis;
+import org.molgenis.auth.MolgenisGroup;
+import org.molgenis.auth.MolgenisPermission;
+import org.molgenis.auth.MolgenisRoleGroupLink;
 import org.molgenis.auth.MolgenisUser;
 import org.molgenis.auth.Person;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.security.SimpleLogin;
 
 import app.DatabaseFactory;
+import app.FillMetadata;
 
 
 public class catalogueUpdateDatabase
 {
 	public static void main(String[] args) throws Exception
 	{
+		//PropertyConfigurator.configure("apps/catalogue/org/molgenis/catalogue/catalogue_log4j.properties");
 		new Molgenis("apps/catalogue/org/molgenis/catalogue/catalogue.molgenis.properties").updateDb(true);
 		
+//		final Map<String, Object> config = new HashMap<String, Object>();
+//		config.put("hibernate.hbm2ddl.auto", "create-drop");
+//		Database db = DatabaseFactory.create(config);
+//		
+//		FillMetadata.fillMetadata(db, false);
+//		
 		Database db = DatabaseFactory.create("apps/catalogue/org/molgenis/catalogue/catalogue.molgenis.properties");
 		
 		// Only add  user if type of Login allows for this
@@ -31,6 +46,35 @@ public class catalogueUpdateDatabase
 			p.setEmail("despoina.antonakaki@gmail.com");
 			db.add(p);
 		}
+		
+		MolgenisUser demo = new MolgenisUser();
+		demo.setName("demo");
+		demo.setPassword("d3m0c@t@logue");
+		demo.setActive(true);
+		db.add(demo);
+		
+		//Create a group :SimpleUsers
+		MolgenisGroup mg = new MolgenisGroup();
+		mg.setName("SimpleUsers");
+		db.add(mg);
+		
+		//add demo to group Simple Users
+		MolgenisRoleGroupLink mrgl = new MolgenisRoleGroupLink();
+		mrgl.setRole_Name("demo");
+		mrgl.setGroup_Name("SimpleUsers");
+		db.add(mrgl);
+		
+		//Now set permission for the demo account 
+		for (int i=2; i<95; i++) {
+			MolgenisPermission mp = new MolgenisPermission();
+			mp.setRole_Name("SimpleUsers");
+			mp.setEntity_Id(i);
+			mp.setPermission("own");
+			db.add(mp);
+			System.out.println("=Inserting molgenis permisssion for ==" + i);
+
+		}
+		
 		
 		//TODO : do batch import
 	}
