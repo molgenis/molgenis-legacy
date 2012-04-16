fdc7baf75a089648bc337c78d16085f23250db3d
Chao Pang <chaopang229@gmail.com>
2012-04-16 22:35:57 +0000

    add a new function for harmonization plugin!
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11624 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- WebContent/res/scripts/catalogue.js
+++ WebContent/res/scripts/catalogue.js
@@ -1,78 +1,87 @@
 //debugger;"
 //This hashmap contains the details of the measurements that are shown when we click on a tree leaf. 
 HashMap = function(){
-			  this._dict = [];
-			}
-			HashMap.prototype._get = function(key){
-			  for(var i=0, couplet; couplet = this._dict[i]; i++){
-			    if(couplet[0] === key){
-			      return couplet;
-			    }
-			  }
-			}
-			HashMap.prototype.put = function(key, value){
-			  var couplet = this._get(key);
-			  if(couplet){
-			    couplet[1] = value;
-			  }else{
-			    this._dict.push([key, value]);
-			  }
-			  return this; // for chaining
-			}
-			HashMap.prototype.get = function(key){
-			  var couplet = this._get(key);
-			  if(couplet){
-			    return couplet[1];
-			  }
-			}
-			
+	this._dict = [];
+}
+HashMap.prototype._get = function(key){
+	for(var i=0, couplet; couplet = this._dict[i]; i++){
+		if(couplet[0] === key){
+			return couplet;
+		}
+	}
+}
+HashMap.prototype.put = function(key, value){
+	var couplet = this._get(key);
+	if(couplet){
+		couplet[1] = value;
+	}else{
+		this._dict.push([key, value]);
+	}
+	return this; // for chaining
+}
+HashMap.prototype.get = function(key){
+	var couplet = this._get(key);
+	if(couplet){
+		return couplet[1];
+	}
+}
+
 function createHashMap(key, content)	{
-				map.put(key, content);
-				
-			}
-			
-			function getHashMapContent(key){
-				
-				var value = map.get(key);
-				$('#details').empty();
-				$('#details').append(value);
-				
-			}
-			
+	map.put(key, content);
+}
+
+function getClickedTable(key){
+	
+	var array = document.getElementById('details').getElementsByTagName('table');
+	
+	for(var i = 0; i < array.length; i++){
+		array[i].style.display = "none";
+	}
+	
+	document.getElementById(key + " table").style.display = "inline";
+}
+
+function getHashMapContent(key){
+	
+	var value = map.get(key);
+	$('#details').empty();
+	$('#details').append(value);
+	
+}
+
 //adding css styling on click 
-	$("ul").delegate("li", "click", function() {
-		$(this).addClass("active");
-		//$(this).addClass("active").siblings().removeClass("active");"
-	});
+$("ul").delegate("li", "click", function() {
+	$(this).addClass("active");
+	//$(this).addClass("active").siblings().removeClass("active");"
+});
 
 //adding css styling on hover 
-	$("li>span").hover(function(){
-        $(this).addClass("highlight");
-    },function() {
-        $(this).removeClass("highlight");
-    });
-    
-	$(document).ready(function(){
-		$("#splitter").splitter();
-		$("#browser").treeview({control: "#masstoggler"});
-		
-		var $scrollingDiv = $("#scrollingDiv");
-		 
-		$(window).scroll(function(){			
-			$scrollingDiv
-				.stop()
-				.animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );			
-		});
+$("li>span").hover(function(){
+	$(this).addClass("highlight");
+},function() {
+	$(this).removeClass("highlight");
+});
 
+$(document).ready(function(){
+	$("#splitter").splitter();
+	$("#browser").treeview({control: "#masstoggler"});
 
+	var $scrollingDiv = $("#scrollingDiv");
+
+	$(window).scroll(function(){			
+		$scrollingDiv
+		.stop()
+		.animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );			
 	});
 
-	$(document).unload(function() {
-//		alert('Handler for .unload() called.');"
-	});
-	
-	
-	
-	
-	
-	
+
+});
+
+$(document).unload(function() {
+//	alert('Handler for .unload() called.');"
+});
+
+
+
+
+
