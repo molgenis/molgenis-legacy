eada62bc471fddad71268db9709f7c212bd67fed
Robert Wagner <robert.wagner42@googlemail.com>
2012-04-17 12:52:54 +0000

    Made Token and TokenFactory serializable since they are used by Login.
    git-svn-id: http://www.molgenis.org/svn/molgenis/trunk@11638 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- src/org/molgenis/framework/server/Token.java
+++ src/org/molgenis/framework/server/Token.java
@@ -1,9 +1,11 @@
 package org.molgenis.framework.server;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Token
+public class Token implements Serializable
 {
+	private static final long serialVersionUID = 1412910190619758373L;
 	private String userName;
 	private Date expiresAt;
 	private Date createdAt;
