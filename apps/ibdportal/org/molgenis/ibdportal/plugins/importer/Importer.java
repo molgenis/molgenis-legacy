311fd3bd2261569cfb0f633a474ebbd23aea073c
mswertz <mswertz@42de9c89-b41c-0410-9fa9-f33e7b6bda7a>
2012-05-01 22:26:56 +0000

    adapted ibd plugins to simplified plugins interface
    git-svn-id: http://www.molgenis.org/svn/molgenis_apps/trunk@11798 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- apps/ibdportal/org/molgenis/ibdportal/plugins/importer/Importer.java
+++ apps/ibdportal/org/molgenis/ibdportal/plugins/importer/Importer.java
@@ -1,8 +1,6 @@
 package org.molgenis.ibdportal.plugins.importer;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +14,7 @@ import org.molgenis.pheno.Measurement;
 import org.molgenis.pheno.ObservedValue;
 import org.molgenis.pheno.Panel;
 import org.molgenis.util.CsvFileReader;
-import org.molgenis.util.CsvReaderListener;
 import org.molgenis.util.Tuple;
-
 
 public class Importer
 {
@@ -39,129 +35,152 @@ public class Importer
 	{
 		this.db = db;
 		logger = Logger.getLogger("Importer for IBD data");
-		
+
 		userName = login.getUserName();
-		
+
 		ibdInv = new Investigation();
 		ibdInv.setName(invName);
 		ibdInv.setOwns_Name(userName);
 		this.db.add(ibdInv);
-		
+
 		ibdCohort = new Panel();
 		ibdCohort.setName(cohortName);
 		ibdCohort.setInvestigation_Name(invName);
 		ibdCohort.setOwns_Name(userName);
 		this.db.add(ibdCohort);
 	}
-	
+
 	public void doImport(String filename) throws Exception
 	{
 		File file = new File(filename);
 		CsvFileReader reader = new CsvFileReader(file);
-		reader.parse(new CsvReaderListener()
+		int line_number = 1;
+		for (Tuple tuple : reader)
 		{
-			public void handleLine(int line_number, Tuple tuple) throws DatabaseException, ParseException, IOException
+			int nrOfDiags = 0;
+			int highestDiag = 4;
+			String patName = null;
+
+			for (String fieldName : tuple.getFields())
 			{
-				int nrOfDiags = 0;
-				int highestDiag = 4;
-				String patName = null;
-				
-				for (String fieldName : tuple.getFields()) {
-					
-					String value = tuple.getString(fieldName);
-					
-					if (fieldName.equals("DATUMDB") || fieldName.equals("VERSIEDB")) {
-						// These two measurements should be set on the cohort, once
-						if (line_number == 1) {
-							Measurement newMeas = new Measurement();
-							newMeas.setName(fieldName);
-							newMeas.setInvestigation_Name(invName);
-							newMeas.setOwns_Name(userName);
-							measurementsToAdd.add(newMeas);
-							ObservedValue newVal = new ObservedValue();
-							newVal.setTarget_Name(cohortName);
-							newVal.setFeature_Name(fieldName);
-							newVal.setValue(value);
-							valuesToAdd.add(newVal);
-						}
-					} else if (fieldName.equals("PAID")) {
-						// If not seen yet, create a new patient with name 'PAID'
-						patName = value;
-						if (patName == null) {
-							// Skip line altogether if PAID is empty
-							return;
-						}
-						if (!seenPatients.contains(patName)) {
-							seenPatients.add(patName);
-							Individual newPat = new Individual();
-							newPat.setName(patName);
-							newPat.setInvestigation_Name(invName);
-							newPat.setOwns_Name(userName);
-							patientsToAdd.add(newPat);
-						}
-					} else if (fieldName.startsWith("SCDIAG")) {
-						// Diagnoses must be summarized to the most severe one
-						// Hierarchy is from highest to lowest:
-						// 2 (Crohn), 1 (Colitis), 3 (IBDU), 4 (IBDI)
-						
-						// First add measurement
-						if (nrOfDiags == 0 && line_number == 1) {
-							Measurement newMeas = new Measurement();
-							newMeas.setName("SCDIAG_HIGHEST");
-							newMeas.setInvestigation_Name(invName);
-							newMeas.setOwns_Name(userName);
-							measurementsToAdd.add(newMeas);
-						}
-						// Read in diagnosis for this column
-						if (value != null) {
-							int newDiag = Integer.parseInt(value);
-							if ((highestDiag == 4 && newDiag < 4) ||
-								(highestDiag == 3 && newDiag < 3) ||
-								(highestDiag == 1 && newDiag == 2)) {
-								highestDiag = newDiag;
-							}
-						}
-						// When we've seen all 22 diagnosis columns, store highest
-						if (nrOfDiags == 21) {
-							ObservedValue newVal = new ObservedValue();
-							newVal.setTarget_Name(patName);
-							newVal.setFeature_Name("SCDIAG_HIGHEST");
-							newVal.setValue(Integer.toString(highestDiag));
-							valuesToAdd.add(newVal);
-						} else {
-							nrOfDiags++;
-						}
-					} else if (fieldName.startsWith("FKLID")) {
-						// Skip!
-					} else if (fieldName.startsWith("SBASAF")) {
-						// Skip!
-					} else {
-						// All other columns result in a (new) Measurement + an ObservedValue
-						if (line_number == 1) {
-							Measurement newMeas = new Measurement();
-							newMeas.setName(fieldName);
-							newMeas.setInvestigation_Name(invName);
-							newMeas.setOwns_Name(userName);
-							measurementsToAdd.add(newMeas);
-						}
+
+				String value = tuple.getString(fieldName);
+
+				if (fieldName.equals("DATUMDB") || fieldName.equals("VERSIEDB"))
+				{
+					// These two measurements should be set on the cohort, once
+					if (line_number == 1)
+					{
+						Measurement newMeas = new Measurement();
+						newMeas.setName(fieldName);
+						newMeas.setInvestigation_Name(invName);
+						newMeas.setOwns_Name(userName);
+						measurementsToAdd.add(newMeas);
 						ObservedValue newVal = new ObservedValue();
-						newVal.setTarget_Name(patName);
+						newVal.setTarget_Name(cohortName);
 						newVal.setFeature_Name(fieldName);
 						newVal.setValue(value);
 						valuesToAdd.add(newVal);
 					}
 				}
-				
-				rowCnt++;
-				if (rowCnt % 100 == 0) {
-					writeToDb();
-					System.out.println("Wrote data from lines " + (rowCnt - 100) + " through " + rowCnt + " to database");
+				else if (fieldName.equals("PAID"))
+				{
+					// If not seen yet, create a new patient with name 'PAID'
+					patName = value;
+					if (patName == null)
+					{
+						// Skip line altogether if PAID is empty
+						return;
+					}
+					if (!seenPatients.contains(patName))
+					{
+						seenPatients.add(patName);
+						Individual newPat = new Individual();
+						newPat.setName(patName);
+						newPat.setInvestigation_Name(invName);
+						newPat.setOwns_Name(userName);
+						patientsToAdd.add(newPat);
+					}
+				}
+				else if (fieldName.startsWith("SCDIAG"))
+				{
+					// Diagnoses must be summarized to the most severe one
+					// Hierarchy is from highest to lowest:
+					// 2 (Crohn), 1 (Colitis), 3 (IBDU), 4 (IBDI)
+
+					// First add measurement
+					if (nrOfDiags == 0 && line_number == 1)
+					{
+						Measurement newMeas = new Measurement();
+						newMeas.setName("SCDIAG_HIGHEST");
+						newMeas.setInvestigation_Name(invName);
+						newMeas.setOwns_Name(userName);
+						measurementsToAdd.add(newMeas);
+					}
+					// Read in diagnosis for this column
+					if (value != null)
+					{
+						int newDiag = Integer.parseInt(value);
+						if ((highestDiag == 4 && newDiag < 4) || (highestDiag == 3 && newDiag < 3)
+								|| (highestDiag == 1 && newDiag == 2))
+						{
+							highestDiag = newDiag;
+						}
+					}
+					// When we've seen all 22 diagnosis columns, store highest
+					if (nrOfDiags == 21)
+					{
+						ObservedValue newVal = new ObservedValue();
+						newVal.setTarget_Name(patName);
+						newVal.setFeature_Name("SCDIAG_HIGHEST");
+						newVal.setValue(Integer.toString(highestDiag));
+						valuesToAdd.add(newVal);
+					}
+					else
+					{
+						nrOfDiags++;
+					}
+				}
+				else if (fieldName.startsWith("FKLID"))
+				{
+					// Skip!
+				}
+				else if (fieldName.startsWith("SBASAF"))
+				{
+					// Skip!
+				}
+				else
+				{
+					// All other columns result in a (new) Measurement + an
+					// ObservedValue
+					if (line_number == 1)
+					{
+						Measurement newMeas = new Measurement();
+						newMeas.setName(fieldName);
+						newMeas.setInvestigation_Name(invName);
+						newMeas.setOwns_Name(userName);
+						measurementsToAdd.add(newMeas);
+					}
+					ObservedValue newVal = new ObservedValue();
+					newVal.setTarget_Name(patName);
+					newVal.setFeature_Name(fieldName);
+					newVal.setValue(value);
+					valuesToAdd.add(newVal);
 				}
 			}
-		});
+
+			rowCnt++;
+			if (rowCnt % 100 == 0)
+			{
+				writeToDb();
+				System.out.println("Wrote data from lines " + (rowCnt - 100) + " through " + rowCnt + " to database");
+			}
+		}
+		line_number++;
 	}
-	
-	public void writeToDb() throws DatabaseException {
+
+	public void writeToDb() throws DatabaseException
+	{
 		// add patients to the db
 		this.db.add(patientsToAdd);
 		// add measurements to the db
@@ -173,10 +192,11 @@ public class Importer
 		measurementsToAdd.clear();
 		valuesToAdd.clear();
 	}
-	
-	public void addPatientsToCohort() throws DatabaseException {
+
+	public void addPatientsToCohort() throws DatabaseException
+	{
 		ibdCohort.setIndividuals_Name(seenPatients);
 		this.db.update(ibdCohort);
 	}
-	
+
 }
