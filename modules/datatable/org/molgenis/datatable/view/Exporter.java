1fc09c115e198bf907586728293603e53b0b24cf
mswertz <m.a.swertz@rug.nl>
2012-05-14 07:56:25 +0200

    Added views: HtmlTableView and CsvExporter. Removed the 'binding' classes as we don't want that variability in the facade

--- modules/datatable/org/molgenis/datatable/view/Exporter.java
+++ modules/datatable/org/molgenis/datatable/view/Exporter.java
@@ -1,12 +1,8 @@
 package org.molgenis.datatable.view;
 
-import org.molgenis.matrix.MatrixException;
-import org.molgenis.pheno.Measurement;
-import org.molgenis.pheno.ObservationTarget;
-import org.molgenis.pheno.ObservedValue;
 
-public interface Exporter<T extends ObservationTarget, M extends Measurement, V extends ObservedValue> {
-	public void export() throws MatrixException;
+public interface Exporter {
+	public void export();
 	
 	public String getFileExtension();
 	public String getMimeType();
