72127095b7377a8f3de434e3b7883ba9a87e18f7
mswertz <m.a.swertz@rug.nl>
2012-06-15 14:50:12 +0200

    CsvTable and MemoryTable now implement limitOffset

--- modules/datatable/org/molgenis/datatable/model/CsvTable.java
+++ modules/datatable/org/molgenis/datatable/model/CsvTable.java
@@ -3,6 +3,7 @@ package org.molgenis.datatable.model;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,22 +11,29 @@ import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.DataFormatException;
 
 import org.molgenis.model.elements.Field;
 import org.molgenis.util.CsvFileReader;
 import org.molgenis.util.CsvReader;
 import org.molgenis.util.CsvStringReader;
 import org.molgenis.util.Tuple;
+import org.molgenis.util.TupleIterator;
 
 /**
  * Wrap a CSV file into a TupleTable
  */
 public class CsvTable implements TupleTable
 {
-	private final CsvReader csv;
-	private final InputStream countStream;
-	private final List<Field> columns = new ArrayList<Field>();
-	
+	private CsvReader csv;
+	private InputStream countStream;
+	private List<Field> columns = new ArrayList<Field>();
+	int limit = 0;
+	int offset = 0;
+
+	File csvFile;
+	String csvString;
+
 	/**
 	 * Read table from a csv file
 	 * 
@@ -33,14 +41,12 @@ public class CsvTable implements TupleTable
 	 * @throws Exception
 	 */
 	public CsvTable(File csvFile) throws Exception
-	{		
-		csv = new CsvFileReader(new FileInputStream(csvFile));
-		countStream = new FileInputStream(csvFile);
-		
+	{
+		this.csvFile = csvFile;
+		this.reset();
 		loadColumns();
 	}
-	
-	
+
 	/**
 	 * Read table from a csv string
 	 * 
@@ -48,37 +54,42 @@ public class CsvTable implements TupleTable
 	 * @throws Exception
 	 */
 	public CsvTable(String csvString) throws Exception
-	{		
-		csv = new CsvStringReader(csvString);
-		countStream = new ByteArrayInputStream(csvString.getBytes());
-		
+	{
+		this.csvString = csvString;
+		this.reset();
 		loadColumns();
 	}
-	
+
 	int rowCount = -1;
-	
+
 	/**
 	 * Count rows (not including header of csv file)
 	 */
 	@Override
-	public int getRowCount() throws TableException {
-		if(rowCount == -1) {
-			try {
-			    LineNumberReader lineReader = new LineNumberReader(new InputStreamReader(countStream));
-			    String line = null;
-			    while ((line = lineReader.readLine()) != null) {
-			    	line = line.trim();
-			    }
-			    
-			    //substract 1 because of header
-			    rowCount = lineReader.getLineNumber() - 1;
-			} catch (Exception e) {
+	public int getRowCount() throws TableException
+	{
+		if (rowCount == -1)
+		{
+			try
+			{
+				LineNumberReader lineReader = new LineNumberReader(new InputStreamReader(countStream));
+				String line = null;
+				while ((line = lineReader.readLine()) != null)
+				{
+					line = line.trim();
+				}
+
+				// substract 1 because of header
+				rowCount = lineReader.getLineNumber() - 1;
+			}
+			catch (Exception e)
+			{
 				throw new TableException(e);
 			}
 		}
 		return rowCount;
 	}
-	
+
 	/**
 	 * Helper method to load the Field metadata
 	 * 
@@ -86,13 +97,13 @@ public class CsvTable implements TupleTable
 	 */
 	private void loadColumns() throws Exception
 	{
-		for(String name: csv.colnames())
+		for (String name : csv.colnames())
 		{
 			Field f = new Field(name);
 			columns.add(f);
 		}
 	}
-	
+
 	@Override
 	public List<Field> getColumns()
 	{
@@ -103,17 +114,31 @@ public class CsvTable implements TupleTable
 	public List<Tuple> getRows()
 	{
 		List<Tuple> result = new ArrayList<Tuple>();
-		for(Tuple row: this)
+		for (Tuple row : this)
 		{
 			result.add(row);
 		}
+
 		return result;
 	}
 
 	@Override
 	public Iterator<Tuple> iterator()
 	{
-		//TODO: can one run iterator twice?
+		try
+		{
+			this.reset();
+		}
+		catch (Exception e)
+		{
+			//should not happen as this is second load
+			e.printStackTrace();
+		}
+		
+		if (limit > 0 || offset > 0)
+		{
+			return new TupleIterator(csv, limit, offset);
+		}
 		return csv.iterator();
 	}
 
@@ -127,7 +152,28 @@ public class CsvTable implements TupleTable
 		}
 		catch (IOException e)
 		{
-			throw new TableException(e); 
+			throw new TableException(e);
 		}
+	}
+
+	private void reset() throws FileNotFoundException, IOException, DataFormatException
+	{
+		if (csvFile != null)
+		{
+			csv = new CsvFileReader(new FileInputStream(csvFile));
+			countStream = new FileInputStream(csvFile);
+		}
+		else
+		{
+			csv = new CsvStringReader(csvString);
+			countStream = new ByteArrayInputStream(csvString.getBytes());
+		}
+	}
+
+	@Override
+	public void setLimitOffset(int limit, int offset)
+	{
+		this.limit = limit;
+		this.offset = offset;
 	}
 }
--- modules/datatable/org/molgenis/datatable/model/MemoryTable.java
+++ modules/datatable/org/molgenis/datatable/model/MemoryTable.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.molgenis.fieldtypes.StringField;
+import org.molgenis.framework.db.QueryRule;
 import org.molgenis.model.elements.Field;
 import org.molgenis.util.Tuple;
 
@@ -15,6 +16,8 @@ public class MemoryTable implements TupleTable
 {
 	private List<Field> columns = new ArrayList<Field>();
 	private List<Tuple> rows = new ArrayList<Tuple>();
+	int limit = 0;
+	int offset = 0;
 
 	/**
 	 * Construct from list of tuples. Field will be derived based on column
@@ -45,26 +48,54 @@ public class MemoryTable implements TupleTable
 	@Override
 	public List<Tuple> getRows()
 	{
-		return this.rows;
+		if (limit > 0 || offset > 0)
+		{
+			List<Tuple> result = new ArrayList<Tuple>();
+			
+			int count = 0;
+			int index = 1;
+			for(Tuple row: this.rows)
+			{
+				if(index > offset)
+				{
+					result.add(row);
+					count++;
+					if(count >= limit) break;
+				}
+				index++;
+			}
+			return result;
+		}
+		else
+		{
+			return this.rows;
+		}
 	}
 
 	@Override
 	public Iterator<Tuple> iterator()
 	{
-		return this.rows.iterator();
+		return this.getRows().iterator();
 	}
 
 	@Override
 	public void close()
 	{
-		// TODO Auto-generated method stub
-		
+		// nothing todo
 	}
 
 	@Override
 	public int getRowCount() throws TableException
 	{
 		return rows.size();
+	}
+
+	@Override
+	public void setLimitOffset(int limit, int offset)
+	{
+		this.limit = limit;
+		this.offset = offset;
+
 	}
 
 }
