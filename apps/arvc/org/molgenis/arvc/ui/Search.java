55694446bd30307d0590d66367207c63b11e6a6b
rw42 <robert.wagner42@gmail.com>
2012-06-20 16:59:23 +0200

    Added code that allows us to display a variant database that contains more than one gene. Option to disable genome browser.

--- apps/arvc/org/molgenis/arvc/ui/Search.java
+++ apps/arvc/org/molgenis/arvc/ui/Search.java
@@ -32,10 +32,10 @@ public class Search extends SearchPlugin
 	public Search(String name, ScreenController<?> parent)
 	{
 		super(name, parent);
-		this.getModel().setPatientPager("res/mutation/mvid/patientPager.jsp");
-		this.getModel().setMutationPager("res/mutation/mvid/mutationPager.jsp");
+		this.getModel().setPatientPager("/arvc/patientPager.jsp");
+		this.getModel().setMutationPager("/arvc/mutationPager.jsp");
 		this.getModel().setPatientViewer("/org/molgenis/mutation/ui/search/patient.ftl");
-		this.getModel().getMbrowse().setShowNames(false);
+		this.getModel().getMbrowse().setIsVisible(false);
 		this.getModel().setExpertSearchFormWrapper(new HtmlFormWrapper(new ExpertSearchForm()));
 	}
 
