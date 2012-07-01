23739a925c77c1d2b92900db4d0c391432152486
mswertz <m.a.swertz@rug.nl>
2012-07-01 22:23:43 +0200

    fix for getRowCount -> getCount

--- modules/datatable/org/molgenis/datatable/controller/Renderers.java
+++ modules/datatable/org/molgenis/datatable/controller/Renderers.java
@@ -43,7 +43,7 @@ public class Renderers {
 	public static class JQGridRenderer implements Renderer {
 		@Override
 		public void export(HttpServletResponse response, String fileName, JQGridPlugin jqGridPlugin, TupleTable tupleTable, int totalPages, int currentPage) throws TableException, IOException {
-			final JQGridResult result = JQGridPlugin.buildJQGridResults(tupleTable.getRowCount(), totalPages, currentPage, tupleTable);
+			final JQGridResult result = JQGridPlugin.buildJQGridResults(tupleTable.getCount(), totalPages, currentPage, tupleTable);
 			final PrintWriter pout = new PrintWriter(response.getOutputStream());
 			pout.print(new Gson().toJson(result));
 			pout.close();
