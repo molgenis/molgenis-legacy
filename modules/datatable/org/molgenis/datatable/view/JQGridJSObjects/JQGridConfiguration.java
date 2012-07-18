0a6ccded64acb9eaf16129c706f134fc6733d33f
joris lops <jorislops@gmail.com>
2012-07-18 15:55:40 +0200

    -Filters in JQGrid are enabled/disabled depending on Interface that TupleTable implements that's used as backend

--- modules/datatable/org/molgenis/datatable/view/JQGridJSObjects/JQGridConfiguration.java
+++ modules/datatable/org/molgenis/datatable/view/JQGridJSObjects/JQGridConfiguration.java
@@ -3,49 +3,59 @@ package org.molgenis.datatable.view.JQGridJSObjects;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.molgenis.datatable.model.FilterableTupleTable;
 import org.molgenis.datatable.model.TableException;
 import org.molgenis.datatable.model.TupleTable;
 import org.molgenis.model.elements.Field;
 
 public class JQGridConfiguration {
 
-    private final String id;
-    public final String url;
-    public final String pager;
-    
-    public final String datatype = "json";
-    
-    public List<String> colNames = new ArrayList<String>();
-    public List<JQGridField> colModel = new ArrayList<JQGridField>();
-    public int rowNum = 10;
-    public Integer[] rowList = new Integer[]{10, 20, 30};
-    public boolean viewrecords = true;
-    public String sortorder = "desc";
-    public String caption = "jqGrid View";
-    public boolean autowidth = true;
-    public boolean sortable = true;
-    public HashMap<String, Object> jsonReader = new HashMap<String, Object>();;
-    //public String postData = "viewType : JQ_GRID";
+	private final String id;
+	public final String url;
+	public final String pager;
 
-    public JQGridConfiguration(String id, String idField, String url, String caption, TupleTable tupleTable) throws TableException {
-        this.id = id;
-        this.pager = "#" +id + "Pager";
-        this.url = url;
-        this.caption = caption;
-        //"{repeatitems: false, id: \"Code\"}"
-        jsonReader.put("repeatitems", false);
-        jsonReader.put("id", idField);
-        
-        for(final Field f : tupleTable.getColumns()) {
-            colModel.add(new JQGridField(f));
-            colNames.add(f.getName());
-        }
-    }
+	public final String datatype = "json";
 
-    public JQGridConfiguration(String id, String url, String caption) {
-        this.id = id;
-        this.pager = "#" +id + "Pager";
-        this.url = url;
-        this.caption = caption;
-    }
+	public List<String> colNames = new ArrayList<String>();
+	public List<JQGridField> colModel = new ArrayList<JQGridField>();
+	public int rowNum = 10;
+	public Integer[] rowList = new Integer[] { 10, 20, 30 };
+	public boolean viewrecords = true;
+	public String sortorder = "desc";
+	public String caption = "jqGrid View";
+	public boolean autowidth = true;
+	public boolean sortable = false;
+	public HashMap<String, Object> jsonReader = new HashMap<String, Object>();;
+	// public String postData = "viewType : JQ_GRID";
+
+	public JQGridToolbar toolbar = new JQGridToolbar();
+
+	public JQGridConfiguration(String id, String idField, String url,
+			String caption, TupleTable tupleTable) throws TableException {
+		this.id = id;
+		pager = "#" + id + "Pager";
+		this.url = url;
+		this.caption = caption;
+		// "{repeatitems: false, id: \"Code\"}"
+		jsonReader.put("repeatitems", false);
+		jsonReader.put("id", idField);
+
+		if (tupleTable instanceof FilterableTupleTable) {
+			sortable = true;
+			toolbar.search = true;
+		}
+
+		for (final Field f : tupleTable.getColumns()) {
+			colModel.add(new JQGridField(f));
+			colNames.add(f.getName());
+		}
+	}
+
+	public JQGridConfiguration(String id, String url, String caption) {
+		this.id = id;
+		pager = "#" + id + "Pager";
+		this.url = url;
+		this.caption = caption;
+	}
 }
