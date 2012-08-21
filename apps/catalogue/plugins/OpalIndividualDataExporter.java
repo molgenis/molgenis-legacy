9221eb9fa5653c72ee858ad1f6a0feebbf2fabaf
ChaoPang <ChaoPang229@gmail.com>
2012-08-21 16:46:10 +0200

    convert the excel file to csv file

--- apps/catalogue/plugins/OpalIndividualDataExporter.java
+++ apps/catalogue/plugins/OpalIndividualDataExporter.java
@@ -1,10 +1,15 @@
 package plugins;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import jxl.Cell;
 import jxl.Workbook;
 import jxl.WorkbookSettings;
 import jxl.write.Label;
@@ -57,13 +62,24 @@ public class OpalIndividualDataExporter {
 
 				File mappingResult = new File(
 						"/Users/pc_iverson/Desktop/importData/" + p.getName()
-								+ ".xls");
+								+ ".csv");
+
+				File temp = new File(
+						"/Users/pc_iverson/Desktop/importData/temp.xls");
+
+				OutputStream os = (OutputStream) new FileOutputStream(
+						mappingResult);
+				String encoding = "UTF8";
+
+				OutputStreamWriter osw = new OutputStreamWriter(os, encoding);
+
+				BufferedWriter bw = new BufferedWriter(osw);
+
 				WorkbookSettings ws = new WorkbookSettings();
 
 				ws.setLocale(new Locale("en", "EN"));
 
-				WritableWorkbook workbook = Workbook.createWorkbook(
-						mappingResult, ws);
+				WritableWorkbook workbook = Workbook.createWorkbook(temp, ws);
 
 				WritableSheet dataSheet = workbook.createSheet("importData", 0);
 
@@ -101,12 +117,6 @@ public class OpalIndividualDataExporter {
 					}
 				}
 
-				// for (ObservationTarget target : listOfTargets) {
-
-				// int rowIndex = listOfFeatures.indexOf(target) + 1;
-				//
-				// dataSheet.addCell(new Label(0, rowIndex, target.getName()));
-
 				if (allValues.size() > 0) {
 
 					for (ObservedValue ov : allValues) {
@@ -123,37 +133,30 @@ public class OpalIndividualDataExporter {
 								.getValue()));
 
 					}
+
+					Cell[] row = null;
+
+					// Gets the cells from sheet
+					for (int i = 0; i < dataSheet.getRows(); i++) {
+						row = dataSheet.getRow(i);
+
+						if (row.length > 0) {
+							bw.write(row[0].getContents());
+							for (int j = 1; j < row.length; j++) {
+								bw.write(',');
+								bw.write(row[j].getContents());
+							}
+						}
+						bw.newLine();
+					}
 				}
 
-				// for (String eachFeature : p.getFeatures_Name()) {
-				//
-				// int colIndex = p.getFeatures_Name()
-				// .indexOf(eachFeature) + 1;
-				//
-				// Query<ObservedValue> q = db.query(ObservedValue.class);
-				//
-				// q.addRules(new QueryRule(ObservedValue.TARGET_NAME,
-				// Operator.EQUALS, target.getName()));
-				// q.addRules(new QueryRule(ObservedValue.FEATURE_NAME,
-				// Operator.EQUALS, eachFeature));
-				//
-				// if (q.find().size() > 0) {
-				//
-				// String value = q.find().get(0).getValue();
-				//
-				// dataSheet.addCell(new Label(colIndex, rowIndex,
-				// value));
-				//
-				// } else {
-				// dataSheet
-				// .addCell(new Label(colIndex, rowIndex, ""));
-				// }
-				// }
-				// }
-				workbook.write();
-				workbook.close();
-				System.out.println("finished");
+				bw.flush();
+				bw.close();
+				temp.delete();
 			}
+
+			System.out.println("finished");
 		}
 	}
 }
