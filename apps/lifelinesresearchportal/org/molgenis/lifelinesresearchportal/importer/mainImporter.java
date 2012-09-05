bfb64f7fa100714a787abbe48c367e35d410efe9
joerivandervelde <joeriv@gmail.com>
2012-09-05 12:33:28 +0200

    Refactor of importer to constructors with file arg

--- apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/importer/mainImporter.java
+++ apps/lifelinesresearchportal/org/molgenis/lifelinesresearchportal/importer/mainImporter.java
@@ -1,8 +1,8 @@
 package org.molgenis.lifelinesresearchportal.importer;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +23,16 @@ import org.molgenis.util.Tuple;
 
 import app.DatabaseFactory;
 
-public class mainImporter {
-	public static void main(String[] args) throws SQLException, Exception {
+public class mainImporter
+{
 
+	public mainImporter() throws FileNotFoundException, SQLException, IOException, Exception
+	{
+		this(new File("/Users/pc_iverson/Desktop/Input/HL7Files/voorbeeld1_dataset.csv"));
+	}
+
+	public mainImporter(File voorbeeld1_dataset) throws FileNotFoundException, SQLException, IOException, Exception
+	{
 		// BasicConfigurator.configure();
 
 		// empty db
@@ -36,13 +43,11 @@ public class mainImporter {
 		Logger.getRootLogger().setLevel(Level.ERROR);
 
 		Database db = DatabaseFactory.create();
-		try {
+		try
+		{
 
 			db.beginTx();
 
-			BufferedReader read = new BufferedReader(
-					new FileReader(
-							"/Users/pc_iverson/Desktop/Input/HL7Files/voorbeeld1_dataset.csv"));
 			// BufferedReader read = new BufferedReader(new FileReader(
 			// "/Users/Roan/Work/LifeLines/voorbeeld1_dataset.csv"));
 
@@ -58,15 +63,15 @@ public class mainImporter {
 			p.setName("TestProtocol");
 			db.add(p);
 
-			CsvReader reader = new CsvFileReader(
-					new File(
-							"/Users/pc_iverson/Desktop/Input/HL7Files/voorbeeld1_dataset.csv"));
+			CsvReader reader = new CsvFileReader(voorbeeld1_dataset);
 			// CsvReader reader = new CsvFileReader(new
 			// File("/Users/Roan/Work/LifeLines/voorbeeld1_dataset.csv"));
 
 			// add measurements
-			for (String name : reader.colnames()) {
-				if (!"Pa_Id".equals(name)) {
+			for (String name : reader.colnames())
+			{
+				if (!"Pa_Id".equals(name))
+				{
 					Measurement m = new Measurement();
 					m.setInvestigation(i);
 					m.setName(name);
@@ -79,7 +84,8 @@ public class mainImporter {
 
 			// read the rows into protocolApp and values
 			int count = 1;
-			for (Tuple row : reader) {
+			for (Tuple row : reader)
+			{
 				Individual indi = new Individual();
 				indi.setName(row.getString("Pa_Id"));
 				indi.setInvestigation(i);
@@ -90,8 +96,10 @@ public class mainImporter {
 				pa.setProtocol(p);
 				paList.add(pa);
 
-				for (String column : reader.colnames()) {
-					if (!"Pa_Id".equals(column)) {
+				for (String column : reader.colnames())
+				{
+					if (!"Pa_Id".equals(column))
+					{
 						ObservedValue ob = new ObservedValue();
 						ob.setFeature_Name(column);
 						ob.setTarget_Name(indi.getName());
@@ -104,7 +112,8 @@ public class mainImporter {
 				}
 
 				// write if list too long
-				if (listOfValues.size() > 100000) {
+				if (listOfValues.size() > 100000)
+				{
 					System.out.println("Done");
 					db.add(listOfIndv);
 					db.add(paList);
@@ -128,10 +137,17 @@ public class mainImporter {
 			db.commitTx();
 
 			// Set measurementnames to protocol
-		} catch (Exception e) {
-			db.rollbackTx();
-			e.printStackTrace();
 		}
+		catch (Exception e)
+		{
+			db.rollbackTx();
+			throw e;
+		}
+	}
+
+	public static void main(String[] args) throws SQLException, Exception
+	{
+		new mainImporter();
 	}
 
 }
