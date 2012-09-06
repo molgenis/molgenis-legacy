7ae40d1be27eb9c9ab4463dbf61756d1e5a376a7
Erik Roos <tom.erik.roos@gmail.com>
2012-09-06 17:04:19 +0200

    JQGrid pedmap

--- modules/datatable/org/molgenis/datatable/plugin/JQGridPluginPedMap.java
+++ modules/datatable/org/molgenis/datatable/plugin/JQGridPluginPedMap.java
@@ -8,12 +8,15 @@ import org.molgenis.datatable.model.PedMapTupleTable;
 import org.molgenis.datatable.model.TupleTable;
 import org.molgenis.datatable.view.JQGridView;
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.db.QueryRule;
 import org.molgenis.framework.db.QueryRule.Operator;
 import org.molgenis.framework.ui.EasyPluginController;
 import org.molgenis.framework.ui.ScreenController;
 import org.molgenis.framework.ui.ScreenView;
+import org.molgenis.framework.ui.html.ActionInput;
 import org.molgenis.framework.ui.html.MolgenisForm;
+import org.molgenis.framework.ui.html.SelectInput;
 import org.molgenis.util.HandleRequestDelegationException;
 import org.molgenis.util.Tuple;
 import org.molgenis.xgap.InvestigationFile;
@@ -23,8 +26,12 @@ import decorators.MolgenisFileHandler;
 /** Simple plugin that only shows a data table for testing */
 public class JQGridPluginPedMap extends EasyPluginController<JQGridPluginPedMap>
 {
-	JQGridView tableView;
-	boolean editable = false;
+	private static final long serialVersionUID = 1049195164197133420L;
+	private JQGridView tableView;
+	private List<InvestigationFile> pedFiles;
+	private List<InvestigationFile> mapFiles;
+	private InvestigationFile selectedPedFile;
+	private InvestigationFile selectedMapFile;
 
 	public JQGridPluginPedMap(String name, ScreenController<?> parent)
 	{
@@ -37,37 +44,40 @@ public class JQGridPluginPedMap extends EasyPluginController<JQGridPluginPedMap>
 		// need to (re) load the table
 		try
 		{
-			// only this line changed ...
-			List<InvestigationFile> pedFiles = db.find(InvestigationFile.class, new QueryRule(
-					InvestigationFile.EXTENSION, Operator.EQUALS, "ped"));
+			pedFiles = getInvestigationFiles(db, "ped");
+			mapFiles = getInvestigationFiles(db, "map");
 
-			if (pedFiles.size() != 1)
+			if (pedFiles.isEmpty())
 			{
-				throw new Exception("Expecting 1 PED file");
+				throw new Exception("No ped files");
 			}
 
-			List<InvestigationFile> mapFiles = db.find(InvestigationFile.class, new QueryRule(
-					InvestigationFile.EXTENSION, Operator.EQUALS, "map"));
-
-			if (mapFiles.size() != 1)
+			if (mapFiles.isEmpty())
 			{
-				throw new Exception("Expecting 1 MAP file");
+				throw new Exception("No map files");
+			}
+
+			if (selectedMapFile == null)
+			{
+				selectedMapFile = mapFiles.get(0);
+			}
+
+			if (selectedPedFile == null)
+			{
+				selectedPedFile = pedFiles.get(0);
 			}
 
 			MolgenisFileHandler mfh = new MolgenisFileHandler(db);
 
-			File pedFile = mfh.getFile(pedFiles.get(0), db);
-			File mapFile = mfh.getFile(mapFiles.get(0), db);
+			File pedFile = mfh.getFile(selectedPedFile, db);
+			File mapFile = mfh.getFile(selectedMapFile, db);
 
 			// create table
 			TupleTable table = new PedMapTupleTable(pedFile, mapFile);
 
-			// add editable decorator
-
 			// check which table to show
-			tableView = new JQGridView("test", this, table);
-
-			tableView.setLabel("<b>Table:</b>Testing using the MemoryTupleTable");
+			tableView = new JQGridView("genotypes", this, table);
+			tableView.setLabel("Genotypes");
 		}
 		catch (Exception e)
 		{
@@ -78,29 +88,68 @@ public class JQGridPluginPedMap extends EasyPluginController<JQGridPluginPedMap>
 
 	// handling of the ajax; should be auto-wired via the JQGridTableView
 	// contructor (TODO)
-	public void download_json_test(Database db, Tuple request, OutputStream out)
+	public void download_json_genotypes(Database db, Tuple request, OutputStream out)
 			throws HandleRequestDelegationException
 	{
-		// handle requests for the table named 'test'
-
+		// handle requests for the table named 'genotypes'
 		tableView.handleRequest(db, request, out);
 
 	}
 
-	//
-	// public void handleRequest(Database db, Tuple request) {
-	// if (request.getAction().equals("edit")) {
-	// System.out.println("hello world");
-	// }
-	// }
+	public void reloadTable(Database db, Tuple request)
+	{
+		String pedFileName = request.getString("pedfile");
+		String mapFileName = request.getString("mapfile");
+		System.out.println("XXXXXloadFile mapFile=" + mapFileName);
+
+		for (InvestigationFile file : pedFiles)
+		{
+			if (file.getName().equals(pedFileName))
+			{
+				selectedPedFile = file;
+			}
+		}
+
+		for (InvestigationFile file : mapFiles)
+		{
+			if (file.getName().equals(mapFileName))
+			{
+				selectedMapFile = file;
+			}
+		}
+
+	}
 
 	// what is shown to the user
 	public ScreenView getView()
 	{
 		MolgenisForm view = new MolgenisForm(this);
 
+		SelectInput selectPed = new SelectInput("pedfile");
+		for (InvestigationFile file : pedFiles)
+		{
+			selectPed.addOption(file.getName(), file.getName());
+		}
+		selectPed.setValue(selectedPedFile.getName());
+		view.add(selectPed);
+
+		SelectInput selectMap = new SelectInput("mapfile");
+		for (InvestigationFile file : mapFiles)
+		{
+			selectMap.addOption(file.getName(), file.getName());
+		}
+		selectMap.setValue(selectedMapFile.getName());
+		view.add(selectMap);
+
+		view.add(new ActionInput("reloadTable", "Reload table"));
+
 		view.add(tableView);
 
 		return view;
+	}
+
+	private List<InvestigationFile> getInvestigationFiles(Database db, String extension) throws DatabaseException
+	{
+		return db.find(InvestigationFile.class, new QueryRule(InvestigationFile.EXTENSION, Operator.EQUALS, extension));
 	}
 }
