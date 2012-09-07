11cd2f8d00a93e8787c690bcae2a1be84b6fb854
mdijkstra <mdijkstra@work>
2012-09-07 12:55:07 +0200

    Folding on targets will result in script names that don't contain the target values anymore. Instead the script name is formed by the workflow element name, followed by a task_number that is the tuple's line number in the folded worksheet.

--- modules/compute4/org/molgenis/compute/commandline/FreemarkerHelper.java
+++ modules/compute4/org/molgenis/compute/commandline/FreemarkerHelper.java
@@ -4,49 +4,47 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.molgenis.compute.design.ComputeParameter;
 import org.molgenis.util.Tuple;
 
-public class FreemarkerHelper
-{
-	ComputeBundle computeBundle;
-	
+public class FreemarkerHelper {
+	List<ComputeParameter> paramList;
+
 	public FreemarkerHelper(ComputeBundle computeBundle) {
-		this.computeBundle = computeBundle;
+		this.paramList = computeBundle.getComputeParameters();
 	}
-	
-	
-	public String helloWorld(String name)
-	{
-		return "hello "+name;
+
+	public FreemarkerHelper(List<ComputeParameter> paramList) {
+		this.paramList = paramList;
 	}
-	
-	public List<String> unfoldCSV(Tuple tuple)
-	{
-		return Worksheet.unfoldWorksheetCSV(Arrays.asList(new Tuple[]{tuple}));
+
+	public String helloWorld(String name) {
+		return "hello " + name;
 	}
-	
+
+	public List<String> unfoldCSV(Tuple tuple) {
+		return Worksheet.unfoldWorksheetCSV(Arrays.asList(new Tuple[] { tuple }));
+	}
+
 	/**
 	 * We want to re-fold one tuple
 	 */
-	public List<Tuple> foldOn(Tuple tuple, String targets)
-	{
-		//first unfuld, then refold again
-		List<Tuple> unfolded = Worksheet.unfoldWorksheet(Arrays.asList(new Tuple[]{tuple}));
-		
-		//fold again
-		List<Tuple> folded = Worksheet.foldWorksheet(unfolded, this.computeBundle.getComputeParameters(), Arrays.asList(targets.split(",")));
-		
+	public List<Tuple> foldOn(Tuple tuple, String targets) {
+		// first unfuld, then refold again
+		List<Tuple> unfolded = Worksheet.unfoldWorksheet(Arrays.asList(new Tuple[] { tuple }));
+
+		// fold again
+		List<Tuple> folded = Worksheet.foldWorksheet(unfolded, paramList, Arrays.asList(targets.split(",")));
+
 		return folded;
 	}
-	
-	public List<String> stringList(List<Tuple> folded, String var)
-	{
+
+	public List<String> stringList(List<Tuple> folded, String var) {
 		List<String> lst = new ArrayList<String>();
-		for (int i = 0; i < folded.size(); i ++)
-		{
+		for (int i = 0; i < folded.size(); i++) {
 			lst.add(folded.get(i).getString(var));
 		}
-		
+
 		return lst;
 	}
 }
