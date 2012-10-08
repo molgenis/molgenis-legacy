ac9a07435cfa408c7da6887e358fa624bd398c7a
DennisHendriksen <d.hendriksen@umcg.nl>
2012-10-08 17:08:02 +0200

    close streams

--- src/org/molgenis/util/PropertiesTuple.java
+++ src/org/molgenis/util/PropertiesTuple.java
@@ -6,25 +6,35 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
+
 public class PropertiesTuple extends SimpleTuple
 {
 	public PropertiesTuple(File f) throws FileNotFoundException, IOException
 	{
 		Properties p = new Properties();
-		p.load(new FileInputStream(f));
+		FileInputStream fis = new FileInputStream(f);
+		try
+		{
+			p.load(fis);
+		}
+		finally
+		{
+			IOUtils.closeQuietly(fis);
+		}
 		this.loadFromProperties(p);
 	}
-	
+
 	public PropertiesTuple(Properties p)
 	{
 		this.loadFromProperties(p);
 	}
-	
+
 	private void loadFromProperties(Properties p)
 	{
-		for(Object key: p.keySet())
+		for (Object key : p.keySet())
 		{
-			this.set(key.toString(),p.get(key));
+			this.set(key.toString(), p.get(key));
 		}
 	}
 }
