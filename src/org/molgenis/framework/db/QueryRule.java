acd087c7475ccf9ca73377a23796436d00ee0c7b
RoanKanninga <roankanninga@gmail.com>
2012-10-18 10:05:06 +0200

    added equals test for QueryRule, deleted a non working variable called 'or'

--- src/org/molgenis/framework/db/QueryRule.java
+++ src/org/molgenis/framework/db/QueryRule.java
@@ -10,6 +10,7 @@
 
 package org.molgenis.framework.db;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -48,6 +49,40 @@ public class QueryRule implements Cloneable
 	public QueryRule()
 	{
 
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((field == null) ? 0 : field.hashCode());
+		result = prime * result + Arrays.hashCode(nestedRules);
+		result = prime * result + ((operator == null) ? 0 : operator.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		QueryRule other = (QueryRule) obj;
+		if (field == null)
+		{
+			if (other.field != null) return false;
+		}
+		else if (!field.equals(other.field)) return false;
+		if (!Arrays.equals(nestedRules, other.nestedRules)) return false;
+		if (operator != other.operator) return false;
+		if (value == null)
+		{
+			if (other.value != null) return false;
+		}
+		else if (!value.equals(other.value)) return false;
+		return true;
 	}
 
 	public QueryRule(QueryRule copy)
