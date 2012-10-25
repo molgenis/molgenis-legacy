f82de494788c78df8faedcf6edcf93d7cc53407d
dennishendriksen <d.hendriksen@umcg.nl>
2012-10-25 08:31:25 +0200

    Fix RV_RETURN_VALUE_IGNORED_BAD_PRACTICE FindBugs warnings

--- src/org/molgenis/generators/db/MapperDecoratorGen.java
+++ src/org/molgenis/generators/db/MapperDecoratorGen.java
@@ -2,6 +2,7 @@ package org.molgenis.generators.db;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
@@ -53,7 +54,11 @@ public class MapperDecoratorGen extends ForEachEntityGenerator
 							.lastIndexOf(".") + 1);
 
 					File targetDir = new File(this.getHandWrittenPath(options) + "/" + packageName.replace(".", "/"));
-					targetDir.mkdirs();
+					boolean created = targetDir.mkdirs();
+					if (!created && !targetDir.exists())
+					{
+						throw new IOException("could not create " + targetDir);
+					}
 
 					File targetFile = new File(this.getHandWrittenPath(options) + "/" + fullKlazzName.replace(".", "/")
 							+ ".java");
--- src/org/molgenis/generators/tests/TestDatabaseGen.java
+++ src/org/molgenis/generators/tests/TestDatabaseGen.java
@@ -2,6 +2,7 @@ package org.molgenis.generators.tests;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
@@ -38,7 +39,11 @@ public class TestDatabaseGen extends Generator
 																				// effect?
 
 		File target = new File(this.getSourcePath(options) + "/test/TestDatabase.java");
-		target.getParentFile().mkdirs();
+		boolean created = target.getParentFile().mkdirs();
+		if (!created && !target.getParentFile().exists())
+		{
+			throw new IOException("could not create " + target.getParentFile());
+		}
 
 		String packageName = "test";
 
