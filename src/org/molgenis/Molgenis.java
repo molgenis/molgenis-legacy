6db4d5856b0da2ea1f655c9899951b0687c39dd5
dennishendriksen <d.hendriksen@umcg.nl>
2012-11-12 14:36:49 +0100

    fix the fix!

--- src/org/molgenis/Molgenis.java
+++ src/org/molgenis/Molgenis.java
@@ -1,7 +1,6 @@
 package org.molgenis;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -800,17 +800,14 @@ public class Molgenis
 
 			conn = data_src.getConnection();
 			String create_tables_file_str = options.output_sql + File.separator + "create_tables.sql";
-			InputStream create_tables_file = new ByteArrayInputStream(create_tables_file_str.getBytes("UTF-8"));  
-			logger.debug("using file " + create_tables_file);
-			// String create_tables_file = "generated" + File.separator + "sql"
-			// + File.separator + "create_tables.sql";
 
 			// READ THE FILE
 			StringBuilder create_tables_sqlBuilder = new StringBuilder();
 			try
 			{
-				BufferedReader in = new BufferedReader(new InputStreamReader(create_tables_file));
-				
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						new FileInputStream(create_tables_file_str), Charset.forName("UTF-8")));
+
 				try
 				{
 					String line;
