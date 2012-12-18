5492870fb1696896963b7addfb81e5ada4609058
dennishendriksen <d.hendriksen@umcg.nl>
2012-12-18 10:38:17 +0100

    fix tests due to tuple interface updates

--- test/java/org/molgenis/util/tuple/KeyValueTupleTest.java
+++ test/java/org/molgenis/util/tuple/KeyValueTupleTest.java
@@ -5,8 +5,6 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,17 +16,10 @@ public class KeyValueTupleTest
 	@BeforeMethod
 	public void setUp()
 	{
-		Map<String, String> map = new LinkedHashMap<String, String>();
-		map.put("col1", "val1");
-		map.put("col2", "val2");
-		map.put("col3", "val3");
-		tuple = new KeyValueTuple(map);
-	}
-
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void KeyValueTuple()
-	{
-		new KeyValueTuple(null);
+		tuple = new KeyValueTuple();
+		tuple.set("col1", "val1");
+		tuple.set("col2", "val2");
+		tuple.set("col3", "val3");
 	}
 
 	@Test
@@ -39,18 +30,16 @@ public class KeyValueTupleTest
 		assertEquals(tuple.get("col3"), "val3");
 	}
 
-	@Test
+	@Test(expectedExceptions = UnsupportedOperationException.class)
 	public void getint()
 	{
 		assertEquals(tuple.get(0), "val1");
-		assertEquals(tuple.get(1), "val2");
-		assertEquals(tuple.get(2), "val3");
 	}
 
 	@Test
 	public void getColNames()
 	{
-		Iterator<String> it = tuple.getColNames();
+		Iterator<String> it = tuple.getColNames().iterator();
 		assertTrue(it.hasNext());
 		assertEquals(it.next(), "col1");
 		assertTrue(it.hasNext());
